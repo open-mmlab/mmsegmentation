5b8d92a7bb1496e30898b18bac3b5f9a2263fb2a
Jiarui XU <xvjiarui0826@gmail.com>
2020-03-30 10:28:52 +0800

    move Pad after Normalize

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -9,11 +9,10 @@ train_pipeline = [
     dict(
         type='Resize', img_scale=[(1024, 512), (4096, 2048)], keep_ratio=True),
     dict(type='RandomCrop', crop_size=(713, 713)),
-    dict(type='Pad', size=(713, 713)),
     dict(type='RandomRotate', rotate_range=(-10, 10)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    # dict(type='Pad', size_divisor=8),
+    dict(type='Pad', size=(713, 713)),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
