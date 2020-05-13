2e6d8fab1cca79b56c8a4c32caa17be13da92540
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-14 00:39:54 +0800

    add resnetv1c

---
+++ configs_unify/deeplabv3plus/deeplabv3plus_r101c_8x2_40ki_cityscapes.py
@@ -0,0 +1,4 @@
+_base_ = './deeplabv3plus_r50_8x2_40ki_cityscapes.py'
+model = dict(
+    pretrained='pretrain_model/resnet101_v1c-5fe8ded3.pth',
+    backbone=dict(type='ResNetV1c', depth=101, stem_channels=64))
