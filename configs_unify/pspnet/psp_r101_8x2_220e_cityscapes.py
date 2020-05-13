335747d8dea488603b9e4aef0cf992bf68bb3f35
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-13 11:33:40 +0800

    fixed epoch config pretrain

--- configs_unify/pspnet/psp_r101_8x2_220e_cityscapes.py
+++ configs_unify/pspnet/psp_r101_8x2_220e_cityscapes.py
@@ -1,2 +1,4 @@
 _base_ = './psp_r50_8x2_220e_cityscapes.py'
-model = dict(pretrained=None, backbone=dict(depth=101))
+model = dict(
+    pretrained='pretrain_model/resnet101c128_csail-159f67a3.pth',
+    backbone=dict(depth=101))
--- configs_unify/pspnet/psp_r50_8x2_220e_cityscapes.py
+++ configs_unify/pspnet/psp_r50_8x2_220e_cityscapes.py
@@ -2,3 +2,6 @@ _base_ = [
     '../_base_/models/psp_r50.py', '../_base_/datasets/cityscapes.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_220e.py'
 ]
+model = dict(
+    pretrained='pretrain_model/resnet50c128_csail-0a46e9a7.pth',
+    backbone=dict(stem_channels=128))
