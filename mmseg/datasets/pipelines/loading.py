3b86e8e29db1cfd60f1a431951530085acfe977a
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-19 23:00:54 +0800

    support FileClient

--- mmseg/datasets/pipelines/loading.py
+++ mmseg/datasets/pipelines/loading.py
@@ -1,3 +1,4 @@
+import io
 import os.path as osp
 
 import mmcv
@@ -10,19 +11,29 @@ from ..builder import PIPELINES
 @PIPELINES.register_module()
 class LoadImageFromFile(object):
 
-    def __init__(self, to_float32=False, color_type='color'):
+    def __init__(self,
+                 to_float32=False,
+                 color_type='color',
+                 file_client_args=dict(backend='disk')):
         self.to_float32 = to_float32
         self.color_type = color_type
+        self.file_client_args = file_client_args.copy()
+        self.file_client = None
 
     def __call__(self, results):
+        if self.file_client is None:
+            self.file_client = mmcv.FileClient(**self.file_client_args)
+
         if results.get('img_prefix') is not None:
             filename = osp.join(results['img_prefix'],
                                 results['img_info']['filename'])
         else:
             filename = results['img_info']['filename']
-        img = mmcv.imread(filename, self.color_type)
+        img_bytes = self.file_client.get(filename)
+        img = mmcv.imfrombytes(img_bytes, flag=self.color_type)
         if self.to_float32:
             img = img.astype(np.float32)
+
         results['filename'] = filename
         results['img'] = img
         results['img_shape'] = img.shape
@@ -48,11 +59,19 @@ class LoadMultiChannelImageFromFiles(object):
     Expects results['filename'] to be a list of filenames
     """
 
-    def __init__(self, to_float32=False, color_type='unchanged'):
+    def __init__(self,
+                 to_float32=False,
+                 color_type='unchanged',
+                 file_client_args=dict(backend='disk')):
         self.to_float32 = to_float32
         self.color_type = color_type
+        self.file_client_args = file_client_args.copy()
+        self.file_client = None
 
     def __call__(self, results):
+        if self.file_client is None:
+            self.file_client = mmcv.FileClient(**self.file_client_args)
+
         if results['img_prefix'] is not None:
             filename = [
                 osp.join(results['img_prefix'], fname)
@@ -60,10 +79,14 @@ class LoadMultiChannelImageFromFiles(object):
             ]
         else:
             filename = results['img_info']['filename']
-        img = np.stack(
-            [mmcv.imread(name, self.color_type) for name in filename], axis=-1)
+        img = []
+        for name in filename:
+            img_bytes = self.file_client.get(name)
+            img.append(mmcv.imfrombytes(img_bytes, flag=self.color_type))
+        img = np.stack(img, axis=-1)
         if self.to_float32:
             img = img.astype(np.float32)
+
         results['filename'] = filename
         results['img'] = img
         results['img_shape'] = img.shape
@@ -86,22 +109,32 @@ class LoadMultiChannelImageFromFiles(object):
 @PIPELINES.register_module()
 class LoadAnnotations(object):
 
-    def __init__(self, use_pil=True, reduce_zero_label=False):
+    def __init__(self,
+                 use_pil=True,
+                 reduce_zero_label=False,
+                 file_client_args=dict(backend='disk')):
+        # use PIL P mode to read annotations
         self.use_pil = use_pil
         self.reduce_zero_label = reduce_zero_label
+        self.file_client_args = file_client_args.copy()
+        self.file_client = None
 
-    def _load_semantic_seg(self, results):
+    def __call__(self, results):
+        if self.file_client is None:
+            self.file_client = mmcv.FileClient(**self.file_client_args)
+
         if results.get('seg_prefix', None) is not None:
             filename = osp.join(results['seg_prefix'],
                                 results['ann_info']['seg_map'])
         else:
             filename = results['ann_info']['seg_map']
+        img_bytes = self.file_client.get(filename)
         if self.use_pil:
             gt_semantic_seg = np.array(
-                Image.open(filename).convert('P'), dtype=np.uint8)
+                Image.open(io.BytesIO(img_bytes)).convert('P'), dtype=np.uint8)
         else:
-            gt_semantic_seg = mmcv.imread(
-                filename, flag='unchanged').squeeze().astype(np.uint8)
+            gt_semantic_seg = mmcv.imfrombytes(
+                img_bytes, flag='unchanged').squeeze().astype(np.uint8)
         # reduce zero_label
         if self.reduce_zero_label:
             # avoid using underflow conversion
@@ -110,10 +143,6 @@ class LoadAnnotations(object):
             gt_semantic_seg[gt_semantic_seg == 254] = 255
         results['gt_semantic_seg'] = gt_semantic_seg
         results['seg_fields'].append('gt_semantic_seg')
-        return results
-
-    def __call__(self, results):
-        results = self._load_semantic_seg(results)
         return results
 
     def __repr__(self):
