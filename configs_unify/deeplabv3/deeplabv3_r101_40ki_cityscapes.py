d51f481fb79e264bf8329682fe28d54839575b3a
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-20 00:45:06 +0800

    add 40ki configs

--- configs_unify/deeplabv3/deeplabv3_r101_40ki_cityscapes.py
+++ configs_unify/deeplabv3/deeplabv3_r101_40ki_cityscapes.py
@@ -1,4 +1,9 @@
 _base_ = './deeplabv3_r50_40ki_cityscapes.py'
 model = dict(
     pretrained='pretrain_model/resnet101_v1c-5fe8ded3.pth',
-    backbone=dict(depth=101))
+    backbone=dict(depth=101),
+    decode_head=dict(classes_weight=[
+        0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754, 1.0489, 0.8786,
+        1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955, 1.0865, 1.1529,
+        1.0507
+    ]))
--- configs_unify/gcnet/gc_r50_40ki_cityscapes.py
+++ configs_unify/gcnet/gc_r50_40ki_cityscapes.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/models/nl_r50.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_60ki.py'
+    '../_base_/models/gc_r50.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40ki.py'
 ]
 model = dict(
     decode_head=dict(classes_weight=[
