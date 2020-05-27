24cdbdc3ccbab3c474c529d19e644b8123e38a27
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-28 00:37:07 +0800

    fixed pretrain

--- configs_unify/fcnnet/fcn_r50_trick_40ki_cityscapes.py
+++ configs_unify/fcnnet/fcn_r50_trick_40ki_cityscapes.py
@@ -2,4 +2,4 @@ _base_ = [
     '../_base_/models/fcn_r50.py', '../_base_/datasets/cityscapes.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40ki.py'
 ]
-model = dict(pretrained='pretrain_model/resnet50_v1c_trick-80a4ced9.pth')
+model = dict(pretrained='pretrain_model/resnet50_v1c_trick-2cccc1ad.pth')
