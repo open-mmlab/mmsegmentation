b6d444ace25136f4acfd7705440fbed2f3d45c89
Jiarui XU <xvjiarui0826@gmail.com>
2020-05-28 08:36:25 +0800

    add HeadLr10

--- configs_unify/_base_/schedules/schedule_60ki.py
+++ configs_unify/_base_/schedules/schedule_60ki.py
@@ -1,5 +1,11 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(
+    constructor='HeadOptimizerConstructor',
+    paramwise_cfg=dict(decode_head_lr_mult=10.),
+    type='SGD',
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
