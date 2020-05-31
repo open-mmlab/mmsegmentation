bdefb016312063f1268336aa7525ebf4a0ac3204
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-01 01:41:13 +0800

    fixed train cfg

--- configs_unify/_base_/models/ocr_hr18.py
+++ configs_unify/_base_/models/ocr_hr18.py
@@ -63,3 +63,6 @@ model = dict(
             loss_decode=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     ])
+# model training and testing settings
+train_cfg = dict()
+test_cfg = dict(mode='whole')
