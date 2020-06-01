2066f282467ab91c545fd9724770edd993820042
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-02 02:03:43 +0800

    fixed 1024 hr

--- configs_unify_1024/_base_/datasets/ade.py
+++ configs_unify_1024/_base_/datasets/ade.py
@@ -3,14 +3,14 @@ dataset_type = 'ADEDataset'
 data_root = 'data/ade/ADEChallengeData2016'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (521, 521)
+crop_size = (520, 520)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_seg=True),
-    dict(type='Resize', img_scale=(2049, 521), ratio_range=(0.5, 2.0)),
+    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='Resize', img_scale=(2048, 520), ratio_range=(0.5, 2.0)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='RandomCrop', crop_size=crop_size),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
@@ -18,7 +18,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2049, 521),
+        img_scale=(2048, 520),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
@@ -30,23 +30,23 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='images/training',
         ann_dir='annotations/training',
-        train_pipeline=train_pipeline),
+        pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='images/validation',
         ann_dir='annotations/validation',
-        test_pipeline=test_pipeline),
+        pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         data_root=data_root,
         img_dir='images/validation',
         ann_dir='annotations/validation',
-        test_pipeline=test_pipeline))
+        pipeline=test_pipeline))
