4d21c7b1b1848a72463bfdb911c70c110ad912c9
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-13 09:55:22 +0800

    remove unused backbone

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -1,11 +1,5 @@
-from .fast_scnn import FastSCNN
 from .hrnet import HRNet
-from .mobile_net_v2 import MobileNetV2
 from .resnet import ResNet, ResNetV1c, ResNetV1d
 from .resnext import ResNeXt
-from .xception import Xception65
 
-__all__ = [
-    'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'Xception65',
-    'MobileNetV2', 'FastSCNN'
-]
+__all__ = ['ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet']
--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -1,5 +1,4 @@
 from .res_layer import ResLayer
 from .self_attention_block import SelfAttentionBlock
-from .weight_init import bias_init_with_prob
 
-__all__ = ['bias_init_with_prob', 'ResLayer', 'SelfAttentionBlock']
+__all__ = ['ResLayer', 'SelfAttentionBlock']
