a96915e3edd5c8d920eb2a46a43b0075c7e78073
Jiarui XU <xvjiarui0826@gmail.com>
2020-06-13 16:07:37 +0800

    remove 20ki and 60ki configs

--- configs/hrnet/fcn_hr18s_512x1024_60ki_cityscapes.py
+++
@@ -1,9 +0,0 @@
-_base_ = './fcn_hr18_512x1024_60ki_cityscapes.py'
-model = dict(
-    pretrained='pretrain_model/hrnetv2_w18_small-b5a04e21.pth',
-    backbone=dict(
-        extra=dict(
-            stage1=dict(num_blocks=(2, )),
-            stage2=dict(num_blocks=(2, 2)),
-            stage3=dict(num_modules=3, num_blocks=(2, 2, 2)),
-            stage4=dict(num_modules=2, num_blocks=(2, 2, 2, 2)))))
--- configs/upernet/uper_r101_512x512_60ki_voc12aug.py
+++
@@ -1,4 +0,0 @@
-_base_ = './uper_r50_512x512_60ki_voc12aug.py'
-model = dict(
-    pretrained='pretrain_model/resnet101_v1c_trick-e67eebb6.pth',
-    backbone=dict(depth=101))
