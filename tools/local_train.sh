5bd9a74f69893e78895a64bc4ba15ef097169c69
rainbowsecret <yhyuan@pku.edu.cn>
2020-07-10 14:43:32 +0000

    revise the script

--- tools/local_train.sh
+++ tools/local_train.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-PYTHON="/opt/conda/bin/python"
-# PYTHON="/data/anaconda/envs/pytorch1.5.1/bin/python"
-$PYTHON -m pip install -e .
+PYTHON="/data/anaconda/envs/pytorch1.5.1/bin/python"
+# $PYTHON -m pip install -e .
 
 CONFIG=$1
 GPUS=$2
 PORT=${PORT:-29500}
+WORK_DIR="./work_dirs/${CONFIG}_$3"
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 ${PYTHON} -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch 
-    
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work_dir $WORK_DIR
+
 # ${@:3}
 # ./tools/dist_train.sh configs/ocrnet/ocrnet_r101-d8_512x1024_40k_cityscapes.py 8
