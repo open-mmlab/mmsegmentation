fef887eecf74b84afba56208720d02fdce8b15b5
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-10 18:58:13 +0800

    add test tutorial

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -4,7 +4,6 @@ CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
 PORT=${PORT:-29500}
-$CONFIG\/$GPUS/
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
