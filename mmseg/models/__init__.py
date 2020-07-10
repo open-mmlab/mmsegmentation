04aa8127f2951cd23402ae6a0156b565a5296883
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-11 01:53:41 +0800

    add necks

--- mmseg/models/__init__.py
+++ mmseg/models/__init__.py
@@ -1,11 +1,13 @@
 from .backbones import *  # noqa: F401,F403
-from .builder import (BACKBONES, HEADS, LOSSES, SEGMENTORS, build_backbone,
-                      build_head, build_loss, build_segmentor)
+from .builder import (BACKBONES, HEADS, LOSSES, NECKS, SEGMENTORS,
+                      build_backbone, build_head, build_loss, build_neck,
+                      build_segmentor)
 from .decode_heads import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
+from .necks import *  # noqa: F401,F403
 from .segmentors import *  # noqa: F401,F403
 
 __all__ = [
-    'BACKBONES', 'HEADS', 'LOSSES', 'SEGMENTORS', 'build_backbone',
-    'build_head', 'build_loss', 'build_segmentor'
+    'BACKBONES', 'NECKS', 'HEADS', 'LOSSES', 'SEGMENTORS', 'build_backbone',
+    'build_neck', 'build_head', 'build_loss', 'build_segmentor'
 ]
