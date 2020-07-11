7f1e74f1d08d572e0b2e246dc692f9ebe61e9f2b
rainbowsecret <yhyuan@pku.edu.cn>
2020-07-11 14:37:36 +0000

    revise the ocrnet_sep_aspp

---
+++ configs/ocrnet/ocrnet_sep_aspp_r50-d8_512x1024_40k_cityscapes.py
@@ -0,0 +1,4 @@
+_base_ = [
+    '../_base_/models/ocrnet_sep_aspp_r50-d8.py', '../_base_/datasets/cityscapes_bs2x.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k_bs2x.py'
+]
