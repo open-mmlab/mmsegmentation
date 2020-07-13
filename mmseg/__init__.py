dede738ea8e150b8a6b94abafd8eb1e87068724d
Kai Chen <chenkaidev@gmail.com>
2020-07-13 13:45:12 +0800

    add pypi deployment

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -1,3 +1,3 @@
-from .version import __version__, short_version
+from .version import __version__, short_version, version_info
 
-__all__ = ['__version__', 'short_version']
+__all__ = ['__version__', 'short_version', 'version_info']
