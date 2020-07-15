05a335f15a16b92f64c173675796395355d88be6
rainbowsecret <yhyuan@pku.edu.cn>
2020-07-15 05:00:48 +0000

    resolve the conflicts

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -1,7 +1,6 @@
 # dataset settings
 dataset_type = 'CityscapesDataset'
-# data_root = 'data/cityscapes/'
-data_root = '/home/yuhui/teamdrive/dataset/original_cityscapes/'
+data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 1024)
