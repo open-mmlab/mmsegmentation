03000948e7036aa7b9d5f1ad241f18c284e8cf95
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-17 01:37:43 +0800

    reduce config

--- configs/fp16/fcn_r101-d8_512x1024_80k_cityscapes.py
+++ configs/fp16/fcn_r101-d8_512x1024_80k_cityscapes.py
@@ -1,4 +1,3 @@
-_base_ = '../fcn/fcn_r50-d8_512x1024_80k_cityscapes.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
+_base_ = '../fcn/fcn_r101-d8_512x1024_80k_cityscapes.py'
 # fp16 settings
 fp16 = dict(loss_scale=512.)
