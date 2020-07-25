afa9d099c9c6150cf869d845d72b4dfca7e4c40a
rainbowsecret <yhyuan@pku.edu.cn>
2020-07-25 07:46:06 +0000

    remove the schedule configs

--- configs/_base_/schedules/schedule_40k_lr2x.py
+++
@@ -1,9 +0,0 @@
-# optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0005)
-optimizer_config = dict()
-# learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=2e-4, by_epoch=False)
-# runtime settings
-total_iters = 40000
-checkpoint_config = dict(by_epoch=False, interval=4000, create_symlink=False)
-evaluation = dict(interval=4000, metric='mIoU')
