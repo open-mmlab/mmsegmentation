2c77085dc22c277881e2604e6778cc029994c651
johnzja <johnzja@qq.com>
2020-08-07 15:15:13 +0800

    doc strings added

--- mmseg/models/decode_heads/sep_fcn_head.py
+++ mmseg/models/decode_heads/sep_fcn_head.py
@@ -5,6 +5,27 @@ from .fcn_head import FCNHead
 
 @HEADS.register_module()
 class SepFCNHead(FCNHead):
+    """Depthwise-Separable Fully Convolutional Network for Semantic Segmentation
+
+    This head is implemented according to Fast-SCNN.
+    Args:
+        in_channels(int): Number of output channels of FFM.
+
+        channels(int): Number of middle-stage channels in the decode head.
+
+        concat_input(bool): Whether to concatenate original decode input into
+            the result of consecutive convolution layers.
+
+        num_classes(int): Used to determine the dimension of final prediction tensor.
+
+        in_index(int): Correspond with 'out_indices' in FastSCNN backbone.
+
+        norm_cfg (dict|None): Config of norm layers.
+
+        align_corners (bool): align_corners argument of F.interpolate.
+
+        loss_decode(dict): Config of loss type and some relevant additional options.
+    """
 
     def __init__(self, **kwargs):
         super(SepFCNHead, self).__init__(**kwargs)
