94913d1d77713079aa9a1417446e9201ae9fdb98
johnzja <johnzja@qq.com>
2020-08-09 21:48:06 +0800

    configs_unify del

--- configs_unify/annnet/ann_r101_60ki_cityscapes.py
+++
@@ -1,4 +0,0 @@
-_base_ = './ann_r50_60ki_cityscapes.py'
-model = dict(
-    pretrained='pretrain_model/resnet101_v1c-5fe8ded3.pth',
-    backbone=dict(depth=101))
--- configs_unify/nlnet/nl_r101_40ki_cityscapes.py
+++
@@ -1,4 +0,0 @@
-_base_ = './nl_r50_40ki_cityscapes.py'
-model = dict(
-    pretrained='pretrain_model/resnet101_v1c-5fe8ded3.pth',
-    backbone=dict(depth=101))
