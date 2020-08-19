56552c7850d4ae321e3e9391a640197e758366c3
Jiarui XU <xvjiarui0826@gmail.com>
2020-08-19 23:36:03 +0800

    matched inference performance

--- mmseg/models/decode_heads/dnl_head.py
+++ mmseg/models/decode_heads/dnl_head.py
@@ -7,10 +7,37 @@ from .fcn_head import FCNHead
 
 
 class DisentangledNonLocal2d(NonLocal2d):
+    """Disentangled Non-Local Blocks.
 
-    def __init__(self, *arg, **kwargs):
+    Args:
+        temperature (float): Temperature to adjust attention. Default: 0.05
+    """
+
+    def __init__(self, *arg, temperature, **kwargs):
         super().__init__(*arg, **kwargs)
+        self.temperature = temperature
         self.conv_mask = nn.Conv2d(self.in_channels, 1, kernel_size=1)
+        # self.g = ConvModule(
+        #     self.in_channels,
+        #     self.in_channels,
+        #     kernel_size=1,
+        #     conv_cfg=None,
+        #     act_cfg=None,
+        #     bias=False)
+        # self.gamma = Scale(0.)
+        # delattr(self, 'conv_out')
+
+    def embedded_gaussian(self, theta_x, phi_x):
+        """Embedded gaussian with temperature."""
+
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+        pairwise_weight = torch.matmul(theta_x, phi_x)
+        if self.use_scale:
+            # theta_x.shape[-1] is `self.inter_channels`
+            pairwise_weight /= theta_x.shape[-1]**0.5
+        pairwise_weight /= self.temperature
+        pairwise_weight = pairwise_weight.softmax(dim=-1)
+        return pairwise_weight
 
     def forward(self, x):
         # x: [N, C, H, W]
@@ -50,6 +77,8 @@ class DisentangledNonLocal2d(NonLocal2d):
         y = y.permute(0, 2, 1).contiguous().reshape(n, self.inter_channels,
                                                     *x.size()[2:])
 
+        # y = self.gamma(y)
+
         # unary_mask: [N, 1, HxW]
         unary_mask = self.conv_mask(x)
         unary_mask = unary_mask.view(n, 1, -1)
@@ -60,9 +89,7 @@ class DisentangledNonLocal2d(NonLocal2d):
         unary_x = unary_x.permute(0, 2, 1).contiguous().reshape(
             n, self.inter_channels, 1, 1)
 
-        y += unary_x
-
-        output = x + self.conv_out(y)
+        output = x + self.conv_out(y) + unary_x
 
         return output
 
@@ -80,24 +107,28 @@ class DNLHead(FCNHead):
             sqrt(1/inter_channels). Default: False.
         mode (str): The nonlocal mode. Options are 'embedded_gaussian',
             'dot_product'. Default: 'embedded_gaussian.'.
+        temperature (float): Temperature to adjust attention. Default: 0.05
     """
 
     def __init__(self,
                  reduction=2,
-                 use_scale=False,
+                 use_scale=True,
                  mode='embedded_gaussian',
+                 temperature=0.05,
                  **kwargs):
         super(DNLHead, self).__init__(num_convs=2, **kwargs)
         self.reduction = reduction
         self.use_scale = use_scale
         self.mode = mode
+        self.temperature = temperature
         self.dnl_block = DisentangledNonLocal2d(
             in_channels=self.channels,
             reduction=self.reduction,
             use_scale=self.use_scale,
             conv_cfg=self.conv_cfg,
             norm_cfg=self.norm_cfg,
-            mode=self.mode)
+            mode=self.mode,
+            temperature=self.temperature)
 
     def forward(self, inputs):
         """Forward function."""
