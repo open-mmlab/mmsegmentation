03d6ddc8f4882758c7b07cfe8e69dcbc12427fb4
Jiarui XU <xvjiarui0826@gmail.com>
2020-08-24 21:29:56 +0800

    Fixed slide inference

--- tools/train.py
+++ tools/train.py
@@ -19,7 +19,7 @@ from mmseg.utils import collect_env, get_root_logger
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a segmentor')
     parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work_dir', help='the dir to save logs and models')
+    parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--load-from', help='the checkpoint file to load weights from')
     parser.add_argument(
