2eb1ba372c93aec20d5777abc79e3a10c2a4bbad
Jiarui XU <xvjiarui0826@gmail.com>
2020-08-25 12:46:33 +0800

    Fixed dilate

--- configs/mobilenet_v2/pspnet_m-v2-d8_512x512_160k_ade20k.py
+++ configs/mobilenet_v2/pspnet_m-v2-d8_512x512_160k_ade20k.py
@@ -5,8 +5,8 @@ model = dict(
         _delete_=True,
         type='MobileNetV2',
         widen_factor=1.,
-        strides=(1, 2, 2, 2, 1, 2, 1),
-        dilations=(1, 1, 1, 1, 1, 1, 1),
+        strides=(1, 2, 2, 1, 1, 1, 1),
+        dilations=(1, 1, 1, 2, 2, 4, 4),
         out_indices=(1, 2, 4, 6)),
     decode_head=dict(in_channels=320),
     auxiliary_head=dict(in_channels=96))
