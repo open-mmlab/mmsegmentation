1fbb537958eb36ddd77567ee6837ec9610706715
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-09-04 15:35:52 +0800

    [Feature] Support MobileNetV2 backbone (#86)
    * [Feature] Support MobileNetV2 backbone
    
    * Fixed import
    
    * Fixed test
    
    * Fixed test
    
    * Fixed dilate
    
    * upload model
    
    * update table
    
    * update table
    
    * update bibtex
    
    * update MMCV requirement

--- tests/test_models/test_forward.py
+++ tests/test_models/test_forward.py
@@ -157,6 +157,11 @@ def test_sem_fpn_forward():
     _test_encoder_decoder_forward('sem_fpn/fpn_r50_512x1024_80k_cityscapes.py')
 
 
+def test_mobilenet_v2_forward():
+    _test_encoder_decoder_forward(
+        'mobilenet_v2/pspnet_m-v2-d8_512x1024_80k_cityscapes.py')
+
+
 def get_world_size(process_group):
 
     return 1
