0be9e8d4ca7f26474d2bbf0156a541e766e7c1fa
Jiarui XU <xvjiarui0826@gmail.com>
2020-09-07 02:01:45 +0800

    Use MMCV DepthSepConv

--- mmseg/models/backbones/fast_scnn.py
+++ mmseg/models/backbones/fast_scnn.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-from mmcv.cnn import ConvModule, constant_init, kaiming_init
+from mmcv.cnn import (ConvModule, DepthwiseSeparableConvModule, constant_init,
+                      kaiming_init)
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from mmseg.models.decode_heads.psp_head import PPM
-from mmseg.ops import DepthwiseSeparableConvModule, resize
+from mmseg.ops import resize
 from mmseg.utils import InvertedResidual
 from ..builder import BACKBONES
 
