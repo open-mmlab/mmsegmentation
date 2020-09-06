4e3c0de5bc36922bdf6deb857e59f4de80e6a211
Jiarui XU <xvjiarui0826@gmail.com>
2020-09-06 17:06:49 +0800

    Add tests

--- tests/test_models/test_forward.py
+++ tests/test_models/test_forward.py
@@ -162,6 +162,11 @@ def test_mobilenet_v2_forward():
         'mobilenet_v2/pspnet_m-v2-d8_512x1024_80k_cityscapes.py')
 
 
+def test_emanet_forward():
+    _test_encoder_decoder_forward(
+        'emanet/emanet_r50-d8_512x1024_80k_cityscapes.py')
+
+
 def get_world_size(process_group):
 
     return 1
