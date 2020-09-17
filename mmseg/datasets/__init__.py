f5496901408ad78724995ea81efb86159355dc24
yamengxi <854341266@qq.com>
2020-09-17 15:03:57 +0800

    fix code syntax again

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -3,8 +3,8 @@ from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cityscapes import CityscapesDataset
 from .custom import CustomDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
-from .voc import PascalVOCDataset
 from .pascal_context import PascalContextDataset
+from .voc import PascalVOCDataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
