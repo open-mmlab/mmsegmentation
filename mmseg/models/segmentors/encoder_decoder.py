7baed6513eb69cb9e7831c617b0d06268f51b2de
yamengxi <49829199+yamengxi@users.noreply.github.com>
2020-09-22 14:56:13 +0800

    Add Pascal Context to mmsegmentation (#133)
    * Add Pascal Context to mmsegmentation
    
    * Add benchmark result to Pascal Context
    
    * fix mmcv version
    
    * fix code syntax
    
    * fix code syntax again
    
    * Update mmseg/models/segmentors/encoder_decoder.py
    
    update hint
    
    Co-authored-by: Jerry Jiarui XU <xvjiarui0826@gmail.com>
    
    * update comment
    
    * fix pascal context model path
    
    * fix model path mistake again
    
    * fix model path mistake again
    
    * fix model path mistakes again
    
    Co-authored-by: Jerry Jiarui XU <xvjiarui0826@gmail.com>

--- mmseg/models/segmentors/encoder_decoder.py
+++ mmseg/models/segmentors/encoder_decoder.py
@@ -167,13 +167,15 @@ class EncoderDecoder(BaseSegmentor):
 
     # TODO refactor
     def slide_inference(self, img, img_meta, rescale):
-        """Inference by sliding-window with overlap."""
+        """Inference by sliding-window with overlap.
+
+        If h_crop > h_img or w_crop > w_img, the small patch will be used to
+        decode without padding.
+        """
 
         h_stride, w_stride = self.test_cfg.stride
         h_crop, w_crop = self.test_cfg.crop_size
         batch_size, _, h_img, w_img = img.size()
-        assert h_crop <= h_img and w_crop <= w_img, (
-            'crop size should not greater than image size')
         num_classes = self.num_classes
         h_grids = max(h_img - h_crop + h_stride - 1, 0) // h_stride + 1
         w_grids = max(w_img - w_crop + w_stride - 1, 0) // w_stride + 1
