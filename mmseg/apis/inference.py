9a8de03aab8a49e268273b6de88d571214d7a4d1
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-09-24 08:53:02 +0200

    Add docstring

--- mmseg/apis/inference.py
+++ mmseg/apis/inference.py
@@ -16,7 +16,8 @@ def init_segmentor(config, checkpoint=None, device='cuda:0'):
             object.
         checkpoint (str, optional): Checkpoint path. If left as None, the model
             will not load any weights.
-
+        device (str, optional) CPU/CUDA device option. Default 'cuda:0'.
+            Use 'cpu' for loading model on CPU.
     Returns:
         nn.Module: The constructed segmentor.
     """
