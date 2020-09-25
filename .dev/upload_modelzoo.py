f3f443ff719bf4f17b0e17182b8261bb68b8b1dc
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-09-25 19:56:10 +0800

    [Enhance] Migrate to MMCV DepthwiseSeparableConv (#158)
    * Add D16-MG124 models
    
    * Use MMCV DepthSepConv
    
    * add OHEM
    
    * add warmup
    
    * fixed test
    
    * fixed test
    
    * change to bs 16
    
    * revert config
    
    * add models
    
    * seperate

---
+++ .dev/upload_modelzoo.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import os.path as osp
+
+import oss2
+
+ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID', None)
+ACCESS_KEY_SECRET = os.getenv('OSS_ACCESS_KEY_SECRET', None)
+BUCKET_NAME = 'openmmlab'
+ENDPOINT = 'https://oss-accelerate.aliyuncs.com'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Upload models to OSS')
+    parser.add_argument('model_zoo', type=str, help='model_zoo input')
+    parser.add_argument(
+        '--dst-folder',
+        type=str,
+        default='mmsegmentation/v0.5',
+        help='destination folder')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    model_zoo = args.model_zoo
+    dst_folder = args.dst_folder
+    bucket = oss2.Bucket(
+        oss2.Auth(ACCESS_KEY_ID, ACCESS_KEY_SECRET), ENDPOINT, BUCKET_NAME)
+
+    for root, dirs, files in os.walk(model_zoo):
+        for file in files:
+            file_path = osp.relpath(osp.join(root, file), model_zoo)
+            print(f'Uploading {file_path}')
+
+            oss2.resumable_upload(bucket, osp.join(dst_folder, file_path),
+                                  osp.join(model_zoo, file_path))
+            bucket.put_object_acl(
+                osp.join(dst_folder, file_path), oss2.OBJECT_ACL_PUBLIC_READ)
+
+
+if __name__ == '__main__':
+    main()
--- mmseg/core/utils/__init__.py
+++ mmseg/core/utils/__init__.py
@@ -1,4 +1,3 @@
-from .dist_utils import allreduce_grads
 from .misc import add_prefix
 
-__all__ = ['add_prefix', 'allreduce_grads']
+__all__ = ['add_prefix']
