8bc434ba6b46659bb26df6f2559e19f7280236e1
Robin Karlsson <robin.karlsson0@gmail.com>
2020-10-07 10:28:00 +0900

    Added inherent label index option

--- tools/convert_datasets/a2d2.py
+++ tools/convert_datasets/a2d2.py
@@ -8,8 +8,72 @@ import numpy as np
 
 import mmcv
 
-# key: RGB color, value: trainId (from Cityscapes)
-SEG_COLOR_DICT = {
+
+# Dictionaries specifying which A2D2 segmentation color corresponds to
+
+# A2D2 'trainId' value
+#   key: RGB color, value: trainId
+SEG_COLOR_DICT_A2D2 = {
+    (255, 0, 0):      28,  # Car 1 --> Car
+    (200, 0, 0):      28,  # Car 2 --> Car
+    (150, 0, 0):      28,  # Car 3 --> Car
+    (128, 0, 0):      28,  # Car 4 --> Car
+    (182, 89, 6):     27,  # Bicycle 1 --> Bicycle
+    (150, 50, 4):     27,  # Bicycle 2 --> Bicycle
+    (90, 30, 1):      27,  # Bicycle 3 --> Bicycle
+    (90, 30, 30):     27,  # Bicycle 4 --> Bicycle
+    (204, 153, 255):  26,  # Pedestrian 1 --> Person
+    (189, 73, 155):   26,  # Pedestrian 2 --> Person
+    (239, 89, 191):   26,  # Pedestrian 3 --> Person
+    (255, 128, 0):    30,  # Truck 1 --> Truck
+    (200, 128, 0):    30,  # Truck 2 --> Truck
+    (150, 128, 0):    30,  # Truck 3 --> Truck
+    (0, 255, 0):      32,  # Small vehicles 1 --> Motorcycle ?
+    (0, 200, 0):      32,  # Small vehicles 2 --> Motorcycle ?
+    (0, 150, 0):      32,  # Small vehicles 3 --> Motorcycle ?
+    (0, 128, 255):    19,  # Traffic signal 1 --> Traffic light
+    (30, 28, 158):    19,  # Traffic signal 2 --> Traffic light
+    (60, 28, 100):    19,  # Traffic signal 3 --> Traffic light
+    (0, 255, 255):    20,  # Traffic sign 1 --> Traffic sign
+    (30, 220, 220):   20,  # Traffic sign 2 --> Traffic sign
+    (60, 157, 199):   20,  # Traffic sign 3 --> Traffic sign
+    (255, 255, 0):    29,  # Utility vehicle 1 --> Car ?
+    (255, 255, 200):  29,  # Utility vehicle 2 --> Car ?
+    (0, 0, 100):      31,  # Tractor --> Truck ?
+    (233, 100, 0):    16,  # Sidebars --> Fence
+    (110, 110, 0):    12,  # Speed bumper --> Road
+    (128, 128, 0):    14,  # Curbstone --> Sidewalk
+    (255, 193, 37):   6,  # Solid line --> Road
+    (64, 0, 64):      22,  # Irrelevant signs --> Ignore
+    (185, 122, 87):   17,  # Road blocks --> Fence
+    (139, 99, 108):   1,  # Non-drivable street --> Road
+    (210, 50, 115):   8,  # Zebra crossing --> Road
+    (255, 0, 128):    34,  # Obstacles / trash --> Ignore ?
+    (255, 246, 143):  18,  # Poles --> Poles
+    (150, 0, 150):    2,  # RD restricted area --> Road ?
+    (204, 255, 153):  33,  # Animals --> Ignore ?
+    (238, 162, 173):  9,  # Grid structure --> Road ???
+    (33, 44, 177):    21,  # Signal corpus --> Traffic light ?
+    (180, 50, 180):   3,  # Drivable cobblestone --> Road
+    (255, 70, 185):   23,  # Electronic traffic --> Ignore ???
+    (238, 233, 191):  4,  # Slow drive area --> Road
+    (147, 253, 194):  24,  # Nature object --> Vegetation
+    (150, 150, 200):  5,  # Parking area --> Road
+    (180, 150, 200):  13,  # Sidewalk --> Sidewalk 
+    (72, 209, 204):   255,  # Ego car --> Ignore
+    (200, 125, 210):  11,  # Painted driv. instr. --> Road ???
+    (159, 121, 238):  10,  # Traffic guide obj. --> Road ???
+    (128, 0, 255):    7,  # Dashed line --> Road
+    (255, 0, 255):    0,  # RD normal street --> Road
+    (135, 206, 255):  25,  # Sky --> Sky
+    (241, 230, 255):  15,  # Buildings --> Building
+    (96, 69, 143):    255,  # Blurred area --> Ignore ?
+    (53, 46, 82):     255,  # Rain dirt --> Ignore ?
+}
+
+# Cityscapes 'trainId' value
+#   key: RGB color, value: trainId (from Cityscapes)
+SEG_COLOR_DICT_CITYSCAPES = {
     (255, 0, 0):      13,  # Car 1 --> Car
     (200, 0, 0):      13,  # Car 2 --> Car
     (150, 0, 0):      13,  # Car 3 --> Car
@@ -24,6 +88,7 @@ SEG_COLOR_DICT = {
     (255, 128, 0):    14,  # Truck 1 --> Truck
     (200, 128, 0):    14,  # Truck 2 --> Truck
     (150, 128, 0):    14,  # Truck 3 --> Truck
+    (0, 0, 100):      14,  # Tractor --> Truck ?
     (0, 255, 0):      17,  # Small vehicles 1 --> Motorcycle ?
     (0, 200, 0):      17,  # Small vehicles 2 --> Motorcycle ?
     (0, 150, 0):      17,  # Small vehicles 3 --> Motorcycle ?
@@ -33,15 +98,14 @@ SEG_COLOR_DICT = {
     (0, 255, 255):     7,  # Traffic sign 1 --> Traffic sign
     (30, 220, 220):    7,  # Traffic sign 2 --> Traffic sign
     (60, 157, 199):    7,  # Traffic sign 3 --> Traffic sign
-    (255, 255, 0):   255,  # Utility vehicle 1 --> Ignore ?
-    (255, 255, 200): 255,  # Utility vehicle 2 --> Ignore ?
-    (233, 100, 0):    25,  # Sidebars --> Ignore ?
+    (255, 255, 0):    13,  # Utility vehicle 1 --> Car ?
+    (255, 255, 200):  13,  # Utility vehicle 2 --> Car ?
+    (233, 100, 0):     4,  # Sidebars --> Fence
     (110, 110, 0):     0,  # Speed bumper --> Road
     (128, 128, 0):     1,  # Curbstone --> Sidewalk
     (255, 193, 37):    0,  # Solid line --> Road
     (64, 0, 64):     255,  # Irrelevant signs --> Ignore
-    (185, 122, 87):    4,  # Road blocks --> Fence ?
-    (0, 0, 100):     255,  # Tractor --> Ignore ?
+    (185, 122, 87):    4,  # Road blocks --> Fence
     (139, 99, 108):    0,  # Non-drivable street --> Road
     (210, 50, 115):    0,  # Zebra crossing --> Road
     (255, 0, 128):   255,  # Obstacles / trash --> Ignore ?
@@ -76,7 +140,43 @@ def modify_label_filename(label_filepath):
     return label_filepath
 
 
-def convert_label_values_to_trainids(label_filepath, ignore_id=255):
+def convert_a2d2_trainids(label_filepath, ignore_id=255):
+    '''Saves a new semantic label using the A2D2 label categories.
+
+    The new image is saved into the same directory as the original image having
+    an additional suffix.
+
+    Args:
+        label_filepath: Path to the original semantic label.
+        ignore_id: Default value for unlabeled elements.
+    '''
+    # Read label file as Numpy array (H, W, 3)
+    orig_label = Image.open(label_filepath)
+    orig_label = np.array(orig_label, dtype=np.int)
+
+    # Empty array with all elements set as 'ignore id' label
+    H, W, _ = orig_label.shape
+    mod_label = ignore_id*np.ones((H,W), dtype=np.int)
+
+    seg_colors = list(SEG_COLOR_DICT_A2D2.keys())
+    for seg_color in seg_colors:
+        # The operation produce (H,W,3) array of (i,j,k)-wise truth values
+        mask = (orig_label == seg_color)
+        # Take the product channel-wise to falsify any partial match and
+        # collapse RGB channel dimension (H,W,3) --> (H,W)
+        #   Ex: [True, False, False] --> [False]
+        mask = np.prod(mask, axis=-1)
+        mask = mask.astype(np.bool)
+        # Segment masked elements with 'trainIds' value
+        mod_label[mask] = SEG_COLOR_DICT_A2D2[seg_color]
+
+    # Save new 'trainids' semantic label
+    label_filepath = modify_label_filename(label_filepath)
+    label_img = Image.fromarray(mod_label.astype(np.uint8))
+    label_img.save(label_filepath)
+
+
+def convert_cityscapes_trainids(label_filepath, ignore_id=255):
     '''Saves a new semantic label following the Cityscapes 'trainids' format.
 
     The new image is saved into the same directory as the original image having
@@ -94,7 +194,7 @@ def convert_label_values_to_trainids(label_filepath, ignore_id=255):
     H, W, _ = orig_label.shape
     mod_label = ignore_id*np.ones((H,W), dtype=np.int)
 
-    seg_colors = list(SEG_COLOR_DICT.keys())
+    seg_colors = list(SEG_COLOR_DICT_CITYSCAPES.keys())
     for seg_color in seg_colors:
         # The operation produce (H,W,3) array of (i,j,k)-wise truth values
         mask = (orig_label == seg_color)
@@ -104,7 +204,7 @@ def convert_label_values_to_trainids(label_filepath, ignore_id=255):
         mask = np.prod(mask, axis=-1)
         mask = mask.astype(np.bool)
         # Segment masked elements with 'trainIds' value
-        mod_label[mask] = SEG_COLOR_DICT[seg_color]
+        mod_label[mask] = SEG_COLOR_DICT_CITYSCAPES[seg_color]
 
     # Save new 'trainids' semantic label
     label_filepath = modify_label_filename(label_filepath)
@@ -137,10 +237,10 @@ def restructure_a2d2_directory(a2d2_path, val_ratio, test_ratio,
         ... ...
 
     Args:
-        a2d2_path:
-        val_ratio:
+        a2d2_path: Absolute path to the A2D2 'camera_lidar_semantic' directory.
+        val_ratio: Float value representing ratio of validation samples.
         test_ratio:
-        label_suffix:
+        label_suffix: Label filename ending string.
     '''
 
     # Create new directory structure (if not already exist)
@@ -163,10 +263,8 @@ def restructure_a2d2_directory(a2d2_path, val_ratio, test_ratio,
     total_samples = len(img_filepaths)
     train_ratio = 1.0 - val_ratio - test_ratio
 
-    # For writing the train/val/test samples sammaries to file
-    train_filenames = []
-    val_filenames = []
-    test_filenames = []
+    train_idx_end = int(np.floor(train_ratio*(total_samples-1)))
+    val_idx_end = train_idx_end + int(np.ceil(val_ratio*total_samples))
 
     # Create symlinks file-by-file
     for sample_idx in range(total_samples):
@@ -178,18 +276,12 @@ def restructure_a2d2_directory(a2d2_path, val_ratio, test_ratio,
         img_filename = img_filepath.rpartition('/')[2]
         ann_filename = ann_filepath.rpartition('/')[2]
 
-        train_idx_end = int(train_ratio*total_samples)
-        val_idx_end = train_idx_end + int(val_ratio*total_samples)
-
-        if sample_idx < train_idx_end:
+        if sample_idx <= train_idx_end:
             split = 'train'
-            train_filenames.append(img_filename[:-4])
-        elif sample_idx < val_idx_end:
+        elif sample_idx <= val_idx_end:
             split = 'val'
-            val_filenames.append(img_filename[:-4])
         else:
             split = 'test'
-            test_filenames.append(img_filename[:-4])
 
         img_symlink_path = osp.join(a2d2_path, 'img_dir', split, img_filename)
         ann_symlink_path = osp.join(a2d2_path, 'ann_dir', split, ann_filename)
@@ -204,35 +296,60 @@ def restructure_a2d2_directory(a2d2_path, val_ratio, test_ratio,
         except FileExistsError:
             pass
 
-    # Sample summary files
-    with open(osp.join(a2d2_path, f'train.txt'), 'w') as file:
-        file.writelines(fn + '\n' for fn in train_filenames)
-    with open(osp.join(a2d2_path, f'val.txt'), 'w') as file:
-        file.writelines(fn + '\n' for fn in val_filenames)
-    with open(osp.join(a2d2_path, f'test.txt'), 'w') as file:
-        file.writelines(fn + '\n' for fn in test_filenames)
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Convert A2D2 annotations to TrainIds')
-    parser.add_argument('a2d2_path', help='A2D2 segmentation data path')
-    parser.add_argument('-o', '--out-dir', help='output path')
-    parser.add_argument('--conv', default=True, help='Convert label images')
     parser.add_argument(
-        '--restruct', default=True, help="Restructure directory structure")
+        'a2d2_path', help='A2D2 segmentation data absolute path\
+                           (NOT the symbolically linked one!)')
     parser.add_argument(
-        '--val', default=0.1, type=float, help="Validation set sample ratio")
+        '-o', '--out-dir', help='Output path')
     parser.add_argument(
-        '--test', default=0.1, type=float, help="Test set sample ratio")
+        '--conv', default=True, help='Convert label images')
     parser.add_argument(
-        '--nproc', default=1, type=int, help='number of process')
+        '--restruct', default=True, help='Restructure directory structure')
+    parser.add_argument(
+        '--type', default='cityscapes', help='Semantic label conversion type')
+    parser.add_argument(
+        '--val', default=0.02, type=float, help='Validation set sample ratio')
+    parser.add_argument(
+        '--test', default=0., type=float, help='Test set sample ratio')
+    parser.add_argument(
+        '--nproc', default=1, type=int, help='Number of process')
     args = parser.parse_args()
     return args
 
 
 def main():
-    '''
+    '''Program for making Audi's A2D2 dataset compatible with mmsegmentation.
+
+    NOTE: The input argument path must be the ABSOLUTE PATH to the dataset
+          - NOT the symbolically linked one (i.e. data/a2d2)!
+
+    Segmentation label conversion:
+        The A2D2 labels are instance segmentations (i.e. car_1, car_2, ...),
+        while semantic segmentation requires categorical segmentations.
+        
+        The function 'convert_TYPE_trainids()' converts all instance
+        segmentation to thier corresponding categorical segmentation and saves
+        them as new label image files.
+
+        Conversion type options
+            A2D2: Generates segmentations using inherent categories.
+            Cityscapes: Generates segmentations according to the categories and
+                        indexing (i.e. 'trainIds') as in Cityscapes.
+
+    Directory restructuring:
+        A2D2 files are not arranged in the required 'train/val/test' directory
+        structure.
+
+        The function 'restructure_a2d2_directory' creates a new compatible
+        directory structure in the root directory, and fills it with symbolic
+        links to the input and segmentation label images.
+    
+    Example usage:
+        python tools/convert_datasets/a2d2.py path/to/a2d2/camera_lidar_semantic
     '''
     args = parse_args()
     a2d2_path = args.a2d2_path
@@ -245,16 +362,24 @@ def main():
 
     # Convert segmentation images to the Cityscapes 'TrainIds' values
     if args.conv == True:
-        if args.nproc > 1:
-            mmcv.track_parallel_progress(convert_label_values_to_trainids, label_filepaths, args.nproc)
+        seg_type = args.type
+        if seg_type == "cityscapes":
+            if args.nproc > 1:
+                mmcv.track_parallel_progress(convert_cityscapes_trainids, label_filepaths, args.nproc)
+            else:
+                mmcv.track_progress(convert_cityscapes_trainids, label_filepaths)
+        elif seg_type == "a2d2":
+            if args.nproc > 1:
+                mmcv.track_parallel_progress(convert_a2d2_trainids, label_filepaths, args.nproc)
+            else:
+                mmcv.track_progress(convert_a2d2_trainids, label_filepaths)
+        
         else:
-            mmcv.track_progress(convert_label_values_to_trainids, label_filepaths)
+            raise ValueError
 
     # Restructure directory structure into 'img_dir' and 'ann_dir'
-    if args.restruct:
+    if args.restruct == True:
         restructure_a2d2_directory(out_dir, args.val, args.test)
-
-
 
 
 if __name__ == '__main__':
