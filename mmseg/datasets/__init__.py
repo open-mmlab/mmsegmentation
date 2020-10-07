35ab68bac932e38b175e404ddc3ae28a863393a8
Robin Karlsson <robin.karlsson0@gmail.com>
2020-10-07 11:44:21 +0900

    Fixed isort error

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -1,3 +1,4 @@
+from .a2d2 import A2D2Dataset
 from .ade import ADE20KDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cityscapes import CityscapesDataset
@@ -5,7 +6,6 @@ from .custom import CustomDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .pascal_context import PascalContextDataset
 from .voc import PascalVOCDataset
-from .a2d2 import A2D2Dataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
