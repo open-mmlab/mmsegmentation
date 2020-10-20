2c426616e3f14f068880fdd1fd6f64e9f5efd568
Robin Karlsson <robin.karlsson0@gmail.com>
2020-10-20 13:27:21 +0900

    Improved docstring with Cityscapes label format reference

--- mmseg/datasets/a2d2.py
+++ mmseg/datasets/a2d2.py
@@ -6,10 +6,25 @@ from .custom import CustomDataset
 
 @DATASETS.register_module()
 class A2D2Dataset(CustomDataset):
-    """Cityscapes dataset.
+    """A2D2 dataset following the Cityscapes 'trainids' label format.
+
+    The dataset features 41,280 frames with semantic segmentation in 38
+    categories. Each pixel in an image is given a label describing the type of
+    object it represents, e.g. pedestrian, car, vegetation, etc.
+
+    NOTE: Instance segmentations and some segmentation classes are collapsed to
+          follow the Cityscapes 'trainids' label format.
+          Ex: 'Car 1' and 'Car 2' --> 'Car'
+              'Non-drivable street' --> 'Road'
+              'Speed bumper' --> 'Road'
+
+          The segmentation conversion is defined in the following file:
+              tools/convert_datasets/a2d2.py
 
     The ``img_suffix`` is fixed to '.png' and ``seg_map_suffix`` is
     fixed to '_labelTrainIds.png' for A2D2 dataset.
+
+    Ref: https://www.a2d2.audi/a2d2/en/dataset.html
     """
 
     CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
