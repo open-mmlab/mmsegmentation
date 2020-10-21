95476e5850c61da214647d8f4da826cbc6307616
yamengxi <854341266@qq.com>
2020-10-21 16:38:42 +0800

    Define blood vessel dataset and fix filename bug

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -1,13 +1,18 @@
 from .ade import ADE20KDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .chase_db1 import Chase_db1Dataset
 from .cityscapes import CityscapesDataset
 from .custom import CustomDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
+from .drive import DriveDataset
+from .hrf import HrfDataset
 from .pascal_context import PascalContextDataset
+from .stare import StareDataset
 from .voc import PascalVOCDataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset',
-    'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset'
+    'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
+    'Chase_db1Dataset', 'DriveDataset', 'HrfDataset', 'StareDataset'
 ]
--- tools/convert_datasets/drive.py
+++ tools/convert_datasets/drive.py
@@ -48,10 +48,9 @@ def main():
         now_dir = osp.join(tmp_dir, 'training', 'images')
         for img_name in os.listdir(now_dir):
             img = mmcv.imread(osp.join(now_dir, img_name))
-            mmcv.imwrite(
-                img,
-                osp.join(out_dir, 'images', 'training',
-                         osp.splitext(img_name)[0] + '.jpg'))
+            mmcv.imwrite(img, osp.join(out_dir, 'images', 'training',
+                         osp.splitext(img_name)[0].replace('_training', '')
+                         + '.jpg'))
 
         now_dir = osp.join(tmp_dir, 'training', '1st_manual')
         for img_name in os.listdir(now_dir):
@@ -70,10 +69,9 @@ def main():
         now_dir = osp.join(tmp_dir, 'test', 'images')
         for img_name in os.listdir(now_dir):
             img = mmcv.imread(osp.join(now_dir, img_name))
-            mmcv.imwrite(
-                img,
-                osp.join(out_dir, 'images', 'validation',
-                         osp.splitext(img_name)[0] + '.jpg'))
+            mmcv.imwrite(img, osp.join(out_dir, 'images', 'validation',
+                         osp.splitext(img_name)[0].replace('_test', '')
+                         + '.jpg'))
 
         now_dir = osp.join(tmp_dir, 'test', '1st_manual')
         if osp.exists(now_dir):
