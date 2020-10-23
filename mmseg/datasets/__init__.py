3bdc276888c806b659d3709e33b4ecea8cb2877f
yamengxi <49829199+yamengxi@users.noreply.github.com>
2020-10-23 08:27:40 +0800

    Define blood vessel dataset and fix filename bug (#203)
    * fix filename bug
    
    * define blood vessel dataset
    
    * redo debug
    
    * fix small bug
    
    * rename dataset
    
    Co-authored-by: yamengxi <yamengxi@sensetime.com>

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -1,13 +1,18 @@
 from .ade import ADE20KDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .chase_db1 import ChaseDB1Dataset
 from .cityscapes import CityscapesDataset
 from .custom import CustomDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
+from .drive import DRIVEDataset
+from .hrf import HRFDataset
 from .pascal_context import PascalContextDataset
+from .stare import STAREDataset
 from .voc import PascalVOCDataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset',
-    'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset'
+    'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
+    'ChaseDB1Dataset', 'DRIVEDataset', 'HRFDataset', 'STAREDataset'
 ]
