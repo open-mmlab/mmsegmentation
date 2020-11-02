afec8331181633d02309adbbb1a92655843ef400
yamengxi <854341266@qq.com>
2020-11-02 14:39:59 +0800

    modify configs

--- configs/_base_/models/cgnet_m3n21.py
+++ configs/_base_/models/cgnet_m3n21.py
@@ -19,4 +19,11 @@ model = dict(
         in_index=-1,
         dropout_ratio=0,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)))
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, \
+            class_weight=[2.5959933, 6.7415504, 3.5354059, 9.8663225, 9.690899,
+                          9.369352, 10.289121, 9.953208, 4.3097677, 9.490387,
+                          7.674431, 9.396905, 10.347791, 6.3927646, 10.226669,
+                          10.241062, 10.280587, 10.396974, 10.055647])))
+# model training and testing settings
+train_cfg = dict(sampler=None)
+test_cfg = dict(mode='whole')
--- configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
+++ configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 
 # optimizer
-optimizer = dict(type='Adam', lr=0.001, weight_decay=0.0005)
+optimizer = dict(type='Adam', lr=0.001, eps=1e-08, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
@@ -13,21 +13,17 @@ total_iters = 60000
 checkpoint_config = dict(by_epoch=False, interval=4000)
 evaluation = dict(interval=4000, metric='mIoU')
 
-crop_size = (680, 680)
-cudnn_benchmark = True
-# model training and testing settings
-train_cfg = dict(sampler=None)
-test_cfg = dict(mode='whole')
-
 img_norm_cfg = dict(
     mean=[72.39239876, 82.90891754, 73.15835921], std=[1, 1, 1], to_rgb=True)
+crop_size = (680, 680)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
-    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='RandomCrop', crop_size=crop_size),
+    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
@@ -40,7 +36,6 @@ test_pipeline = [
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
