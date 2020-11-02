718fab1398b7daade5debf3869f2c193631861ab
yamengxi <854341266@qq.com>
2020-11-02 16:34:25 +0800

    fix name bug

--- configs/_base_/models/cgnet_m3n21.py
+++ configs/_base_/models/cgnet_m3n21.py
@@ -8,8 +8,8 @@ model = dict(
         in_channels=3,
         num_channels=[32, 64, 128],
         num_blocks=[3, 21],
-        dilation=[2, 4],
-        reduction=[8, 16]),
+        dilations=[2, 4],
+        reductions=[8, 16]),
     decode_head=dict(
         type='CGHead',
         in_channels=256,
@@ -19,11 +19,15 @@ model = dict(
         in_index=-1,
         dropout_ratio=0,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0, \
-            class_weight=[2.5959933, 6.7415504, 3.5354059, 9.8663225, 9.690899,
-                          9.369352, 10.289121, 9.953208, 4.3097677, 9.490387,
-                          7.674431, 9.396905, 10.347791, 6.3927646, 10.226669,
-                          10.241062, 10.280587, 10.396974, 10.055647])))
+            type='CrossEntropyLoss',
+            use_sigmoid=False,
+            loss_weight=1.0,
+            class_weight=[
+                2.5959933, 6.7415504, 3.5354059, 9.8663225, 9.690899, 9.369352,
+                10.289121, 9.953208, 4.3097677, 9.490387, 7.674431, 9.396905,
+                10.347791, 6.3927646, 10.226669, 10.241062, 10.280587,
+                10.396974, 10.055647
+            ])))
 # model training and testing settings
 train_cfg = dict(sampler=None)
 test_cfg = dict(mode='whole')
--- configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
+++ configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
@@ -36,6 +36,7 @@ test_pipeline = [
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
