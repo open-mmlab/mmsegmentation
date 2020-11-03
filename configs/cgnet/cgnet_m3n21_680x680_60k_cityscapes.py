aa331894cc9eda5970fd4b237fc8df7585bf2a1d
yamengxi <854341266@qq.com>
2020-11-03 10:45:07 +0800

    fix data pipeline

--- configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
+++ configs/cgnet/cgnet_m3n21_680x680_60k_cityscapes.py
@@ -20,10 +20,10 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='RandomCrop', crop_size=crop_size),
     dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
