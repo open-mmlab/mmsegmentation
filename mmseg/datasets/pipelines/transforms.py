365ce9e2af8bea31c878b7f7f0cedaf84bdc7ebf
yamengxi <49829199+yamengxi@users.noreply.github.com>
2020-11-10 18:30:07 +0800

    [Feature] Add Rerange transform (#228)
    * add rerange transform
    
    * restore
    
    * delete rerange config
    
    * delete rerange config hint
    
    * add min < max assert
    
    * restore

--- mmseg/datasets/pipelines/transforms.py
+++ mmseg/datasets/pipelines/transforms.py
@@ -393,6 +393,53 @@ class Normalize(object):
 
 
 @PIPELINES.register_module()
+class Rerange(object):
+    """Rerange the image pixel value.
+
+    Args:
+        min_value (float or int): Minimum value of the reranged image.
+            Default: 0.
+        max_value (float or int): Maximum value of the reranged image.
+            Default: 255.
+    """
+
+    def __init__(self, min_value=0, max_value=255):
+        assert isinstance(min_value, float) or isinstance(min_value, int)
+        assert isinstance(max_value, float) or isinstance(max_value, int)
+        assert min_value < max_value
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, results):
+        """Call function to rerange images.
+
+        Args:
+            results (dict): Result dict from loading pipeline.
+
+        Returns:
+            dict: Reranged results.
+        """
+
+        img = results['img']
+        img_min_value = np.min(img)
+        img_max_value = np.max(img)
+
+        assert img_min_value < img_max_value
+        # rerange to [0, 1]
+        img = (img - img_min_value) / (img_max_value - img_min_value)
+        # rerange to [min_value, max_value]
+        img = img * (self.max_value - self.min_value) + self.min_value
+        results['img'] = img
+
+        return results
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(min_value={self.min_value}, max_value={self.max_value})'
+        return repr_str
+
+
+@PIPELINES.register_module()
 class RandomCrop(object):
     """Random crop the image & seg.
 
