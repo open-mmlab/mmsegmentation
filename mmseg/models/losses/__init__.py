db1c4083986c9ea6079fd7a6a339ca8869d73ea6
Junjun2016 <hejunjun@sjtu.edu.cn>
2020-11-13 14:49:38 +0800

    support soft cross entropy loss

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -1,10 +1,13 @@
 from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
+from .soft_cross_entropy_loss import (SoftCrossEntropyLoss, calculate_weights,
+                                      soft_cross_entropy)
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss'
+    'weight_reduce_loss', 'weighted_loss', 'SoftCrossEntropyLoss',
+    'calculate_weights', 'soft_cross_entropy'
 ]
