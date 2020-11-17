61e1d5c814be05c02fef9a5c6c0b629bbf2908c9
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2020-11-17 00:14:03 -0800

    [Enhancement] Support ignore_index for sigmoid BCE (#210)
    * [Enhancement] Add args check for ignore_index
    
    * Support ignore_index

--- configs/fastscnn/fast_scnn_4x8_80k_lr0.12_cityscapes.py
+++ configs/fastscnn/fast_scnn_4x8_80k_lr0.12_cityscapes.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 
 # Re-config the data sampler.
-data = dict(samples_per_gpu=8, workers_per_gpu=4)
+data = dict(samples_per_gpu=2, workers_per_gpu=4)
 
 # Re-config the optimizer.
 optimizer = dict(type='SGD', lr=0.12, momentum=0.9, weight_decay=4e-5)
