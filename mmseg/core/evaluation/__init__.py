993be2523b908d1e0cb45bc68e92d210b5b2cda1
Junjun2016 <hejunjun@sjtu.edu.cn>
2020-11-24 11:21:22 +0800

    add dice evaluation metric (#225)
    * add dice evaluation metric
    
    * add dice evaluation metric
    
    * add dice evaluation metric
    
    * support 2 metrics
    
    * support 2 metrics
    
    * support 2 metrics
    
    * support 2 metrics
    
    * fix docstring
    
    * use np.round once for all

--- mmseg/core/evaluation/__init__.py
+++ mmseg/core/evaluation/__init__.py
@@ -1,7 +1,8 @@
 from .class_names import get_classes, get_palette
 from .eval_hooks import DistEvalHook, EvalHook
-from .mean_iou import mean_iou
+from .metrics import eval_metrics, mean_dice, mean_iou
 
 __all__ = [
-    'EvalHook', 'DistEvalHook', 'mean_iou', 'get_classes', 'get_palette'
+    'EvalHook', 'DistEvalHook', 'mean_dice', 'mean_iou', 'eval_metrics',
+    'get_classes', 'get_palette'
 ]
