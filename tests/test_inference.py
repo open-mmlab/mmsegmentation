41ccbdc5c92f9e240da6ba8009a110ff9f1bb69d
YouqingXiaozhua <843213558@qq.com>
2020-11-24 20:55:13 +0800

    add inference test

---
+++ tests/test_inference.py
@@ -0,0 +1,25 @@
+import os.path as osp
+
+from mmseg.apis import inference_segmentor, init_segmentor
+import mmcv
+
+
+def test_test_time_augmentation_on_cpu():
+    config_file = 'configs/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes.py'
+    config = mmcv.Config.fromfile(config_file)
+
+    # Replace SyncBN with BN to inference on CPU
+    norm_cfg = dict(type='BN', requires_grad=True)
+    config.model.backbone.norm_cfg = norm_cfg
+    config.model.decode_head.norm_cfg = norm_cfg
+    config.model.auxiliary_head.norm_cfg = norm_cfg
+
+    # Enable test time augmentation
+    config.test_pipeline[1].flip = True
+
+    checkpoint_file = None
+    model = init_segmentor(config, checkpoint_file, device='cpu')
+
+    img = mmcv.imread(osp.join(osp.dirname(__file__), 'data/color.jpg'), 'color')
+    result = inference_segmentor(model, img)
+    assert result[0].shape == (288, 512)
