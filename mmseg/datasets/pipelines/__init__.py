90785e3f4d04df6ac1696cb883e7645af71ef6ae
yamengxi <854341266@qq.com>
2020-12-02 12:40:09 +0800

    fix __init__

--- mmseg/datasets/pipelines/__init__.py
+++ mmseg/datasets/pipelines/__init__.py
@@ -5,12 +5,12 @@ from .loading import LoadAnnotations, LoadImageFromFile
 from .test_time_aug import MultiScaleFlipAug
 from .transforms import (CLAHE, AdjustGamma, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip,
-                         RandomRotate, Rerange, Resize, Rgb2Gray, SegRescale)
+                         RandomRotate, Rerange, Resize, RGB2Gray, SegRescale)
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad', 'RandomCrop',
     'Normalize', 'SegRescale', 'PhotoMetricDistortion', 'RandomRotate',
-    'AdjustGamma', 'CLAHE', 'Rerange', 'Rgb2Gray'
+    'AdjustGamma', 'CLAHE', 'Rerange', 'RGB2Gray'
 ]
