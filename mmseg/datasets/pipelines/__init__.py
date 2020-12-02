7f7e995417e6bca7091cfcd177ea7397033c1749
yamengxi <854341266@qq.com>
2020-12-02 12:33:51 +0800

    add all data transformers to __init__

--- mmseg/datasets/pipelines/__init__.py
+++ mmseg/datasets/pipelines/__init__.py
@@ -3,12 +3,14 @@ from .formating import (Collect, ImageToTensor, ToDataContainer, ToTensor,
                         Transpose, to_tensor)
 from .loading import LoadAnnotations, LoadImageFromFile
 from .test_time_aug import MultiScaleFlipAug
-from .transforms import (Normalize, Pad, PhotoMetricDistortion, RandomCrop,
-                         RandomFlip, Resize, SegRescale)
+from .transforms import (CLAHE, AdjustGamma, Normalize, Pad,
+                         PhotoMetricDistortion, RandomCrop, RandomFlip,
+                         Rerange, Resize, Rgb2Gray, SegRescale)
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad', 'RandomCrop',
-    'Normalize', 'SegRescale', 'PhotoMetricDistortion'
+    'Normalize', 'SegRescale', 'PhotoMetricDistortion', 'AdjustGamma', 'CLAHE',
+    'Rerange', 'Rgb2Gray'
 ]
