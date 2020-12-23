c31911d6e016b563e35678addfa830d380beb0c4
Jiarui XU <xvjiarui0826@gmail.com>
2020-12-23 10:26:03 -0800

    add more configs

--- configs/fcn/fcn_r18-d8_769x769_80k_cityscapes.py
+++ configs/fcn/fcn_r18-d8_769x769_80k_cityscapes.py
@@ -1,6 +1,7 @@
 _base_ = './fcn_r50-d8_769x769_80k_cityscapes.py'
 model = dict(
-    pretrained='https://download.openmmlab.com/pretrain/third_party/resnet18_v1c-b5776b93.pth',
+    pretrained=
+    'https://download.openmmlab.com/pretrain/third_party/resnet18_v1c-b5776b93.pth',
     backbone=dict(depth=18),
     decode_head=dict(
         in_channels=512,
--- configs/fcn/fcn_r50b-d8_512x1024_80k_cityscapes.py
+++ configs/fcn/fcn_r50b-d8_512x1024_80k_cityscapes.py
@@ -1,4 +1,2 @@
 _base_ = './fcn_r50-d8_512x1024_80k_cityscapes.py'
-model = dict(
-    type='ResNet',
-    pretrained='torchvision://resnet50')
+model = dict(type='ResNet', pretrained='torchvision://resnet50')
