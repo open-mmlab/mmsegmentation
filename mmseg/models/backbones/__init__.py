25d8d77fab767097701128fa5255c1bdb3d95112
yamengxi <49829199+yamengxi@users.noreply.github.com>
2020-12-26 02:02:50 -0600

    [New model] Support MobileNetV3 (#268)
    * delete markdownlint
    
    * Support MobileNetV3
    
    * fix import
    
    * add mobilenetv3 head and configs
    
    * Modify MobileNetV3 to semantic segmentation version
    
    * modify mobilenetv3 configs
    
    * add std configs
    
    * fix Conv2dAdaptivePadding bug
    
    * add configs
    
    * add unitest and fix bugs
    
    * fix lraspp unitest bugs
    
    * restore
    
    * fix unitest
    
    * add MobileNetV3 docstring
    
    * add mmcv
    
    * add mmcv
    
    * fix syntax bug
    
    * fix unitest bug
    
    * fix unitest bug
    
    * fix unitest bugs
    
    * fix docstring
    
    * add configs
    
    * restore
    
    * delete unnecessary assert
    
    * modify unitest
    
    * delete benchmark

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -2,6 +2,7 @@ from .cgnet import CGNet
 from .fast_scnn import FastSCNN
 from .hrnet import HRNet
 from .mobilenet_v2 import MobileNetV2
+from .mobilenet_v3 import MobileNetV3
 from .resnest import ResNeSt
 from .resnet import ResNet, ResNetV1c, ResNetV1d
 from .resnext import ResNeXt
@@ -9,5 +10,5 @@ from .unet import UNet
 
 __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
-    'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet'
+    'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3'
 ]
