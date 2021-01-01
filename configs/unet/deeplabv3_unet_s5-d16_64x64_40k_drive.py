8d44cb669bc59e25070a1edb9743cad530ea66ff
Junjun2016 <hejunjun@sjtu.edu.cn>
2021-01-01 23:37:43 +0800

    Add UNet as backbone and FCN PSPNet DeepLabV3 as decode_head benchmark on 4 retinal vessel segmentation datasets

--- configs/unet/deeplabv3_unet_s5-d16_64x64_40k_drive.py
+++ configs/unet/deeplabv3_unet_s5-d16_64x64_40k_drive.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/unet_s5-d16.py', '../_base_/datasets/drive.py',
+    '../_base_/models/deeplabv3_unet_s5-d16.py', '../_base_/datasets/drive.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
 test_cfg = dict(crop_size=(64, 64), stride=(42, 42))
