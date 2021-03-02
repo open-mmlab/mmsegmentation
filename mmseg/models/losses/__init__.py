631322bc1149b23a5fd80a8e457d2834b64d5993
xiexinch <xinchen.xie@qq.com>
2021-03-02 20:44:38 +0800

    minor change

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -1,8 +1,8 @@
 from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
-from .lovasz_loss import LovaszLoss
 from .dice_loss import DiceLoss
+from .lovasz_loss import LovaszLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
