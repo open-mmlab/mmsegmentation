d3fd6aef5972e0b147991b87fbd5d3f597422c95
xiexinch <xinchen.xie@qq.com>
2021-03-27 14:32:03 +0800

    inherit from base class

--- mmseg/core/evaluation/eval_hooks.py
+++ mmseg/core/evaluation/eval_hooks.py
@@ -1,143 +1,146 @@
 import os.path as osp
 import warnings
 
+from mmcv.runner import Hook
 from torch.utils.data import DataLoader
 
 try:
     from mmcv.runner import DistEvalHook as BasicDistEvalHook
     from mmcv.runner import EvalHook as BasicEvalHook
+
     use_mmcv_hook = True
 except ImportError:
     warnings.warn('Please install the latest version of mmcv.')
-    from mmcv.runner import Hook
     use_mmcv_hook = False
 
-if not use_mmcv_hook:
 
-    class EvalHook(Hook):
-        """Evaluation hook.
+class SegEvalHook(Hook):
+    """Evaluation hook.
 
-        Attributes:
-            dataloader (DataLoader): A PyTorch dataloader.
-            interval (int): Evaluation interval (by epochs). Default: 1.
-        """
+    Attributes:
+        dataloader (DataLoader): A PyTorch dataloader.
+        interval (int): Evaluation interval (by epochs). Default: 1.
+    """
 
-        def __init__(self,
-                     dataloader,
-                     interval=1,
-                     by_epoch=False,
-                     **eval_kwargs):
-            if not isinstance(dataloader, DataLoader):
-                raise TypeError(
-                    'dataloader must be a pytorch DataLoader, but got '
-                    f'{type(dataloader)}')
-            warnings.warn(
-                'DeprecatedWarning: EvalHook and DistEvalHook in mmseg will'
-                'be deprecated, please install mmcv from master branch.')
-            self.dataloader = dataloader
-            self.interval = interval
-            self.by_epoch = by_epoch
-            self.eval_kwargs = eval_kwargs
+    def __init__(self, dataloader, interval=1, by_epoch=False, **eval_kwargs):
+        if not isinstance(dataloader, DataLoader):
+            raise TypeError('dataloader must be a pytorch DataLoader, but got '
+                            f'{type(dataloader)}')
+        warnings.warn(
+            'DeprecatedWarning: EvalHook and DistEvalHook in mmseg will'
+            'be deprecated, please install mmcv from master branch.')
+        self.dataloader = dataloader
+        self.interval = interval
+        self.by_epoch = by_epoch
+        self.eval_kwargs = eval_kwargs
 
-        def after_train_iter(self, runner):
-            """After train epoch hook."""
-            if self.by_epoch or not self.every_n_iters(runner, self.interval):
-                return
-            from mmseg.apis import single_gpu_test
-            runner.log_buffer.clear()
-            results = single_gpu_test(
-                runner.model, self.dataloader, show=False)
+    def after_train_iter(self, runner):
+        """After train epoch hook."""
+        if self.by_epoch or not self.every_n_iters(runner, self.interval):
+            return
+        from mmseg.apis import single_gpu_test
+        runner.log_buffer.clear()
+        results = single_gpu_test(runner.model, self.dataloader, show=False)
+        self.evaluate(runner, results)
+
+    def after_train_epoch(self, runner):
+        """After train epoch hook."""
+        if not self.by_epoch or not self.every_n_epochs(runner, self.interval):
+            return
+        from mmseg.apis import single_gpu_test
+        runner.log_buffer.clear()
+        results = single_gpu_test(runner.model, self.dataloader, show=False)
+        self.evaluate(runner, results)
+
+    def evaluate(self, runner, results):
+        """Call evaluate function of dataset."""
+        eval_res = self.dataloader.dataset.evaluate(
+            results, logger=runner.logger, **self.eval_kwargs)
+        for name, val in eval_res.items():
+            runner.log_buffer.output[name] = val
+        runner.log_buffer.ready = True
+
+
+class SegDistEvalHook(SegEvalHook):
+    """Distributed evaluation hook.
+
+    Attributes:
+        dataloader (DataLoader): A PyTorch dataloader.
+        interval (int): Evaluation interval (by epochs). Default: 1.
+        tmpdir (str | None): Temporary directory to save the results of all
+            processes. Default: None.
+        gpu_collect (bool): Whether to use gpu or cpu to collect results.
+            Default: False.
+    """
+
+    def __init__(self,
+                 dataloader,
+                 interval=1,
+                 gpu_collect=False,
+                 by_epoch=False,
+                 **eval_kwargs):
+        if not isinstance(dataloader, DataLoader):
+            raise TypeError(
+                'dataloader must be a pytorch DataLoader, but got {}'.format(
+                    type(dataloader)))
+        self.dataloader = dataloader
+        self.interval = interval
+        self.gpu_collect = gpu_collect
+        self.by_epoch = by_epoch
+        self.eval_kwargs = eval_kwargs
+
+    def after_train_iter(self, runner):
+        """After train epoch hook."""
+        if self.by_epoch or not self.every_n_iters(runner, self.interval):
+            return
+        from mmseg.apis import multi_gpu_test
+        runner.log_buffer.clear()
+        results = multi_gpu_test(
+            runner.model,
+            self.dataloader,
+            tmpdir=osp.join(runner.work_dir, '.eval_hook'),
+            gpu_collect=self.gpu_collect)
+        if runner.rank == 0:
+            print('\n')
             self.evaluate(runner, results)
 
-        def after_train_epoch(self, runner):
-            """After train epoch hook."""
-            if not self.by_epoch or not self.every_n_epochs(
-                    runner, self.interval):
-                return
-            from mmseg.apis import single_gpu_test
-            runner.log_buffer.clear()
-            results = single_gpu_test(
-                runner.model, self.dataloader, show=False)
+    def after_train_epoch(self, runner):
+        """After train epoch hook."""
+        if not self.by_epoch or not self.every_n_epochs(runner, self.interval):
+            return
+        from mmseg.apis import multi_gpu_test
+        runner.log_buffer.clear()
+        results = multi_gpu_test(
+            runner.model,
+            self.dataloader,
+            tmpdir=osp.join(runner.work_dir, '.eval_hook'),
+            gpu_collect=self.gpu_collect)
+        if runner.rank == 0:
+            print('\n')
             self.evaluate(runner, results)
 
-        def evaluate(self, runner, results):
-            """Call evaluate function of dataset."""
-            eval_res = self.dataloader.dataset.evaluate(
-                results, logger=runner.logger, **self.eval_kwargs)
-            for name, val in eval_res.items():
-                runner.log_buffer.output[name] = val
-            runner.log_buffer.ready = True
 
-    class DistEvalHook(EvalHook):
-        """Distributed evaluation hook.
-
-        Attributes:
-            dataloader (DataLoader): A PyTorch dataloader.
-            interval (int): Evaluation interval (by epochs). Default: 1.
-            tmpdir (str | None): Temporary directory to save the results of all
-                processes. Default: None.
-            gpu_collect (bool): Whether to use gpu or cpu to collect results.
-                Default: False.
-        """
-
-        def __init__(self,
-                     dataloader,
-                     interval=1,
-                     gpu_collect=False,
-                     by_epoch=False,
-                     **eval_kwargs):
-            if not isinstance(dataloader, DataLoader):
-                raise TypeError(
-                    'dataloader must be a pytorch DataLoader, but got {}'.
-                    format(type(dataloader)))
-            self.dataloader = dataloader
-            self.interval = interval
-            self.gpu_collect = gpu_collect
-            self.by_epoch = by_epoch
-            self.eval_kwargs = eval_kwargs
-
-        def after_train_iter(self, runner):
-            """After train epoch hook."""
-            if self.by_epoch or not self.every_n_iters(runner, self.interval):
-                return
-            from mmseg.apis import multi_gpu_test
-            runner.log_buffer.clear()
-            results = multi_gpu_test(
-                runner.model,
-                self.dataloader,
-                tmpdir=osp.join(runner.work_dir, '.eval_hook'),
-                gpu_collect=self.gpu_collect)
-            if runner.rank == 0:
-                print('\n')
-                self.evaluate(runner, results)
-
-        def after_train_epoch(self, runner):
-            """After train epoch hook."""
-            if not self.by_epoch or not self.every_n_epochs(
-                    runner, self.interval):
-                return
-            from mmseg.apis import multi_gpu_test
-            runner.log_buffer.clear()
-            results = multi_gpu_test(
-                runner.model,
-                self.dataloader,
-                tmpdir=osp.join(runner.work_dir, '.eval_hook'),
-                gpu_collect=self.gpu_collect)
-            if runner.rank == 0:
-                print('\n')
-                self.evaluate(runner, results)
-else:
+if use_mmcv_hook:
 
     class EvalHook(BasicEvalHook):
-
         greater_keys = ['mIoU', 'mAcc', 'aAcc']
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
 
     class DistEvalHook(BasicDistEvalHook):
-
         greater_keys = ['mIoU', 'mAcc', 'aAcc']
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+else:
+
+    class EvalHook(SegEvalHook):
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+    class DistEvalHook(SegDistEvalHook):
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
