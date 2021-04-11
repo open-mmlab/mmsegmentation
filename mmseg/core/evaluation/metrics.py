2cffeaf2c51465e8da8e53419d8dd1095a5b0b5e
sennnnn <201730271412@mail.scut.edu.cn>
2021-04-11 13:49:37 +0800

    Fix fence(IoU) = 0 when training on PascalContextDataset59;

--- mmseg/core/evaluation/metrics.py
+++ mmseg/core/evaluation/metrics.py
@@ -57,11 +57,11 @@ def intersect_and_union(pred_label,
 
     intersect = pred_label[pred_label == label]
     area_intersect = torch.histc(
-        intersect.float(), bins=(num_classes), min=0, max=num_classes)
+        intersect.float(), bins=(num_classes), min=0, max=num_classes - 1)
     area_pred_label = torch.histc(
-        pred_label.float(), bins=(num_classes), min=0, max=num_classes)
+        pred_label.float(), bins=(num_classes), min=0, max=num_classes - 1)
     area_label = torch.histc(
-        label.float(), bins=(num_classes), min=0, max=num_classes)
+        label.float(), bins=(num_classes), min=0, max=num_classes - 1)
     area_union = area_pred_label + area_label - area_intersect
     return area_intersect, area_union, area_pred_label, area_label
 
