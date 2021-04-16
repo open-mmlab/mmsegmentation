2de91d7f5fea2e3219bd7ceecc7b186645c29749
sennnnn <201730271412@mail.scut.edu.cn>
2021-04-16 11:00:30 +0800

    Add "fcn", "deeplabv3", "deeplabv3+", "pspnet" config file for model training based on PascalContextDataset59;
    Add some ignore items in ".gitignore";

--- configs/deeplabv3/deeplabv3_r50-d8_480x480_80k_pascal_context_59.py
+++ configs/deeplabv3/deeplabv3_r50-d8_480x480_80k_pascal_context_59.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../_base_/models/fcn_r50-d8.py',
+    '../_base_/models/deeplabv3_r50-d8.py',
     '../_base_/datasets/pascal_context_59.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py'
+    '../_base_/schedules/schedule_80k.py'
 ]
 model = dict(
     decode_head=dict(num_classes=59),
