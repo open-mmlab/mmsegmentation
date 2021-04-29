16e21cac52e470e0ff81e9c15f8091cf0d760581
sennnnn <201730271412@mail.scut.edu.cn>
2021-04-29 18:07:31 +0800

    Re-implement of SETR
    * Add two head -- SETRUPHead (Naive, PUP) & SETRMLAHead (MLA);

---
+++ mmseg/models/decode_heads/setr_mla_head.py
@@ -0,0 +1,202 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.cnn import build_norm_layer
+
+from ..builder import HEADS
+from .decode_head import BaseDecodeHead
+
+
+class MLAConv(nn.Module):
+
+    def __init__(self, in_channels=1024, mla_channels=256, norm_cfg=None):
+        super(MLAConv, self).__init__()
+        self.mla_p2_1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, mla_channels, 1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p3_1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, mla_channels, 1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p4_1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, mla_channels, 1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p5_1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, mla_channels, 1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p2 = nn.Sequential(
+            nn.Conv2d(mla_channels, mla_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p3 = nn.Sequential(
+            nn.Conv2d(mla_channels, mla_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p4 = nn.Sequential(
+            nn.Conv2d(mla_channels, mla_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+        self.mla_p5 = nn.Sequential(
+            nn.Conv2d(mla_channels, mla_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mla_channels)[1], nn.ReLU())
+
+    def to_2D(self, x):
+        n, hw, c = x.shape
+        h = w = int(math.sqrt(hw))
+        x = x.transpose(1, 2).reshape(n, c, h, w)
+        return x
+
+    def forward(self, res2, res3, res4, res5):
+
+        res2 = self.to_2D(res2)
+        res3 = self.to_2D(res3)
+        res4 = self.to_2D(res4)
+        res5 = self.to_2D(res5)
+
+        mla_p5_1x1 = self.mla_p5_1x1(res5)
+        mla_p4_1x1 = self.mla_p4_1x1(res4)
+        mla_p3_1x1 = self.mla_p3_1x1(res3)
+        mla_p2_1x1 = self.mla_p2_1x1(res2)
+
+        mla_p4_plus = mla_p5_1x1 + mla_p4_1x1
+        mla_p3_plus = mla_p4_plus + mla_p3_1x1
+        mla_p2_plus = mla_p3_plus + mla_p2_1x1
+
+        mla_p5 = self.mla_p5(mla_p5_1x1)
+        mla_p4 = self.mla_p4(mla_p4_plus)
+        mla_p3 = self.mla_p3(mla_p3_plus)
+        mla_p2 = self.mla_p2(mla_p2_plus)
+
+        return mla_p2, mla_p3, mla_p4, mla_p5
+
+
+class MLAHead(nn.Module):
+
+    def __init__(self, mla_channels=256, mlahead_channels=128, norm_cfg=None):
+        super(MLAHead, self).__init__()
+        self.head2 = nn.Sequential(
+            nn.Conv2d(
+                mla_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU(),
+            nn.Conv2d(
+                mlahead_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU())
+        self.head3 = nn.Sequential(
+            nn.Conv2d(
+                mla_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU(),
+            nn.Conv2d(
+                mlahead_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU())
+        self.head4 = nn.Sequential(
+            nn.Conv2d(
+                mla_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU(),
+            nn.Conv2d(
+                mlahead_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU())
+        self.head5 = nn.Sequential(
+            nn.Conv2d(
+                mla_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU(),
+            nn.Conv2d(
+                mlahead_channels, mlahead_channels, 3, padding=1, bias=False),
+            build_norm_layer(norm_cfg, mlahead_channels)[1], nn.ReLU())
+
+    def forward(self, mla_p2, mla_p3, mla_p4, mla_p5):
+        head2 = F.interpolate(
+            self.head2(mla_p2),
+            4 * mla_p2.shape[-1],
+            mode='bilinear',
+            align_corners=True)
+        head3 = F.interpolate(
+            self.head3(mla_p3),
+            4 * mla_p3.shape[-1],
+            mode='bilinear',
+            align_corners=True)
+        head4 = F.interpolate(
+            self.head4(mla_p4),
+            4 * mla_p4.shape[-1],
+            mode='bilinear',
+            align_corners=True)
+        head5 = F.interpolate(
+            self.head5(mla_p5),
+            4 * mla_p5.shape[-1],
+            mode='bilinear',
+            align_corners=True)
+        return torch.cat([head2, head3, head4, head5], dim=1)
+
+
+@HEADS.register_module()
+class SETRMLAHead(BaseDecodeHead):
+    """Vision Transformer with support for patch or hybrid CNN input stage.
+
+    MLA head of `SETR  <https://arxiv.org/pdf/2012.15840.pdf>`.
+
+    Args:
+        img_size (tuple): Input image size. Default: (224, 224).
+        embed_dim (int): embedding dimension. Default: 768.
+        norm_layer (dict): Config dict for input normalization.
+            Default: norm_layer=dict(type='LN', eps=1e-6, requires_grad=True).
+        mla_channels (int): Channels of reshape-conv of multi-level feature
+            aggregation. Default: 256.
+        mlahead_channels (int): Channels of conv-conv-4x of multi-level feature
+            aggregation. Default: 128.
+    """
+
+    def __init__(self,
+                 img_size=(224, 224),
+                 embed_dim=768,
+                 norm_layer=dict(type='LN', eps=1e-6, requires_grad=True),
+                 mla_channels=256,
+                 mlahead_channels=128,
+                 **kwargs):
+        super(SETRMLAHead, self).__init__(
+            input_transform='multiple_select', **kwargs)
+        self.img_size = img_size
+        self.embed_dim = embed_dim
+        self.mla_channels = mla_channels
+        self.mlahead_channels = mlahead_channels
+
+        self.norm = nn.ModuleList([
+            build_norm_layer(norm_layer, self.embed_dim)[1]
+            for i in range(len(self.in_index))
+        ])
+
+        self.mla = MLAConv(
+            in_channels=self.embed_dim,
+            mla_channels=self.mla_channels,
+            norm_cfg=self.norm_cfg)
+
+        self.mlahead = MLAHead(
+            mla_channels=self.mla_channels,
+            mlahead_channels=self.mlahead_channels,
+            norm_cfg=self.norm_cfg)
+        self.cls = nn.Conv2d(
+            4 * self.mlahead_channels, self.num_classes, 3, padding=1)
+
+    def forward(self, inputs):
+        inputs = self._transform_inputs(inputs)
+
+        # Convert from nchw to nlc
+        for i in range(len(inputs)):
+            x = inputs[i]
+            if x.dim() == 3:
+                x = self.norm[i](x)
+            elif x.dim() == 4:
+                n, c, h, w = x.shape
+                x = x.reshape(n, c, h * w).transpose(2, 1)
+                x = self.norm[i](x)
+            else:
+                raise NotImplementedError
+            inputs[i] = x
+
+        inputs = self.mla(*inputs)
+
+        inputs = self.mlahead(*inputs)
+        out = self.cls(inputs)
+
+        out = F.interpolate(
+            out,
+            size=self.img_size,
+            mode='bilinear',
+            align_corners=self.align_corners)
+        return out
---
+++ mmseg/models/decode_heads/setr_up_head.py
@@ -0,0 +1,173 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.cnn import build_norm_layer
+
+from ..builder import HEADS
+from ..utils import trunc_normal_
+from .decode_head import BaseDecodeHead
+
+
+@HEADS.register_module()
+class SETRUPHead(BaseDecodeHead):
+    """Vision Transformer with support for patch or hybrid CNN input stage.
+
+    Naive or PUP head of `SETR  <https://arxiv.org/pdf/2012.15840.pdf>`.
+
+    Args:
+        img_size (tuple): Input image size. Default: (224, 224).
+        embed_dim (int): embedding dimension. Default: 768.
+        norm_layer (dict): Config dict for input normalization.
+            Default: norm_layer=dict(type='LN', eps=1e-6, requires_grad=True).
+        num_conv (int): Number of decoder convolutions. Default: 1.
+        up_mode (str): Interpolate mode of upsampling. Default: bilinear.
+        num_up_layer (str): Nunber of upsampling layers. Default: 1.
+        conv3x3_conv1x1 (bool): Naive head use conv1x1_conv1x1, PUP head use
+            conv3x3_conv1x1. Default: True.
+    """
+
+    def __init__(self,
+                 img_size=(224, 224),
+                 embed_dim=768,
+                 norm_layer=dict(type='LN', eps=1e-6, requires_grad=True),
+                 num_conv=1,
+                 up_mode='bilinear',
+                 num_up_layer=1,
+                 conv3x3_conv1x1=True,
+                 **kwargs):
+        super(SETRUPHead, self).__init__(**kwargs)
+
+        if isinstance(img_size, int):
+            self.img_size = (img_size, img_size)
+        elif isinstance(img_size, tuple):
+            self.img_size = img_size
+        else:
+            raise TypeError('img_size must be type of int or tuple')
+
+        self.num_conv = num_conv
+        _, self.norm = build_norm_layer(norm_layer, embed_dim)
+        self.up_mode = up_mode
+        self.num_up_layer = num_up_layer
+        self.conv3x3_conv1x1 = conv3x3_conv1x1
+
+        out_channel = self.num_classes
+
+        if self.num_conv == 2:
+            if self.conv3x3_conv1x1:
+                self.conv_0 = nn.Conv2d(
+                    embed_dim, 256, kernel_size=3, stride=1, padding=1)
+            else:
+                self.conv_0 = nn.Conv2d(embed_dim, 256, 1, 1)
+            self.conv_1 = nn.Conv2d(256, out_channel, 1, 1)
+            _, self.unified_bn_fc_0 = build_norm_layer(self.norm_cfg, 256)
+
+        elif self.num_conv == 4:
+            self.conv_0 = nn.Conv2d(
+                embed_dim, 256, kernel_size=3, stride=1, padding=1)
+            self.conv_1 = nn.Conv2d(
+                256, 256, kernel_size=3, stride=1, padding=1)
+            self.conv_2 = nn.Conv2d(
+                256, 256, kernel_size=3, stride=1, padding=1)
+            self.conv_3 = nn.Conv2d(
+                256, 256, kernel_size=3, stride=1, padding=1)
+            self.conv_4 = nn.Conv2d(256, out_channel, kernel_size=1, stride=1)
+
+        else:
+            raise NotImplementedError
+
+            _, self.unified_bn_fc_0 = build_norm_layer(self.norm_cfg, 256)
+            _, self.unified_bn_fc_1 = build_norm_layer(self.norm_cfg, 256)
+            _, self.unified_bn_fc_2 = build_norm_layer(self.norm_cfg, 256)
+            _, self.unified_bn_fc_3 = build_norm_layer(self.norm_cfg, 256)
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x):
+        x = self._transform_inputs(x)
+
+        if x.dim() == 3:
+            x = self.norm(x)
+        elif x.dim() == 4:
+            n, c, h, w = x.shape
+            x = x.reshape(n, c, h * w).transpose(2, 1)
+            x = self.norm(x)
+            x = x.transpose(1, 2).reshape(n, c, h, w)
+        else:
+            raise NotImplementedError
+
+        if self.num_conv == 2:
+            if self.num_up_layer == 1:
+                x = self.conv_0(x)
+                x = self.unified_bn_fc_0(x)
+                x = F.relu(x, inplace=True)
+                x = self.conv_1(x)
+                x = F.interpolate(
+                    x,
+                    size=self.img_size,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+            elif self.num_up_layer == 2:
+                x = self.conv_0(x)
+                x = self.unified_bn_fc_0(x)
+                x = F.relu(x, inplace=True)
+                x = F.interpolate(
+                    x,
+                    size=x.shape[-1] * 4,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+                x = self.conv_1(x)
+                x = F.interpolate(
+                    x,
+                    size=self.img_size,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+            else:
+                raise NotImplementedError
+        elif self.num_conv == 4:
+            if self.num_up_layer == 4:
+                x = self.conv_0(x)
+                x = self.unified_bn_fc_0(x)
+                x = F.relu(x, inplace=True)
+                x = F.interpolate(
+                    x,
+                    size=x.shape[-1] * 2,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+                x = self.conv_1(x)
+                x = self.unified_bn_fc_1(x)
+                x = F.relu(x, inplace=True)
+                x = F.interpolate(
+                    x,
+                    size=x.shape[-1] * 2,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+                x = self.conv_2(x)
+                x = self.unified_bn_fc_2(x)
+                x = F.relu(x, inplace=True)
+                x = F.interpolate(
+                    x,
+                    size=x.shape[-1] * 2,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+                x = self.conv_3(x)
+                x = self.unified_bn_fc_3(x)
+                x = F.relu(x, inplace=True)
+                x = self.conv_4(x)
+                x = F.interpolate(
+                    x,
+                    size=x.shape[-1] * 2,
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+            else:
+                raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+        return x
