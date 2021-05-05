27d1479b8f8904aa17ce848365595f7f55aabcdd
sennnnn <201730271412@mail.scut.edu.cn>
2021-05-05 22:28:32 +0800

    Fix some learning schedule bug;

--- configs/setr/setr_naive_768x768_40k_cityscapes_bs_8.py
+++ configs/setr/setr_naive_768x768_40k_cityscapes_bs_8.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/setr_naive.py',
     '../_base_/datasets/cityscapes_768x768.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_80k.py'
+    '../_base_/schedules/schedule_40k.py'
 ]
 model = dict(
     backbone=dict(drop_rate=0.),
--- configs/setr/setr_pup_768x768_40k_cityscapes_bs_8.py
+++ configs/setr/setr_pup_768x768_40k_cityscapes_bs_8.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/setr_pup.py', '../_base_/datasets/cityscapes_768x768.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
 model = dict(
     backbone=dict(drop_rate=0.),
