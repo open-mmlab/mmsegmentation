[fix] fix the bug of large seg map IoU calculation

mmseg/core/evaluation/metrics.py
@@ -75,12 +75,9 @@ def intersect_and_union(pred_label,
     label = label[mask]
 
     intersect = pred_label[pred_label == label]
-    area_intersect = torch.histc(
-        intersect.float(), bins=(num_classes), min=0, max=num_classes - 1)
-    area_pred_label = torch.histc(
-        pred_label.float(), bins=(num_classes), min=0, max=num_classes - 1)
-    area_label = torch.histc(
-        label.float(), bins=(num_classes), min=0, max=num_classes - 1)
+    area_intersect = torch.bincount(intersect, minlength=num_classes)
+    area_pred_label = torch.bincount(pred_label, minlength=num_classes)
+    area_label = torch.bincount(label, minlength=num_classes)
     area_union = area_pred_label + area_label - area_intersect
     return area_intersect, area_union, area_pred_label, area_label
 