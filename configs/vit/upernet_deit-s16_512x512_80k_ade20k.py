656c1676e7d0628103de20f7eb2755427291fc78
xiexinch <xinchen.xie@qq.com>
2021-05-18 15:30:44 +0800

    fix import error

--- configs/vit/upernet_deit-s16_512x512_80k_ade20k.py
+++ configs/vit/upernet_deit-s16_512x512_80k_ade20k.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/upernet_vit-d16.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/upernet_vit-b16.py', '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 
