949cb65dddbcebf8e2253ea5cf179af5c6a384a2
sennnnn <201730271412@mail.scut.edu.cn>
2021-05-21 15:13:48 +0800

    [Refactor]SETR Naive Head and SETR PUP Head

--- mmseg/models/decode_heads/setr_up_head.py
+++ mmseg/models/decode_heads/setr_up_head.py
@@ -1,10 +1,65 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.cnn import build_norm_layer
+from mmcv.cnn import ConvModule, build_norm_layer
 
 from ..builder import HEADS
 from ..utils import trunc_normal_
 from .decode_head import BaseDecodeHead
+
+
+class UPModule(nn.ModuleList):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels=256,
+                 num_convs=1,
+                 up_mode='bilinear',
+                 up_scale=4,
+                 align_corners=False,
+                 num_up_layer=0,
+                 kernel_size=3,
+                 norm_cfg=None,
+                 act_cfg=dict(type='ReLU')):
+        super(UPModule, self).__init__()
+        self.num_convs = num_convs
+        self.up_mode = up_mode
+        self.up_scale = up_scale
+        self.align_corners = align_corners
+        self.num_up_layer = num_up_layer
+        self.kernel_size = kernel_size
+
+        for i in range(num_convs):
+            if i == 0:
+                tmp_in_channels = in_channels
+                tmp_out_channels = out_channels
+            else:
+                tmp_in_channels = out_channels
+                tmp_out_channels = out_channels
+            self.append(
+                ConvModule(
+                    in_channels=tmp_in_channels,
+                    out_channels=tmp_out_channels,
+                    kernel_size=kernel_size,
+                    stride=1,
+                    padding=int(kernel_size - 1) // 2,
+                    norm_cfg=norm_cfg,
+                    act_cfg=act_cfg))
+
+    def forward(self, x):
+        # There is an upsample defined in segmentor class..
+        up_rest_count = self.num_up_layer
+        for op in self:
+            x = op(x)
+            if up_rest_count > 0:
+                x = F.interpolate(
+                    x,
+                    size=(x.shape[-2] * self.up_scale,
+                          x.shape[-1] * self.up_scale),
+                    mode=self.up_mode,
+                    align_corners=self.align_corners)
+                up_rest_count -= 1
+
+        return x
 
 
 @HEADS.register_module()
@@ -19,66 +74,39 @@ class SETRUPHead(BaseDecodeHead):
             Default: norm_layer=dict(type='LN', eps=1e-6, requires_grad=True).
         num_convs (int): Number of decoder convolutions. Default: 1.
         up_mode (str): Interpolate mode of upsampling. Default: bilinear.
+        up_scale (int): The scale factor of interpolate. Default:4.
         num_up_layer (str): Nunber of upsampling layers. Default: 1.
         kernel_size (int): The kernel size of convolution when decoding
             feature information from backbone. Default: 3.
     """
 
     def __init__(self,
-                 embed_dim=1024,
                  norm_layer=dict(type='LN', eps=1e-6, requires_grad=True),
-                 num_convs=2,
+                 num_convs=1,
                  up_mode='bilinear',
-                 num_up_layer=1,
+                 up_scale=4,
+                 num_up_layer=0,
                  kernel_size=3,
                  **kwargs):
-
-        assert num_convs in [
-            2, 4
-        ], 'naive head (num_convs = 2), pup head (num_convs=4).'
 
         assert kernel_size in [1, 3], 'kernel_size must be 1 or 3.'
 
         super(SETRUPHead, self).__init__(**kwargs)
 
         assert isinstance(self.in_channels, int)
-        assert embed_dim == self.in_channels
 
-        self.num_convs = num_convs
-        _, self.norm = build_norm_layer(norm_layer, embed_dim)
-        self.up_mode = up_mode
-        self.num_up_layer = num_up_layer
-        self.kernel_size = kernel_size
+        _, self.norm = build_norm_layer(norm_layer, self.in_channels)
 
-        out_channel = self.num_classes
-
-        if self.num_convs == 2:
-            self.conv_0 = nn.Conv2d(
-                embed_dim,
-                256,
-                kernel_size=kernel_size,
-                stride=1,
-                padding=int(kernel_size - 1) // 2)
-            self.conv_seg = nn.Conv2d(
-                256, out_channel, kernel_size=1, stride=1)
-            _, self.unified_bn_fc_0 = build_norm_layer(self.norm_cfg, 256)
-
-        elif self.num_convs == 4:
-            self.conv_0 = nn.Conv2d(
-                embed_dim, 256, kernel_size=kernel_size, stride=1, padding=1)
-            self.conv_1 = nn.Conv2d(
-                256, 256, kernel_size=kernel_size, stride=1, padding=1)
-            self.conv_2 = nn.Conv2d(
-                256, 256, kernel_size=kernel_size, stride=1, padding=1)
-            self.conv_3 = nn.Conv2d(
-                256, 256, kernel_size=kernel_size, stride=1, padding=1)
-            self.conv_seg = nn.Conv2d(
-                256, out_channel, kernel_size=1, stride=1)
-
-            _, self.unified_bn_fc_0 = build_norm_layer(self.norm_cfg, 256)
-            _, self.unified_bn_fc_1 = build_norm_layer(self.norm_cfg, 256)
-            _, self.unified_bn_fc_2 = build_norm_layer(self.norm_cfg, 256)
-            _, self.unified_bn_fc_3 = build_norm_layer(self.norm_cfg, 256)
+        self.up = UPModule(
+            in_channels=self.in_channels,
+            out_channels=self.channels,
+            num_convs=num_convs,
+            up_mode=up_mode,
+            up_scale=up_scale,
+            align_corners=self.align_corners,
+            num_up_layer=num_up_layer,
+            norm_cfg=self.norm_cfg,
+            act_cfg=self.act_cfg)
 
     def init_weights(self):
         for m in self.modules():
@@ -98,55 +126,6 @@ class SETRUPHead(BaseDecodeHead):
         x = self.norm(x)
         x = x.transpose(1, 2).reshape(n, c, h, w)
 
-        if self.num_convs == 2:
-            if self.num_up_layer == 1:
-                x = self.conv_0(x)
-                x = self.unified_bn_fc_0(x)
-                x = F.relu(x, inplace=True)
-                x = self.conv_seg(x)
-            elif self.num_up_layer == 2:
-                x = self.conv_0(x)
-                x = self.unified_bn_fc_0(x)
-                x = F.relu(x, inplace=True)
-                x = F.interpolate(
-                    x,
-                    size=(x.shape[-2] * 4, x.shape[-1] * 4),
-                    mode=self.up_mode,
-                    align_corners=self.align_corners)
-                x = self.conv_seg(x)
-            else:
-                raise NotImplementedError
-        elif self.num_convs == 4:
-            if self.num_up_layer == 4:
-                x = self.conv_0(x)
-                x = self.unified_bn_fc_0(x)
-                x = F.relu(x, inplace=True)
-                x = F.interpolate(
-                    x,
-                    size=(x.shape[-2] * 2, x.shape[-1] * 2),
-                    mode=self.up_mode,
-                    align_corners=self.align_corners)
-                x = self.conv_1(x)
-                x = self.unified_bn_fc_1(x)
-                x = F.relu(x, inplace=True)
-                x = F.interpolate(
-                    x,
-                    size=(x.shape[-2] * 2, x.shape[-1] * 2),
-                    mode=self.up_mode,
-                    align_corners=self.align_corners)
-                x = self.conv_2(x)
-                x = self.unified_bn_fc_2(x)
-                x = F.relu(x, inplace=True)
-                x = F.interpolate(
-                    x,
-                    size=(x.shape[-2] * 2, x.shape[-1] * 2),
-                    mode=self.up_mode,
-                    align_corners=self.align_corners)
-                x = self.conv_3(x)
-                x = self.unified_bn_fc_3(x)
-                x = F.relu(x, inplace=True)
-                x = self.conv_seg(x)
-            else:
-                raise NotImplementedError
-
-        return x
+        x = self.up(x)
+        out = self.cls_seg(x)
+        return out
--- tests/test_models/test_heads/test_setr_up_head.py
+++ tests/test_models/test_heads/test_setr_up_head.py
@@ -8,57 +8,18 @@ from .utils import to_cuda
 def test_setr_up_head(capsys):
 
     with pytest.raises(AssertionError):
-        # num_convs must be [2/4]
-        SETRUPHead(num_classes=19, num_convs=-1)
-
-    with pytest.raises(AssertionError):
         # kernel_size must be [1/3]
         SETRUPHead(num_classes=19, kernel_size=2)
-
-    with pytest.raises(TypeError):
-        # unutilizing ConvModule, so there is not auto norm layer.
-        SETRUPHead(in_channels=32, channels=16, embed_dim=32, num_classes=19)
 
     with pytest.raises(AssertionError):
         # in_channels must be int type and in_channels must be same
         # as embed_dim.
-        SETRUPHead(
-            in_channels=(32, 32), channels=16, embed_dim=32, num_classes=19)
-        SETRUPHead(in_channels=24, channels=16, embed_dim=32, num_classes=19)
-
-    with pytest.raises(NotImplementedError):
-        x = [torch.randn(1, 32, 4, 4)]
-        # PUP head
-        # when num_convs=4 , num_up_layer must be 4.
-        head = SETRUPHead(
-            in_channels=32,
-            channels=16,
-            embed_dim=32,
-            num_classes=19,
-            num_convs=4,
-            num_up_layer=1,
-            norm_cfg=dict(type='SyncBN'))
-        head(x)
-
-    with pytest.raises(NotImplementedError):
-        x = [torch.randn(1, 32, 4, 4)]
-        # Naive head
-        # when num_convs=2, num_up_layer can be [1/2].
-        head = SETRUPHead(
-            in_channels=32,
-            channels=16,
-            num_classes=19,
-            embed_dim=32,
-            num_convs=2,
-            num_up_layer=4,
-            norm_cfg=dict(type='SyncBN'))
-        head(x)
+        SETRUPHead(in_channels=(32, 32), channels=16, num_classes=19)
 
     # test init_weights of head
     head = SETRUPHead(
         in_channels=32,
         channels=16,
-        embed_dim=32,
         norm_cfg=dict(type='SyncBN'),
         num_classes=19)
     head.init_weights()
@@ -71,9 +32,9 @@ def test_setr_up_head(capsys):
         in_channels=32,
         channels=16,
         num_classes=19,
-        embed_dim=32,
-        num_convs=2,
-        num_up_layer=1,
+        num_convs=1,
+        up_scale=4,
+        num_up_layer=0,
         kernel_size=1,
         norm_cfg=dict(type='BN'))
 
@@ -100,9 +61,9 @@ def test_setr_up_head(capsys):
         in_channels=32,
         channels=16,
         num_classes=19,
-        embed_dim=32,
         num_convs=4,
-        num_up_layer=4,
+        up_scale=2,
+        num_up_layer=3,
         kernel_size=3,
         norm_cfg=dict(type='BN'))
 
@@ -128,9 +89,9 @@ def test_setr_up_head(capsys):
         in_channels=32,
         channels=16,
         num_classes=19,
-        embed_dim=32,
-        num_convs=2,
-        num_up_layer=2,
+        num_convs=1,
+        up_scale=4,
+        num_up_layer=1,
         kernel_size=3,
         norm_cfg=dict(type='BN'))
 
