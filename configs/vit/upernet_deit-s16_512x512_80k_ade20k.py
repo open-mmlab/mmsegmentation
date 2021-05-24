69cb384307e43ca849f7f32417a717e179b21a5d
xiexinch <xinchen.xie@qq.com>
2021-05-24 15:18:30 +0800

    remove neck

--- configs/vit/upernet_deit-s16_512x512_80k_ade20k.py
+++ configs/vit/upernet_deit-s16_512x512_80k_ade20k.py
@@ -6,7 +6,6 @@ _base_ = [
 model = dict(
     pretrained='https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth',  # noqa
     backbone=dict(num_heads=6, embed_dim=384, drop_path_rate=0.1),
-    neck=dict(in_channels=[384], out_channels=384),
     decode_head=dict(num_classes=150, in_channels=[384, 384, 384, 384]),
     auxiliary_head=dict(num_classes=150, in_channels=384))  # yapf: disable
 
