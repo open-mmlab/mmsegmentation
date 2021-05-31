4ce41524d28cb66c37229448e8ca1ade00d95ac0
liyinhao <liyinhao0413@qq.com>
2021-05-31 10:31:15 +0800

    Update meta to fix best model can not test bug

--- mmseg/core/evaluation/eval_hooks.py
+++ mmseg/core/evaluation/eval_hooks.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 
 from mmcv.runner import DistEvalHook as _DistEvalHook
@@ -51,9 +52,45 @@ class EvalHook(_EvalHook):
         from mmseg.apis import single_gpu_test
         runner.log_buffer.clear()
         results = single_gpu_test(runner.model, self.dataloader, show=False)
-        key_score = self.evaluate(runner, results)
-        if self.save_best is not None:
-            self._save_ckpt(runner, key_score)
+        self.evaluate(runner, results)
+
+    def _save_ckpt(self, runner, key_score):
+        """Save the best checkpoint.
+
+        It will compare the score according to the compare function, write
+        related information (best score, best checkpoint path) and save the
+        best checkpoint into ``work_dir``.
+        """
+        if self.by_epoch:
+            current = f'epoch_{runner.epoch + 1}'
+            cur_type, cur_time = 'epoch', runner.epoch + 1
+        else:
+            current = f'iter_{runner.iter + 1}'
+            cur_type, cur_time = 'iter', runner.iter + 1
+
+        best_score = runner.meta['hook_msgs'].get(
+            'best_score', self.init_value_map[self.rule])
+        if self.compare_func(key_score, best_score):
+            best_score = key_score
+            runner.meta['hook_msgs']['best_score'] = best_score
+
+            if self.best_ckpt_path and osp.isfile(self.best_ckpt_path):
+                os.remove(self.best_ckpt_path)
+
+            best_ckpt_name = f'best_{self.key_indicator}_{current}.pth'
+            self.best_ckpt_path = osp.join(runner.work_dir, best_ckpt_name)
+            runner.meta['hook_msgs']['best_ckpt'] = self.best_ckpt_path
+
+            runner.save_checkpoint(
+                runner.work_dir,
+                best_ckpt_name,
+                meta=runner.meta,
+                create_symlink=False)
+            runner.logger.info(
+                f'Now best checkpoint is saved as {best_ckpt_name}.')
+            runner.logger.info(
+                f'Best {self.key_indicator} is {best_score:0.4f} '
+                f'at {cur_time} {cur_type}.')
 
 
 class DistEvalHook(_DistEvalHook):
@@ -92,9 +129,7 @@ class DistEvalHook(_DistEvalHook):
             efficient_test=self.efficient_test)
         if runner.rank == 0:
             print('\n')
-            key_score = self.evaluate(runner, results)
-            if self.save_best is not None:
-                self._save_ckpt(runner, key_score)
+            self.evaluate(runner, results)
 
     def after_train_epoch(self, runner):
         """After train epoch hook.
@@ -112,6 +147,4 @@ class DistEvalHook(_DistEvalHook):
             gpu_collect=self.gpu_collect)
         if runner.rank == 0:
             print('\n')
-            key_score = self.evaluate(runner, results)
-            if self.save_best is not None:
-                self._save_ckpt(runner, key_score)
+            self.evaluate(runner, results)
--- tools/train.py
+++ tools/train.py
@@ -149,6 +149,7 @@ def main():
             PALETTE=datasets[0].PALETTE)
     # add an attribute for visualization convenience
     model.CLASSES = datasets[0].CLASSES
+    meta.update(cfg.checkpoint_config.meta)
     train_segmentor(
         model,
         datasets,
