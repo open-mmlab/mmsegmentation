cabd80f4682c14383d1c15a8d0c2859a7c0dfac2
Jiarui XU <xvjiarui0826@gmail.com>
2021-05-31 19:33:31 -0700

    remove redundent

--- tools/train.py
+++ tools/train.py
@@ -149,7 +149,6 @@ def main():
             PALETTE=datasets[0].PALETTE)
     # add an attribute for visualization convenience
     model.CLASSES = datasets[0].CLASSES
-    meta.update(cfg.checkpoint_config.meta)
     train_segmentor(
         model,
         datasets,
