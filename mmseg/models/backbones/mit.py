99d7308626cc9fcb893bba13e2ab2bc10d9d0eee
sennnnn <201730271412@mail.scut.edu.cn>
2021-06-20 11:36:25 +0800

    1. Refactor segformer backbone parameters init;
    2. Remove rebundant functions and unit tests;

--- mmseg/models/backbones/mit.py
+++ mmseg/models/backbones/mit.py
@@ -3,18 +3,20 @@ import math
 import torch
 import torch.nn as nn
 from mmcv.cnn import (ConvModule, Linear, build_activation_layer,
-                      build_norm_layer)
+                      build_norm_layer, constant_init, normal_init,
+                      trunc_normal_init)
+from mmcv.cnn.bricks import DropPath
 from mmcv.runner import BaseModule, ModuleList, load_checkpoint
+from torch.nn.modules.utils import _pair as to_2tuple
 
 from ...utils import get_root_logger
 from ..builder import BACKBONES
-from ..utils import DropPath, to_2tuple, trunc_normal_
 
 
 class DWConv(BaseModule):
 
     def __init__(self, dim=768):
-        super(DWConv, self).__init__()
+        super().__init__()
         self.dwconv = ConvModule(
             in_channels=dim,
             out_channels=dim,
@@ -26,9 +28,9 @@ class DWConv(BaseModule):
 
     def forward(self, x, H, W):
         B, _, C = x.shape
-        x = x.transpose(1, 2).view(B, C, H, W)
+        x = x.transpose(1, 2).reshape(B, C, H, W)
         x = self.dwconv(x)
-        x = x.flatten(2).transpose(1, 2)
+        x = x.flatten(2).transpose(1, 2).contiguous()
 
         return x
 
@@ -36,41 +38,23 @@ class DWConv(BaseModule):
 class Mlp(BaseModule):
 
     def __init__(self,
-                 in_features,
-                 hidden_features=None,
-                 out_features=None,
+                 embed_dims,
+                 feedforward_channels,
                  act_cfg=dict(type='GELU'),
-                 drop=0.):
+                 drop_rate=0.):
         super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = Linear(in_features, hidden_features)
-        self.dwconv = DWConv(hidden_features)
-        self.act = build_activation_layer(act_cfg)
-        self.fc2 = Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if isinstance(m, nn.Linear) and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
-            elif isinstance(m, nn.Conv2d):
-                fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                fan_out //= m.groups
-                m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
-                if m.bias is not None:
-                    m.bias.data.zero_()
+        in_channels = embed_dims
+        self.fc1 = Linear(in_channels, feedforward_channels)
+        self.dwconv = DWConv(feedforward_channels)
+        self.act = build_activation_layer(act_cfg)
+        self.fc2 = Linear(feedforward_channels, in_channels)
+        self.drop = nn.Dropout(drop_rate)
 
     def forward(self, x, H, W):
         x = self.fc1(x)
         x = self.dwconv(x, H, W)
         x = self.act(x)
-        x = self.drop(x)
         x = self.fc2(x)
         x = self.drop(x)
         return x
@@ -111,36 +95,20 @@ class Attention(BaseModule):
                 stride=sr_ratio)
             _, self.norm = build_norm_layer(norm_cfg, dim)
 
-    def init_weights(self):
-        for m in self.modules:
-            if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if isinstance(m, nn.Linear) and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
-            elif isinstance(m, nn.Conv2d):
-                fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                fan_out //= m.groups
-                m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-
     def forward(self, x, H, W):
         B, N, C = x.shape
         q = self.q(x).reshape(B, N, self.num_heads,
                               C // self.num_heads).permute(0, 2, 1, 3)
 
         if self.sr_ratio > 1:
-            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
-            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
-            x_ = self.norm(x_)
-            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads,
-                                     C // self.num_heads).permute(
-                                         2, 0, 3, 1, 4)
+            x = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x = self.sr(x).reshape(B, C, -1).permute(0, 2, 1)
+            x = self.norm(x)
+            kv = self.kv(x).reshape(B, N, 2, self.num_heads,
+                                    C // self.num_heads).permute(
+                                        2, 0, 3, 1, 4)
         else:
-            kv = self.kv(x).reshape(B, -1, 2, self.num_heads,
+            kv = self.kv(x).reshape(B, N, 2, self.num_heads,
                                     C // self.num_heads).permute(
                                         2, 0, 3, 1, 4)
         k, v = kv[0], kv[1]
@@ -164,9 +132,9 @@ class Block(BaseModule):
                  mlp_ratio=4.,
                  qkv_bias=False,
                  qk_scale=None,
-                 drop=0.,
-                 attn_drop=0.,
-                 drop_path=0.,
+                 drop_rate=0.,
+                 attn_drop_rate=0.,
+                 drop_path_rate=0.,
                  act_cfg=dict(type='GELU'),
                  norm_cfg=dict(type='LN'),
                  sr_ratio=1):
@@ -177,36 +145,36 @@ class Block(BaseModule):
             num_heads=num_heads,
             qkv_bias=qkv_bias,
             qk_scale=qk_scale,
-            attn_drop=attn_drop,
-            proj_drop=drop,
+            attn_drop=attn_drop_rate,
+            proj_drop=drop_rate,
             sr_ratio=sr_ratio)
         # NOTE: drop path for stochastic depth, we shall see if this is better
         # than dropout here
         self.drop_path = DropPath(
-            drop_path) if drop_path > 0. else nn.Identity()
+            drop_path_rate) if drop_path_rate > 0. else nn.Identity()
         _, self.norm2 = build_norm_layer(norm_cfg, dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(
-            in_features=dim,
-            hidden_features=mlp_hidden_dim,
+            embed_dims=dim,
+            feedforward_channels=mlp_hidden_dim,
             act_cfg=act_cfg,
-            drop=drop)
+            drop_rate=drop_rate)
 
     def init_weights(self):
         for m in self.modules:
             if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if isinstance(m, nn.Linear) and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
+                trunc_normal_init(m.weight, std=.02)
+                if m.bias is not None:
+                    constant_init(m.bias, 0)
             elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
+                constant_init(m.bias, 0)
+                constant_init(m.weight, 1.0)
             elif isinstance(m, nn.Conv2d):
                 fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 fan_out //= m.groups
-                m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+                normal_init(m.weight, 0, math.sqrt(2.0 / fan_out))
                 if m.bias is not None:
-                    m.bias.data.zero_()
+                    constant_init(m.bias, 0)
 
     def forward(self, x, H, W):
         x = x + self.drop_path(self.attn(self.norm1(x), H, W))
@@ -230,9 +198,9 @@ class OverlapPatchEmbed(BaseModule):
 
         self.img_size = img_size
         self.patch_size = patch_size
-        self.H, self.W = img_size[0] // patch_size[0], img_size[
+        num_rows, num_cols = img_size[0] // patch_size[0], img_size[
             1] // patch_size[1]
-        self.num_patches = self.H * self.W
+        self.num_patches = num_rows * num_cols
         self.proj = nn.Conv2d(
             in_chans,
             embed_dim,
@@ -240,22 +208,6 @@ class OverlapPatchEmbed(BaseModule):
             stride=stride,
             padding=(patch_size[0] // 2, patch_size[1] // 2))
         self.norm = nn.LayerNorm(embed_dim)
-
-    def init_weights(self):
-        for m in self.modules:
-            if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if isinstance(m, nn.Linear) and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
-            elif isinstance(m, nn.Conv2d):
-                fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                fan_out //= m.groups
-                m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
-                if m.bias is not None:
-                    m.bias.data.zero_()
 
     def forward(self, x):
         x = self.proj(x)
@@ -390,22 +342,22 @@ class MixVisionTransformer(BaseModule):
         _, self.norm4 = build_norm_layer(norm_cfg, embed_dims[3])
 
     def init_weights(self):
-        if isinstance(self.pretrained, type(None)):
+        if self.pretrained is None:
             for m in self.modules:
                 if isinstance(m, nn.Linear):
-                    trunc_normal_(m.weight, std=.02)
-                    if isinstance(m, nn.Linear) and m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
+                    trunc_normal_init(m.weight, std=.02)
+                    if m.bias is not None:
+                        constant_init(m.bias, 0)
                 elif isinstance(m, nn.LayerNorm):
-                    nn.init.constant_(m.bias, 0)
-                    nn.init.constant_(m.weight, 1.0)
+                    constant_init(m.bias, 0)
+                    constant_init(m.weight, 1.0)
                 elif isinstance(m, nn.Conv2d):
                     fan_out = m.kernel_size[0] * m.kernel_size[
                         1] * m.out_channels
                     fan_out //= m.groups
-                    m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+                    normal_init(m.weight, 0, math.sqrt(2.0 / fan_out))
                     if m.bias is not None:
-                        m.bias.data.zero_()
+                        constant_init(m.bias)
         elif isinstance(self.pretrained, str):
             logger = get_root_logger()
             load_checkpoint(
