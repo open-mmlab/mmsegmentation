6365d168157b8c66f598923e04c18335d2840a9d
sennnnn <201730271412@mail.scut.edu.cn>
2021-06-20 22:34:54 +0800

    Use nn.ModuleList to refactor segformer head.

--- mmseg/models/decode_heads/segformer_head.py
+++ mmseg/models/decode_heads/segformer_head.py
@@ -1,91 +1,60 @@
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
 from mmcv.cnn import ConvModule
 
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
+from mmseg.ops import resize
 
 
 @HEADS.register_module()
 class SegFormerHead(BaseDecodeHead):
-    """The MLP Head of segformer."""
+    """The MLP Head of segformer.
+
+    Args:
+        interpolate_mode: The interpolate mode of MLP head upsample operation.
+            Default: 'bilinear'.
+    """
 
     def __init__(self, interpolate_mode='bilinear', **kwargs):
         super(SegFormerHead, self).__init__(
             input_transform='multiple_select', **kwargs)
+
         self.interpolate_mode = interpolate_mode
 
-        embed_dim = self.channels
+        num_inputs = len(self.in_channels)
 
-        self.conv_c4 = ConvModule(
-            in_channels=self.in_channels[-1],
-            out_channels=embed_dim,
-            kernel_size=1,
-            stride=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=self.act_cfg)
-        self.conv_c3 = ConvModule(
-            in_channels=self.in_channels[-2],
-            out_channels=embed_dim,
-            kernel_size=1,
-            stride=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=self.act_cfg)
-        self.conv_c2 = ConvModule(
-            in_channels=self.in_channels[-3],
-            out_channels=embed_dim,
-            kernel_size=1,
-            stride=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=self.act_cfg)
-        self.conv_c1 = ConvModule(
-            in_channels=self.in_channels[-4],
-            out_channels=embed_dim,
-            kernel_size=1,
-            stride=1,
-            norm_cfg=self.norm_cfg,
-            act_cfg=self.act_cfg)
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
 
         self.conv_fuse = ConvModule(
-            in_channels=embed_dim * 4,
-            out_channels=embed_dim,
+            in_channels=self.channels * num_inputs,
+            out_channels=self.channels,
             kernel_size=1,
             norm_cfg=self.norm_cfg)
 
     def forward(self, inputs):
         # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
-        x = self._transform_inputs(inputs)
-        c1, c2, c3, c4 = x
+        inputs = self._transform_inputs(inputs)
+        outs = []
+        for x, conv in zip(inputs, self.convs):
+            outs.append(
+                resize(
+                    input=conv(x),
+                    size=inputs[0].shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners))
 
-        # MLP decoder on C1-C4
-        n, _, h, w = c1.shape
+        out = self.conv_fuse(torch.cat(outs, dim=1))
 
-        out_c4 = self.conv_c4(c4)
-        out_c4 = F.interpolate(
-            out_c4,
-            size=(h, w),
-            mode=self.mode,
-            align_corners=self.align_corners)
+        out = self.cls_seg(out)
 
-        out_c3 = self.conv_c3(c3)
-        out_c3 = F.interpolate(
-            out_c3,
-            size=(h, w),
-            mode=self.mode,
-            align_corners=self.align_corners)
-
-        out_c2 = self.conv_c2(c2)
-        out_c2 = F.interpolate(
-            out_c2,
-            size=(h, w),
-            mode=self.mode,
-            align_corners=self.align_corners)
-
-        out_c1 = self.conv_c1(c1)
-
-        out = self.conv_fuse(
-            torch.cat([out_c4, out_c3, out_c2, out_c1], dim=1))
-
-        x = self.cls_seg(out)
-
-        return x
+        return out
