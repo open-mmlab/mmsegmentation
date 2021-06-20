a6b02ee0c4115ad2291d3d9909499bf72b220a53
sennnnn <201730271412@mail.scut.edu.cn>
2021-06-20 21:56:38 +0800

    Replace Linear Layer to 1X1 Conv

--- mmseg/models/decode_heads/segformer_head.py
+++ mmseg/models/decode_heads/segformer_head.py
@@ -1,91 +1,91 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import ConvModule
 
-from ..builder import HEADS
-from .decode_head import BaseDecodeHead
-
-
-class MLP(nn.Module):
-    """Linear Embedding."""
-
-    def __init__(self, input_dim=2048, embed_dim=768):
-        super().__init__()
-        self.proj = nn.Linear(input_dim, embed_dim)
-
-    def forward(self, x):
-        x = x.flatten(2).transpose(1, 2)
-        x = self.proj(x)
-        return x
+from mmseg.models.builder import HEADS
+from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 
 
 @HEADS.register_module()
 class SegFormerHead(BaseDecodeHead):
-    """
-    SegFormer: Simple and Efficient Design for Semantic Segmentation with
-    Transformers
-    """
+    """The MLP Head of segformer."""
 
-    def __init__(self, feature_strides, decoder_params, **kwargs):
+    def __init__(self, interpolate_mode='bilinear', **kwargs):
         super(SegFormerHead, self).__init__(
             input_transform='multiple_select', **kwargs)
-        assert len(feature_strides) == len(self.in_channels)
-        assert min(feature_strides) == feature_strides[0]
-        self.feature_strides = feature_strides
+        self.interpolate_mode = interpolate_mode
 
-        c1_in_channels, c2_in_channels, c3_in_channels, c4_in_channels = \
-            self.in_channels
+        embed_dim = self.channels
 
-        decoder_params = decoder_params
-        embedding_dim = decoder_params['embed_dim']
-
-        self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
-        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
-        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
-        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
-
-        self.linear_fuse = ConvModule(
-            in_channels=embedding_dim * 4,
-            out_channels=embedding_dim,
+        self.conv_c4 = ConvModule(
+            in_channels=self.in_channels[-1],
+            out_channels=embed_dim,
             kernel_size=1,
-            norm_cfg=dict(type='SyncBN', requires_grad=True))
+            stride=1,
+            norm_cfg=self.norm_cfg,
+            act_cfg=self.act_cfg)
+        self.conv_c3 = ConvModule(
+            in_channels=self.in_channels[-2],
+            out_channels=embed_dim,
+            kernel_size=1,
+            stride=1,
+            norm_cfg=self.norm_cfg,
+            act_cfg=self.act_cfg)
+        self.conv_c2 = ConvModule(
+            in_channels=self.in_channels[-3],
+            out_channels=embed_dim,
+            kernel_size=1,
+            stride=1,
+            norm_cfg=self.norm_cfg,
+            act_cfg=self.act_cfg)
+        self.conv_c1 = ConvModule(
+            in_channels=self.in_channels[-4],
+            out_channels=embed_dim,
+            kernel_size=1,
+            stride=1,
+            norm_cfg=self.norm_cfg,
+            act_cfg=self.act_cfg)
 
-        self.linear_pred = nn.Conv2d(
-            embedding_dim, self.num_classes, kernel_size=1)
+        self.conv_fuse = ConvModule(
+            in_channels=embed_dim * 4,
+            out_channels=embed_dim,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
 
     def forward(self, inputs):
-        x = self._transform_inputs(inputs)  # len=4, 1/4,1/8,1/16,1/32
+        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        x = self._transform_inputs(inputs)
         c1, c2, c3, c4 = x
 
-        # MLP decoder on C1-C4 #
-        n, _, h, w = c4.shape
+        # MLP decoder on C1-C4
+        n, _, h, w = c1.shape
 
-        _c4 = self.linear_c4(c4).permute(0, 2,
-                                         1).reshape(n, -1, c4.shape[2],
-                                                    c4.shape[3])
-        _c4 = F.interpolate(
-            _c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
+        out_c4 = self.conv_c4(c4)
+        out_c4 = F.interpolate(
+            out_c4,
+            size=(h, w),
+            mode=self.mode,
+            align_corners=self.align_corners)
 
-        _c3 = self.linear_c3(c3).permute(0, 2,
-                                         1).reshape(n, -1, c3.shape[2],
-                                                    c3.shape[3])
-        _c3 = F.interpolate(
-            _c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
+        out_c3 = self.conv_c3(c3)
+        out_c3 = F.interpolate(
+            out_c3,
+            size=(h, w),
+            mode=self.mode,
+            align_corners=self.align_corners)
 
-        _c2 = self.linear_c2(c2).permute(0, 2,
-                                         1).reshape(n, -1, c2.shape[2],
-                                                    c2.shape[3])
-        _c2 = F.interpolate(
-            _c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
+        out_c2 = self.conv_c2(c2)
+        out_c2 = F.interpolate(
+            out_c2,
+            size=(h, w),
+            mode=self.mode,
+            align_corners=self.align_corners)
 
-        _c1 = self.linear_c1(c1).permute(0, 2,
-                                         1).reshape(n, -1, c1.shape[2],
-                                                    c1.shape[3])
+        out_c1 = self.conv_c1(c1)
 
-        _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
+        out = self.conv_fuse(
+            torch.cat([out_c4, out_c3, out_c2, out_c1], dim=1))
 
-        x = self.dropout(_c)
-        x = self.linear_pred(x)
+        x = self.cls_seg(out)
 
         return x
