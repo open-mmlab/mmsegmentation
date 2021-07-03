79b343bdf5c6ad0faf7dfb251c95a9428f3f2360
Robin Karlsson <robin.karlsson0@gmail.com>
2021-07-03 21:43:59 +0900

    FIX linting

--- mmseg/datasets/a2d2.py
+++ mmseg/datasets/a2d2.py
@@ -28,16 +28,15 @@ class A2D2Dataset(CustomDataset):
     """
 
     CLASSES = ('car', 'bicycle', 'pedestrian', 'truck', 'small_vehicles',
-               'traffic_signal', 'traffic_sign', 'utility_vehicle',
-               'sidebars', 'speed_bumber', 'curbstone', 'solid_line',
-               'irrelevant_signs', 'road_blocks', 'tractor',
-               'non-drivable_street', 'zebra_crossing', 'obstacles_trash',
-               'poles', 'rd_restricted_area', 'animals', 'grid_structure',
+               'traffic_signal', 'traffic_sign', 'utility_vehicle', 'sidebars',
+               'speed_bumber', 'curbstone', 'solid_line', 'irrelevant_signs',
+               'road_blocks', 'tractor', 'non-drivable_street',
+               'zebra_crossing', 'obstacles_trash', 'poles',
+               'rd_restricted_area', 'animals', 'grid_structure',
                'signal_corpus', 'drivable_cobblestone', 'electronic_traffic',
-               'slow_drive_area', 'nature_object', 'parking_area',
-               'sidewalk', 'painted_drive_instr', 'traffic_guide_obj',
-               'dashed_line', 'rd_normal_street', 'sky', 'buildings'
-                )
+               'slow_drive_area', 'nature_object', 'parking_area', 'sidewalk',
+               'painted_drive_instr', 'traffic_guide_obj', 'dashed_line',
+               'rd_normal_street', 'sky', 'buildings')
 
     PALETTE = [[255, 0, 0], [182, 89, 6], [204, 153, 255], [255, 128, 0],
                [0, 255, 0], [0, 128, 255], [0, 255, 255], [255, 255, 0],
@@ -45,9 +44,10 @@ class A2D2Dataset(CustomDataset):
                [64, 0, 64], [185, 122, 87], [0, 0, 100], [139, 99, 108],
                [210, 50, 115], [255, 0, 128], [255, 246, 143], [150, 0, 150],
                [204, 255, 153], [238, 162, 173], [33, 44, 177], [180, 50, 180],
-               [255, 70, 185], [238, 233, 191], [147, 253, 194], [150, 150, 200],
-               [180, 150, 200], [200, 125, 210], [159, 121, 238], [128, 0, 255],
-               [255, 0, 255], [135, 206, 255], [241, 230, 255]]
+               [255, 70, 185], [238, 233, 191], [147, 253, 194],
+               [150, 150, 200], [180, 150, 200], [200, 125, 210],
+               [159, 121, 238], [128, 0, 255], [255, 0, 255], [135, 206, 255],
+               [241, 230, 255]]
 
     def __init__(self, **kwargs):
         super(A2D2Dataset, self).__init__(
