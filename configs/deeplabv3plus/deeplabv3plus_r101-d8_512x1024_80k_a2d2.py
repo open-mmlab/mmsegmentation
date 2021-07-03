2358d53d0c6348e72a50778641f9d822c3077274
Robin Karlsson <robin.karlsson0@gmail.com>
2021-07-03 21:47:23 +0900

    ADD Model config files

--- configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_80k_a2d2.py
+++ configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_80k_a2d2.py
@@ -1,2 +1,4 @@
 _base_ = './deeplabv3plus_r50-d8_512x1024_80k_a2d2.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
+model = dict(
+    pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101),
+    decode_head=dict(num_classes=35), auxiliary_head=dict(num_classes=35))
