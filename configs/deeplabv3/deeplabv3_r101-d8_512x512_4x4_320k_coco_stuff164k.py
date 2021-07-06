5946215e0961b62860b50e04a2d265786a5035e2
JunJun2016 <hejunjun@sjtu.edu.cn>
2021-07-07 00:58:04 +0800

    fix wrong configs

--- configs/deeplabv3/deeplabv3_r101-d8_512x512_4x4_320k_coco_stuff164k.py
+++ configs/deeplabv3/deeplabv3_r101-d8_512x512_4x4_320k_coco_stuff164k.py
@@ -1,2 +1,2 @@
-_base_ = './deeplabv3_r50-d8_512x512_320k_coco_stuff164k.py'
+_base_ = './deeplabv3_r50-d8_512x512_4x4_320k_coco_stuff164k.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
