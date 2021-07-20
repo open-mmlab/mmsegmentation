b3903caaba235bbeebdae935b8c67cf2b77c362f
xiexinch <xinchen.xie@qq.com>
2021-07-20 15:58:32 +0800

    fix configs error

--- configs/dpt/dpt_vit-l16_512x512_160k_ade20k.py
+++ configs/dpt/dpt_vit-l16_512x512_160k_ade20k.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/dpt_vit-16.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/dpt_vit-l16.py', '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 
