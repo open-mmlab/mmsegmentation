0b1adc3be918403c366cb7256745b084f9abd220
duan-she-li <1063135843@qq.com>
2021-07-22 22:03:38 +0800

    修正segmenter配置参数

--- local_configs/segmenter_mask/segmenterM_vit-b16_ln_256x256_160k_ade20k.py
+++ local_configs/segmenter_mask/segmenterM_vit-b16_ln_256x256_160k_ade20k.py
@@ -1,21 +1,19 @@
 _base_ = [
     '../_base_/models/segmenterM_vit.py',
-    '../_base_/datasets/ade20k.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_20k.py'
+    '../_base_/datasets/ade20k_256.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
 ]
 
-# bt16 means vit-B seg-T patchsize=16
-# in the origin config of segmenter, encoder and decoder transformer's parameter are the same
 model = dict(
-    backbone=dict(drop_path_rate=0.1, final_norm=True),
+    backbone=dict(img_size=(256, 256),drop_path_rate=0.1, final_norm=True),
     decode_head=dict(
         num_classes=150,
-        channels=192,
+        channels=768,
         num_layers=12,
-        num_heads=3),
+        num_heads=12),
     auxiliary_head=dict(num_classes=150),
     # ensure image size can be divided by the patch size
-    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(341, 341)))
+    test_cfg=dict(mode='slide', crop_size=(256, 256), stride=(171, 171)))
 
 # AdamW optimizer, no weight decay for position embedding & layer norm
 # in backbone
@@ -43,5 +41,4 @@ lr_config = dict(
     min_lr=0.0,
     by_epoch=False)
 
-# By default, models are trained on 8 GPUs with 2 images per GPU
-data = dict(samples_per_gpu=2)
+
