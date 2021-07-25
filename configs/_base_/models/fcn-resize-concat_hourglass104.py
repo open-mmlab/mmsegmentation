0f13d01fd33f0f8816a019ebe6c01c4d35d6ad38
Junjun2016 <hejunjun@sjtu.edu.cn>
2021-07-25 22:34:35 +0800

    fix config error

--- configs/_base_/models/fcn-resize-concat_hourglass104.py
+++ configs/_base_/models/fcn-resize-concat_hourglass104.py
@@ -4,6 +4,7 @@ model = dict(
     decode_head=dict(
         in_channels=[256, 256],
         in_index=(0, 1),
-        channels=sum([256, 256], input_transform='resize_concat'),
+        channels=sum([256, 256]),
+        input_transform='resize_concat',
         num_convs=2,
         kernel_size=3))
