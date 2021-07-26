623900528a7990756f59bfcf12af649cf849388e
sennnnn <201730271412@mail.scut.edu.cn>
2021-07-26 16:30:34 +0800

    [Enhancement] Add a more reasonable model gather tool.

--- .dev/gather_models.py
+++ .dev/gather_models.py
@@ -1,16 +1,19 @@
 import argparse
-import glob
+import hashlib
 import json
 import os
 import os.path as osp
 import shutil
-import subprocess
 
-import mmcv
 import torch
 
-# build schedule look-up table to automatically find the final model
-RESULTS_LUT = ['mIoU', 'mAcc', 'aAcc']
+
+def calculate_file_sha256(file_path):
+    """calculate file sha256 hash code."""
+    with open(file_path, 'rb') as fp:
+        sha256_cal = hashlib.sha256()
+        sha256_cal.update(fp.read())
+        return sha256_cal.hexdigest()
 
 
 def process_checkpoint(in_file, out_file):
@@ -21,176 +24,141 @@ def process_checkpoint(in_file, out_file):
     # if it is necessary to remove some sensitive data in checkpoint['meta'],
     # add the code here.
     torch.save(checkpoint, out_file)
-    sha = subprocess.check_output(['sha256sum', out_file]).decode()
+    # The hash code calculation and rename command differ on different system
+    # platform.
+    sha = calculate_file_sha256(out_file)
     final_file = out_file.rstrip('.pth') + '-{}.pth'.format(sha[:8])
-    subprocess.Popen(['mv', out_file, final_file])
-    return final_file
+    os.rename(out_file, final_file)
+
+    # Remove prefix and suffix
+    final_file_name = osp.split(final_file)[1]
+    final_file_name = osp.splitext(final_file_name)[0]
+
+    return final_file_name
 
 
-def get_final_iter(config):
-    iter_num = config.split('_')[-2]
-    assert iter_num.endswith('k')
-    return int(iter_num[:-1]) * 1000
+def collect_log(ckpt_folder, collect_folder, config_name):
+    # Extract log file. When there are multiple log file, we will throw a
+    # AssertionError
+    log_json_name = [
+        x for x in os.listdir(ckpt_folder) if osp.splitext(x)[1] == '.json'
+    ]
+    assert len(
+        log_json_name
+    ) == 1, 'There are multiple log files, please remove useless log file.'
+
+    log_json_name = log_json_name[0]
+    log_json_path = osp.join(ckpt_folder, log_json_name)
+
+    log_name = log_json_name.rstrip('.json')
+    log_path = osp.join(ckpt_folder, log_name)
+
+    log_json_dst_path = osp.join(collect_folder,
+                                 f'{config_name}_{log_json_name}')
+    log_dst_path = osp.join(collect_folder, f'{config_name}_{log_name}')
+
+    shutil.copy(log_json_path, log_json_dst_path)
+    shutil.copy(log_path, log_dst_path)
+
+    log_time = log_name.rstrip('.log')
+
+    print(f'[1/3] {config_name} log json collect: {log_json_path} => '
+          f'{log_json_dst_path}')
+    print(f'[2/3] {config_name} log collect: {log_path} => {log_dst_path}')
+
+    return log_json_path, log_time
 
 
-def get_final_results(log_json_path, iter_num):
-    result_dict = dict()
-    with open(log_json_path, 'r') as f:
-        for line in f.readlines():
-            log_line = json.loads(line)
-            if 'mode' not in log_line.keys():
-                continue
+def collect_ckpt(log_jsons, log_time, ckpt_folder, collect_folder, config_name,
+                 publish_flag, collect_mode):
+    _ = log_jsons[0]
+    log_jsons = log_jsons[1:]
+    if collect_mode.lower() == 'last':
+        src_ckpt_name = f'iter_{len(log_jsons) * 50}.pth'
+        output_metric = log_jsons[-1]['IoU.referring object']
+    elif collect_mode.lower() == 'best':
+        max_metric = -1
+        max_iter = -1
+        for i, log_json in enumerate(log_jsons):
+            if log_json['mode'] == 'val' or \
+                    'mIoU' in log_json:
+                metric = log_json['IoU.referring object']
+                iter = log_json['iter']
+                last_iter = log_jsons[i - 1]['iter']
+                # TODO: Fix TextLoggerHook Bug
+                if last_iter > iter:
+                    iter = last_iter + 50
+                if metric > max_metric:
+                    max_iter = iter
+                    max_metric = metric
+        src_ckpt_name = f'iter_{max_iter}.pth'
+        output_metric = max_metric
 
-            if log_line['mode'] == 'train' and log_line['iter'] == iter_num:
-                result_dict['memory'] = log_line['memory']
+    src_ckpt_path = osp.join(ckpt_folder, src_ckpt_name)
+    dst_ckpt_path = osp.join(collect_folder, f'{config_name}_{log_time}.pth')
 
-            if log_line['iter'] == iter_num:
-                result_dict.update({
-                    key: log_line[key]
-                    for key in RESULTS_LUT if key in log_line
-                })
-                return result_dict
+    if not publish_flag:
+        shutil.copy(src_ckpt_path, dst_ckpt_path)
+        process_checkpoint(dst_ckpt_path, dst_ckpt_path)
+    else:
+        os.rename(src_ckpt_path, dst_ckpt_path)
+
+    print(
+        f'[3/3] {config_name} mode({collect_mode}) publish({not publish_flag})'
+        f' metric({output_metric}) '
+        f'ckpt collect: {src_ckpt_path} => {dst_ckpt_path}')
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Gather benchmarked models')
+    parser = argparse.ArgumentParser()
     parser.add_argument(
-        'root',
+        'config_path',
+        help='The comprehensive config path of model files'
+        'which need to collect.')
+    parser.add_argument('-f', '--folder', help='The model ckpts save path.')
+    parser.add_argument('-c', '--collect', help='The collect folder path.')
+    parser.add_argument(
+        '-p',
+        '--no-publish',
+        action='store_true',
+        help='Remove optimizer related params and calculate hash code which '
+        'can verify completeness and correctness of file.')
+    parser.add_argument(
+        '-m',
+        '--mode',
+        default='best',
         type=str,
-        help='root path of benchmarked models to be gathered')
-    parser.add_argument(
-        'config',
-        type=str,
-        help='root path of benchmarked configs to be gathered')
-    parser.add_argument(
-        'out_dir',
-        type=str,
-        help='output path of gathered models to be stored')
-    parser.add_argument('out_file', type=str, help='the output json file name')
-    parser.add_argument(
-        '--filter', type=str, nargs='+', default=[], help='config filter')
-    parser.add_argument(
-        '--all', action='store_true', help='whether include .py and .log')
+        help='The collect mode: collect best ckpt or last ckpt.')
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    models_root = args.root
-    models_out = args.out_dir
-    config_name = args.config
-    mmcv.mkdir_or_exist(models_out)
+    config_path = args.config_path
+    publish_flag = args.no_publish
+    collect_mode = args.mode
 
-    # find all models in the root directory to be gathered
-    raw_configs = list(mmcv.scandir(config_name, '.py', recursive=True))
+    assert collect_mode.lower() in ['best', 'last']
 
-    # filter configs that is not trained in the experiments dir
-    used_configs = []
-    for raw_config in raw_configs:
-        work_dir = osp.splitext(osp.basename(raw_config))[0]
-        if osp.exists(osp.join(models_root, work_dir)):
-            used_configs.append((work_dir, raw_config))
-    print(f'Find {len(used_configs)} models to be gathered')
+    # strip dir path and file suffix
+    config_name = osp.split(config_path)[1]  # strip prefix
+    config_name = osp.splitext(config_name)[0]  # strip suffix
 
-    # find final_ckpt and log file for trained each config
-    # and parse the best performance
-    model_infos = []
-    for used_config, raw_config in used_configs:
-        bypass = True
-        for p in args.filter:
-            if p in used_config:
-                bypass = False
-                break
-        if bypass:
-            continue
-        exp_dir = osp.join(models_root, used_config)
-        # check whether the exps is finished
-        final_iter = get_final_iter(used_config)
-        final_model = 'iter_{}.pth'.format(final_iter)
-        model_path = osp.join(exp_dir, final_model)
+    ckpt_folder = args.folder or osp.join('work_dirs', config_name)
+    collect_folder = args.collect or osp.join('collect', config_name)
 
-        # skip if the model is still training
-        if not osp.exists(model_path):
-            print(f'{used_config} train not finished yet')
-            continue
+    if not osp.exists(collect_folder):
+        os.makedirs(collect_folder, 0o775)
 
-        # get logs
-        log_json_paths = glob.glob(osp.join(exp_dir, '*.log.json'))
-        log_json_path = log_json_paths[0]
-        model_performance = None
-        for idx, _log_json_path in enumerate(log_json_paths):
-            model_performance = get_final_results(_log_json_path, final_iter)
-            if model_performance is not None:
-                log_json_path = _log_json_path
-                break
+    log_json_path, log_time = collect_log(ckpt_folder, collect_folder,
+                                          config_name)
+    log_jsons = []
+    for line in open(log_json_path, 'r').readlines():
+        log_jsons.append(json.loads(line))
 
-        if model_performance is None:
-            print(f'{used_config} model_performance is None')
-            continue
-
-        model_time = osp.split(log_json_path)[-1].split('.')[0]
-        model_infos.append(
-            dict(
-                config=used_config,
-                raw_config=raw_config,
-                results=model_performance,
-                iters=final_iter,
-                model_time=model_time,
-                log_json_path=osp.split(log_json_path)[-1]))
-
-    # publish model for each checkpoint
-    publish_model_infos = []
-    for model in model_infos:
-        model_publish_dir = osp.join(models_out,
-                                     model['raw_config'].rstrip('.py'))
-        model_name = osp.split(model['config'])[-1].split('.')[0]
-
-        publish_model_path = osp.join(model_publish_dir,
-                                      model_name + '_' + model['model_time'])
-        trained_model_path = osp.join(models_root, model['config'],
-                                      'iter_{}.pth'.format(model['iters']))
-        if osp.exists(model_publish_dir):
-            for file in os.listdir(model_publish_dir):
-                if file.endswith('.pth'):
-                    print(f'model {file} found')
-                    model['model_path'] = osp.abspath(
-                        osp.join(model_publish_dir, file))
-                    break
-            if 'model_path' not in model:
-                print(f'dir {model_publish_dir} exists, no model found')
-
-        else:
-            mmcv.mkdir_or_exist(model_publish_dir)
-
-            # convert model
-            final_model_path = process_checkpoint(trained_model_path,
-                                                  publish_model_path)
-            model['model_path'] = final_model_path
-
-        new_json_path = f'{model_name}-{model["log_json_path"]}'
-        # copy log
-        shutil.copy(
-            osp.join(models_root, model['config'], model['log_json_path']),
-            osp.join(model_publish_dir, new_json_path))
-        if args.all:
-            new_txt_path = new_json_path.rstrip('.json')
-            shutil.copy(
-                osp.join(models_root, model['config'],
-                         model['log_json_path'].rstrip('.json')),
-                osp.join(model_publish_dir, new_txt_path))
-
-        if args.all:
-            # copy config to guarantee reproducibility
-            raw_config = osp.join(config_name, model['raw_config'])
-            mmcv.Config.fromfile(raw_config).dump(
-                osp.join(model_publish_dir, osp.basename(raw_config)))
-
-        publish_model_infos.append(model)
-
-    models = dict(models=publish_model_infos)
-    mmcv.dump(models, osp.join(models_out, args.out_file))
+    collect_ckpt(log_jsons, log_time, ckpt_folder, collect_folder, config_name,
+                 publish_flag, collect_mode)
 
 
 if __name__ == '__main__':
