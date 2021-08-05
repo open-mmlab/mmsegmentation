6cc779441536491a01b4a0aa89a1e2879637d266
Robin Karlsson <robin.karlsson0@gmail.com>
2021-08-05 09:57:34 +0900

    FIX Removed obsolete config files for build failiure

--- configs/deeplabv3/deeplabv3_r101-d8_512x1024_160k_a2d2.py
+++
@@ -1,2 +0,0 @@
-_base_ = './deeplabv3_r50-d8_512x1024_160k_a2d2.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
--- configs/pspnet/pspnet_r50-d8_512x1024_80k_a2d2.py
+++
@@ -1,4 +0,0 @@
-_base_ = [
-    '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/a2d2.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
-]
