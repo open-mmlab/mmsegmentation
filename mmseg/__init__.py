93a558f0d77a154ec42de19637fc0b2613eeb9d8
Junjun2016 <hejunjun@sjtu.edu.cn>
2021-08-12 14:34:02 +0800

    [Fix] Update digit_version (#778)
    * update digit_version
    
    * add unittest
    
    * fix import

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -1,4 +1,7 @@
+import warnings
+
 import mmcv
+from packaging.version import parse
 
 from .version import __version__, version_info
 
@@ -6,16 +9,44 @@ MMCV_MIN = '1.3.7'
 MMCV_MAX = '1.4.0'
 
 
-def digit_version(version_str):
-    digit_version = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            digit_version.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            digit_version.append(int(patch_version[0]) - 1)
-            digit_version.append(int(patch_version[1]))
-    return digit_version
+def digit_version(version_str: str, length: int = 4):
+    """Convert a version string into a tuple of integers.
+
+    This method is usually used for comparing two versions. For pre-release
+    versions: alpha < beta < rc.
+
+    Args:
+        version_str (str): The version string.
+        length (int): The maximum number of version levels. Default: 4.
+
+    Returns:
+        tuple[int]: The version info in digits (integers).
+    """
+    version = parse(version_str)
+    assert version.release, f'failed to parse version {version_str}'
+    release = list(version.release)
+    release = release[:length]
+    if len(release) < length:
+        release = release + [0] * (length - len(release))
+    if version.is_prerelease:
+        mapping = {'a': -3, 'b': -2, 'rc': -1}
+        val = -4
+        # version.pre can be None
+        if version.pre:
+            if version.pre[0] not in mapping:
+                warnings.warn(f'unknown prerelease version {version.pre[0]}, '
+                              'version checking may go wrong')
+            else:
+                val = mapping[version.pre[0]]
+            release.extend([val, version.pre[-1]])
+        else:
+            release.extend([val, 0])
+
+    elif version.is_postrelease:
+        release.extend([1, version.post])
+    else:
+        release.extend([0, 0])
+    return tuple(release)
 
 
 mmcv_min_version = digit_version(MMCV_MIN)
@@ -27,4 +58,4 @@ assert (mmcv_min_version <= mmcv_version <= mmcv_max_version), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_min_version}, <={mmcv_max_version}.'
 
-__all__ = ['__version__', 'version_info']
+__all__ = ['__version__', 'version_info', 'digit_version']
