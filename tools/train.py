9895e345968cb50f2d02d979815d49efb9d562ed
sennnnn <201730271412@mail.scut.edu.cn>
2021-08-12 18:12:53 +0800

    Modify default work dir when training.

--- tools/train.py
+++ tools/train.py
@@ -76,8 +76,10 @@ def main():
         cfg.work_dir = args.work_dir
     elif cfg.get('work_dir', None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        config_dir = osp.split(osp.dirname(args.config))[1]
+        config_name = osp.splitext(osp.basename(args.config))[0]
+        cfg.work_dir = osp.join('./work_dirs', config_dir, config_name)
+
     if args.load_from is not None:
         cfg.load_from = args.load_from
     if args.resume_from is not None:
