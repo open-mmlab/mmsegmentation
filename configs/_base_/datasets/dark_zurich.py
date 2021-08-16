ad39317ed272f09a417c7ad8948293b6cb025352
xiexinch <xinchen.xie@qq.com>
2021-08-16 16:11:36 +0800

    reset image size

--- configs/_base_/datasets/dark_zurich.py
+++ configs/_base_/datasets/dark_zurich.py
@@ -1,15 +1,15 @@
 # dataset settings
 dataset_type = 'DarkZurichDataset'
-data_root = 'data/dark1/'
+data_root = 'data/dark_zurich/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 1024)
+crop_size = (1080, 960)
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        img_scale=(1080, 1920),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
