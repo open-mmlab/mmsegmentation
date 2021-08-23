9d24dd09cff99ce3a3eb77c92008bb2bb56e29fd
MengzhangLI <mcmong@pku.edu.cn>
2021-08-23 18:36:55 +0800

    fix unittest

--- tests/test_models/test_backbones/test_bisenet.py
+++ tests/test_models/test_backbones/test_bisenet.py
@@ -22,17 +22,17 @@ def test_fastscnn_backbone():
     model.init_weights()
     model.train()
     batch_size = 2
-    imgs = torch.randn(batch_size, 3, 1024, 2048)
+    imgs = torch.randn(batch_size, 3, 512, 1024)
     feat = model(imgs)
 
     assert len(feat) == 5
     # output for Segment Head
-    assert feat[0].shape == torch.Size([batch_size, 128, 128, 256])
+    assert feat[0].shape == torch.Size([batch_size, 128, 64, 128])
     # feat_2
-    assert feat[1].shape == torch.Size([batch_size, 16, 256, 512])
+    assert feat[1].shape == torch.Size([batch_size, 16, 128, 256])
     # feat_3
-    assert feat[2].shape == torch.Size([batch_size, 32, 128, 256])
+    assert feat[2].shape == torch.Size([batch_size, 32, 64, 128])
     # feat_4
-    assert feat[3].shape == torch.Size([batch_size, 64, 64, 128])
+    assert feat[3].shape == torch.Size([batch_size, 64, 32, 64])
     # feat_5_4
-    assert feat[4].shape == torch.Size([batch_size, 128, 32, 64])
+    assert feat[4].shape == torch.Size([batch_size, 128, 16, 32])
