cdf91351e0c91eba13a30b36d24e9feaca43de5c
xiexinch <xinchen.xie@qq.com>
2021-08-24 14:32:19 +0800

    add train_pipeline

--- configs/_base_/datasets/dark_zurich.py
+++ configs/_base_/datasets/dark_zurich.py
@@ -3,7 +3,19 @@ dataset_type = 'DarkZurichDataset'
 data_root = 'data/dark_zurich/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
+crop_size = (960, 1080)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img']),
+]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -22,6 +34,7 @@ test_pipeline = [
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
+    train=dict(type=dataset_type, pipeline=train_pipeline),
     test=dict(
         type=dataset_type,
         data_root=data_root,
