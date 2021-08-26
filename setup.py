ef56ec615b2cf3df3baa7c2ea6db5df82e90ed73
Junjun2016 <hejunjun@sjtu.edu.cn>
2021-08-26 14:01:27 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ def parse_requirements(fname='requirements.txt', with_version=True):
     return packages
 
 
-def add_mim_extention():
+def add_mim_extension():
     """Add extra files that are required to support MIM into the package.
 
     These files will be added by creating a symlink to the originals if the
@@ -147,7 +147,7 @@ def add_mim_extention():
 
 
 if __name__ == '__main__':
-    add_mim_extention()
+    add_mim_extension()
     setup(
         name='mmsegmentation',
         version=get_version(),
