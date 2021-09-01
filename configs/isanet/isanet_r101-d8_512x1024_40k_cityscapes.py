20b6e1aa194d607f59734d0bee9d5472b3f75029
LayneH <laynehuang@outlook.com>
2021-09-01 07:47:00 +0000

    Update configs

--- configs/isanet/isanet_r101-d8_512x1024_40k_cityscapes.py
+++ configs/isanet/isanet_r101-d8_512x1024_40k_cityscapes.py
@@ -1,5 +1,2 @@
-_base_ = [
-    '../_base_/models/isanet_r50-d8.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
-]
+_base_ = './isanet_r50-d8_512x1024_40k_cityscapes.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
