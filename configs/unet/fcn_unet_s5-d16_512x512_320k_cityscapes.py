97276f70f3669c1a069433dd95efc47a174b8b5f
Junjun2016 <hejunjun@sjtu.edu.cn>
2021-09-14 21:00:41 +0800

    add unet configs on cityscapes

---
+++ configs/unet/fcn_unet_s5-d16_512x512_320k_cityscapes.py
@@ -0,0 +1,11 @@
+_base_ = [
+    '../_base_/models/fcn_unet_s5-d16.py',
+    '../_base_/datasets/cityscapes_512x512.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_320k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=19),
+    auxiliary_head=dict(num_classes=19),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
