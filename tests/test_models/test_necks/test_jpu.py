d056d80479cdc228b49ae079112567b3a2de55ab
MengzhangLI <mcmong@pku.edu.cn>
2021-09-18 16:13:51 +0800

    Fixing lint error

--- tests/test_models/test_necks/test_jpu.py
+++ tests/test_models/test_necks/test_jpu.py
@@ -11,15 +11,15 @@ def test_fastfcn_neck():
     model.train()
     batch_size = 1
     input = [
-        torch.randn(batch_size, 256, 256, 512),
-        torch.randn(batch_size, 512, 128, 256),
-        torch.randn(batch_size, 1024, 64, 128),
-        torch.randn(batch_size, 2048, 32, 64)
+        torch.randn(batch_size, 256, 128, 256),
+        torch.randn(batch_size, 512, 64, 128),
+        torch.randn(batch_size, 1024, 32, 64),
+        torch.randn(batch_size, 2048, 16, 32)
     ]
     feat = model(input)
 
     assert len(feat) == 4
-    assert feat[0].shape == torch.Size([batch_size, 256, 256, 512])
-    assert feat[1].shape == torch.Size([batch_size, 512, 128, 256])
-    assert feat[2].shape == torch.Size([batch_size, 1024, 64, 128])
-    assert feat[3].shape == torch.Size([batch_size, 2048, 128, 256])
+    assert feat[0].shape == torch.Size([batch_size, 256, 128, 256])
+    assert feat[1].shape == torch.Size([batch_size, 512, 64, 128])
+    assert feat[2].shape == torch.Size([batch_size, 1024, 32, 64])
+    assert feat[3].shape == torch.Size([batch_size, 2048, 16, 32])
