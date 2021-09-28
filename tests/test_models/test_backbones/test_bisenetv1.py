ac1b2f73f469ac990e3b547a96a36d03af4eb2ae
MengzhangLI <mcmong@pku.edu.cn>
2021-09-28 13:57:52 +0800

    Fixing unittest error

--- tests/test_models/test_backbones/test_bisenetv1.py
+++ tests/test_models/test_backbones/test_bisenetv1.py
@@ -25,20 +25,20 @@ def test_bisenetv1_backbone():
     model.init_weights()
     model.train()
     batch_size = 2
-    imgs = torch.randn(batch_size, 3, 512, 1024)
+    imgs = torch.randn(batch_size, 3, 256, 512)
     feat = model(imgs)
 
     assert len(feat) == 3
     # output for segment Head
-    assert feat[0].shape == torch.Size([batch_size, 256, 64, 128])
+    assert feat[0].shape == torch.Size([batch_size, 256, 32, 64])
     # for auxiliary head 1
-    assert feat[1].shape == torch.Size([batch_size, 128, 64, 128])
+    assert feat[1].shape == torch.Size([batch_size, 128, 32, 64])
     # for auxiliary head 2
-    assert feat[2].shape == torch.Size([batch_size, 128, 32, 64])
+    assert feat[2].shape == torch.Size([batch_size, 128, 16, 32])
 
     # Test input with rare shape
     batch_size = 2
-    imgs = torch.randn(batch_size, 3, 952, 527)
+    imgs = torch.randn(batch_size, 3, 527, 279)
     feat = model(imgs)
     assert len(feat) == 3
 
@@ -83,27 +83,27 @@ def test_bisenetv1_context_path():
 
 
 def test_bisenetv1_attention_refinement_module():
-    x_arm = AttentionRefinementModule(512, 128)
-    assert x_arm.conv_layer.in_channels == 512
-    assert x_arm.conv_layer.out_channels == 128
+    x_arm = AttentionRefinementModule(256, 64)
+    assert x_arm.conv_layer.in_channels == 256
+    assert x_arm.conv_layer.out_channels == 64
     assert x_arm.conv_layer.kernel_size == (3, 3)
-    x = torch.randn(2, 512, 32, 64)
+    x = torch.randn(2, 256, 32, 64)
     x_out = x_arm(x)
-    assert x_out.shape == torch.Size([2, 128, 32, 64])
+    assert x_out.shape == torch.Size([2, 64, 32, 64])
 
 
 def test_bisenetv1_feature_fusion_module():
-    ffm = FeatureFusionModule(256, 512)
-    assert ffm.conv1.in_channels == 256
-    assert ffm.conv1.out_channels == 512
+    ffm = FeatureFusionModule(128, 256)
+    assert ffm.conv1.in_channels == 128
+    assert ffm.conv1.out_channels == 256
     assert ffm.conv1.kernel_size == (1, 1)
     assert ffm.gap.output_size == (1, 1)
-    assert ffm.conv_atten[0].in_channels == 512
-    assert ffm.conv_atten[0].out_channels == 512
+    assert ffm.conv_atten[0].in_channels == 256
+    assert ffm.conv_atten[0].out_channels == 256
     assert ffm.conv_atten[0].kernel_size == (1, 1)
 
-    ffm = FeatureFusionModule(256, 256)
-    x1 = torch.randn(2, 128, 128, 256)
-    x2 = torch.randn(2, 128, 128, 256)
+    ffm = FeatureFusionModule(128, 128)
+    x1 = torch.randn(2, 64, 64, 128)
+    x2 = torch.randn(2, 64, 64, 128)
     x_out = ffm(x1, x2)
-    assert x_out.shape == torch.Size([2, 256, 128, 256])
+    assert x_out.shape == torch.Size([2, 128, 64, 128])
