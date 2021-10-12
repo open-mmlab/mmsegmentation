58f7bece4e7a63837fa453147a3c91724ed2f2a5
rstrudel <rstrudel@gmail.com>
2021-10-12 18:08:00 +0200

    configs: transfer from _base_ to segmenter

--- configs/segmenter/segmenter_vit-b_mask_512x512_160k_bs8_ade20k.py
+++ configs/segmenter/segmenter_vit-b_mask_512x512_160k_bs8_ade20k.py
@@ -1,13 +1,43 @@
 _base_ = [
-    '../_base_/models/segmenter_vit-b_mask.py',
+    '../_base_/models/segmenter_vit.py',
     '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k.py',
 ]
-find_unused_parameters = True
+
+backbone_norm_cfg = dict(type='LN', eps=1e-6, requires_grad=True)
 model = dict(
-    backbone=dict(img_size=(512, 512)),
-    decode_head=dict(num_classes=150),
+    type='EncoderDecoder',
+    pretrained='pretrain/vit_base_p16_384.pth',
+    backbone=dict(
+        type='VisionTransformer',
+        img_size=(512, 512),
+        patch_size=16,
+        in_channels=3,
+        embed_dims=768,
+        num_layers=12,
+        num_heads=12,
+        drop_path_rate=0.1,
+        attn_drop_rate=0.0,
+        drop_rate=0.0,
+        final_norm=True,
+        norm_cfg=backbone_norm_cfg,
+        with_cls_token=True,
+        interpolate_mode='bicubic',
+    ),
+    decode_head=dict(
+        type='SegmenterMaskTransformerHead',
+        in_channels=768,
+        channels=768,
+        num_classes=150,
+        num_layers=2,
+        num_heads=12,
+        embed_dims=768,
+        in_index=-1,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+    ),
     auxiliary_head=[],
     test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(512, 512)),
 )
--- configs/segmenter/segmenter_vit-s_linear_512x512_160k_bs8_ade20k.py
+++ configs/segmenter/segmenter_vit-s_linear_512x512_160k_bs8_ade20k.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/segmenter_vit-s_linear.py',
+    '../_base_/models/segmenter_vit.py',
     '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k.py',
