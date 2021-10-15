a610c4a950c58611e0f10852c10ba8be62cfbdd8
linxinyang <810427220@qq.com>
2021-10-15 22:13:40 +0800

    add code of twins to mmseg

--- configs/twins/fpn_alt_gvt_b_512x512_80k_ade20k.py
+++ configs/twins/fpn_alt_gvt_b_512x512_80k_ade20k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/fpn_r50.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py'
+    '../_base_/default_runtime.py', 'twins_base.py'
 ]
 
 model = dict(
@@ -17,15 +17,7 @@ model = dict(
     decode_head=dict(num_classes=150),
     )
 
-# optimizer
 optimizer = dict(type='AdamW', lr=0.0001, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=1e-6, by_epoch=False)
-# runtime settings
-runner = dict(type='IterBasedRunner', max_iters=80000)
-checkpoint_config = dict(by_epoch=False, interval=8000)
-evaluation = dict(interval=8000, metric='mIoU')
-
 data = dict(
     samples_per_gpu=4)
---
+++ configs/twins/twins_base.py
@@ -0,0 +1,11 @@
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-6, by_epoch=False)
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=80000)
+checkpoint_config = dict(by_epoch=False, interval=8000)
+evaluation = dict(interval=8000, metric='mIoU')
+
+find_unused_parameters = True
