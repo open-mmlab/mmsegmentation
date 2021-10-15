087d6179d508bba2ba287c01acedb4c38f26adeb
linxinyang <810427220@qq.com>
2021-10-15 22:24:44 +0800

    add code of twins to mmseg

--- configs/twins/fpn_pcpvt_s_512x512_80k_ade20k.py
+++ configs/twins/fpn_pcpvt_s_512x512_80k_ade20k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/fpn_r50.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+    '../_base_/default_runtime.py', 'twins_base.py'
 ]
 
 model = dict(
