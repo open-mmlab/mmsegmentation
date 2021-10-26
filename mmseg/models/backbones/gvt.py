b8d4f803c1a4d178ce8a398d7f5925b64d66f76b
linxinyang <linxinyang@meituan.com>
2021-10-26 21:11:03 +0800

    this is a debug step, and needs to be recovered

--- mmseg/models/backbones/gvt.py
+++ mmseg/models/backbones/gvt.py
@@ -156,65 +156,6 @@ class GroupAttention(nn.Module):
         return x
 
 
-class Attention(nn.Module):
-
-    def __init__(self,
-                 dim,
-                 num_heads=8,
-                 qkv_bias=False,
-                 qk_scale=None,
-                 attn_drop=0.,
-                 proj_drop=0.,
-                 sr_ratio=1):
-        super().__init__()
-        assert dim % num_heads == 0, f'dim {dim} should be divided by ' \
-                                     f'num_heads {num_heads}.'
-
-        self.dim = dim
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim**-0.5
-
-        self.q = nn.Linear(dim, dim, bias=qkv_bias)
-        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-        self.sr_ratio = sr_ratio
-        if sr_ratio > 1:
-            self.sr = nn.Conv2d(
-                dim, dim, kernel_size=sr_ratio, stride=sr_ratio)
-            self.norm = nn.LayerNorm(dim)
-
-    def forward(self, x, H, W):
-        B, N, C = x.shape  # 1, 21760, 64
-        q = self.q(x).reshape(B, N, self.num_heads,
-                              C // self.num_heads).permute(0, 2, 1, 3)
-
-        if self.sr_ratio > 1:
-            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
-            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
-            x_ = self.norm(x_)
-            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads,
-                                     C // self.num_heads).permute(
-                                         2, 0, 3, 1, 4)
-        else:
-            kv = self.kv(x).reshape(B, -1, 2, self.num_heads,
-                                    C // self.num_heads).permute(
-                                        2, 0, 3, 1, 4)
-        k, v = kv[0], kv[1]
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-
-        return x
-
 
 class Attention(BaseModule):
     """Window based multi-head self-attention (W-MSA) module with relative
@@ -341,7 +282,7 @@ class Block(nn.Module):
 
 
 class TransformerEncoderLayer(BaseModule):
-    """Implements one encoder layer in Vision Transformer.
+    """Implements one encoder layer in Twins.
 
     Args:
         embed_dims (int): The feature dimension.
@@ -359,6 +300,8 @@ class TransformerEncoderLayer(BaseModule):
             Defalut: dict(type='GELU').
         norm_cfg (dict): Config dict for normalization layer.
             Default: dict(type='LN').
+            # TODO: fix  sr_ratio
+        sr_ratio (float):
         batch_first (bool): Key, Query and Value are shape of
             (batch, n, embed_dim)
             or (n, batch, embed_dim). Default: True.
