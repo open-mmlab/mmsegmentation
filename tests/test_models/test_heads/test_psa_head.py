8cc436ad661a86040adf758565412d21ce37cb37
MengzhangLI <mcmong@pku.edu.cn>
2021-10-28 23:35:03 +0800

    smaller input & channels of unittest

--- tests/test_models/test_heads/test_psa_head.py
+++ tests/test_models/test_heads/test_psa_head.py
@@ -11,112 +11,112 @@ def test_psa_head():
     with pytest.raises(AssertionError):
         # psa_type must be in 'bi-direction', 'collect', 'distribute'
         PSAHead(
-            in_channels=32,
-            channels=16,
+            in_channels=4,
+            channels=2,
             num_classes=19,
-            mask_size=(39, 39),
+            mask_size=(13, 13),
             psa_type='gather')
 
     # test no norm_cfg
     head = PSAHead(
-        in_channels=32, channels=16, num_classes=19, mask_size=(39, 39))
+        in_channels=4, channels=2, num_classes=19, mask_size=(13, 13))
     assert not _conv_has_norm(head, sync_bn=False)
 
     # test with norm_cfg
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         norm_cfg=dict(type='SyncBN'))
     assert _conv_has_norm(head, sync_bn=True)
 
     # test 'bi-direction' psa_type
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32, channels=16, num_classes=19, mask_size=(39, 39))
+        in_channels=4, channels=2, num_classes=19, mask_size=(13, 13))
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'bi-direction' psa_type, shrink_factor=1
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         shrink_factor=1)
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'bi-direction' psa_type with soft_max
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         psa_softmax=True)
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'collect' psa_type
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         psa_type='collect')
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'collect' psa_type, shrink_factor=1
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         shrink_factor=1,
         psa_type='collect')
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'collect' psa_type, shrink_factor=1, compact=True
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         psa_type='collect',
         shrink_factor=1,
         compact=True)
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
 
     # test 'distribute' psa_type
-    inputs = [torch.randn(1, 32, 39, 39)]
+    inputs = [torch.randn(1, 4, 13, 13)]
     head = PSAHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
-        mask_size=(39, 39),
+        mask_size=(13, 13),
         psa_type='distribute')
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
-    assert outputs.shape == (1, head.num_classes, 39, 39)
+    assert outputs.shape == (1, head.num_classes, 13, 13)
--- tests/test_models/test_heads/test_setr_up_head.py
+++ tests/test_models/test_heads/test_setr_up_head.py
@@ -15,12 +15,12 @@ def test_setr_up_head(capsys):
     with pytest.raises(AssertionError):
         # in_channels must be int type and in_channels must be same
         # as embed_dim.
-        SETRUPHead(in_channels=(32, 32), channels=16, num_classes=19)
+        SETRUPHead(in_channels=(4, 4), channels=2, num_classes=19)
 
     # test init_cfg of head
     head = SETRUPHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         norm_cfg=dict(type='SyncBN'),
         num_classes=19,
         init_cfg=dict(type='Kaiming'))
@@ -28,11 +28,11 @@ def test_setr_up_head(capsys):
 
     # test inference of Naive head
     # the auxiliary head of Naive head is same as Naive head
-    img_size = (32, 32)
-    patch_size = 16
+    img_size = (4, 4)
+    patch_size = 2
     head = SETRUPHead(
-        in_channels=32,
-        channels=16,
+        in_channels=4,
+        channels=2,
         num_classes=19,
         num_convs=1,
         up_scale=4,
@@ -42,14 +42,14 @@ def test_setr_up_head(capsys):
     h, w = img_size[0] // patch_size, img_size[1] // patch_size
 
     # Input square NCHW format feature information
-    x = [torch.randn(1, 32, h, w)]
+    x = [torch.randn(1, 4, h, w)]
     if torch.cuda.is_available():
         head, x = to_cuda(head, x)
     out = head(x)
     assert out.shape == (1, head.num_classes, h * 4, w * 4)
 
     # Input non-square NCHW format feature information
-    x = [torch.randn(1, 32, h, w * 2)]
+    x = [torch.randn(1, 4, h, w * 2)]
     if torch.cuda.is_available():
         head, x = to_cuda(head, x)
     out = head(x)
