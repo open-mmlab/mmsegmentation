58149339662c2cd2ba9874ff0c9d318b28e59323
MengzhangLI <mcmong@pku.edu.cn>
2021-10-28 15:51:28 +0800

    fix single loss type

--- mmseg/models/decode_heads/decode_head.py
+++ mmseg/models/decode_heads/decode_head.py
@@ -83,11 +83,11 @@ class BaseDecodeHead(BaseModule, metaclass=ABCMeta):
 
         self.ignore_index = ignore_index
         self.align_corners = align_corners
-        self.loss_decode = nn.ModuleList()
 
         if isinstance(loss_decode, dict):
-            self.loss_decode.append(build_loss(loss_decode))
+            self.loss_decode = build_loss(loss_decode)
         elif isinstance(loss_decode, (list, tuple)):
+            self.loss_decode = nn.ModuleList()
             for loss in loss_decode:
                 self.loss_decode.append(build_loss(loss))
         else:
@@ -242,19 +242,27 @@ class BaseDecodeHead(BaseModule, metaclass=ABCMeta):
         else:
             seg_weight = None
         seg_label = seg_label.squeeze(1)
-        for loss_decode in self.loss_decode:
-            if loss_decode.loss_name not in loss:
-                loss[loss_decode.loss_name] = loss_decode(
-                    seg_logit,
-                    seg_label,
-                    weight=seg_weight,
-                    ignore_index=self.ignore_index)
-            else:
-                loss[loss_decode.loss_name] += loss_decode(
-                    seg_logit,
-                    seg_label,
-                    weight=seg_weight,
-                    ignore_index=self.ignore_index)
+
+        if isinstance(self.loss_decode, nn.modules.container.ModuleList):
+            for loss_decode in self.loss_decode:
+                if loss_decode.loss_name not in loss:
+                    loss[loss_decode.loss_name] = loss_decode(
+                        seg_logit,
+                        seg_label,
+                        weight=seg_weight,
+                        ignore_index=self.ignore_index)
+                else:
+                    loss[loss_decode.loss_name] += loss_decode(
+                        seg_logit,
+                        seg_label,
+                        weight=seg_weight,
+                        ignore_index=self.ignore_index)
+        else:
+            loss[self.loss_decode.loss_name] = self.loss_decode(
+                seg_logit,
+                seg_label,
+                weight=seg_weight,
+                ignore_index=self.ignore_index)
 
         loss['acc_seg'] = accuracy(seg_logit, seg_label)
         return loss
