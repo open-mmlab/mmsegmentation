a53c7e691de7e25014718c65a571529906de9b28
RockeyCoss <1350291943@qq.com>
2021-11-08 13:01:09 -0800

    [Feature] add focal loss

---
+++ mmseg/models/losses/focal_loss.py
@@ -0,0 +1,251 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# Modified from https://github.com/open-mmlab/mmdetection
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.ops import sigmoid_focal_loss as _sigmoid_focal_loss
+
+from ..builder import LOSSES
+from .utils import weight_reduce_loss
+
+
+# This method is only for debugging
+def py_sigmoid_focal_loss(pred,
+                          target,
+                          weight=None,
+                          gamma=2.0,
+                          alpha=0.5,
+                          reduction='mean',
+                          avg_factor=None):
+    """PyTorch version of `Focal Loss <https://arxiv.org/abs/1708.02002>`_.
+
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, C), C is the
+            number of classes
+        target (torch.Tensor): The learning label of the prediction with
+            shape (N, C)
+        weight (torch.Tensor, optional): Sample-wise loss weight.
+        gamma (float, optional): The gamma for calculating the modulating
+            factor. Defaults to 2.0.
+        alpha (float, optional): A balanced form for Focal Loss.
+            Defaults to 0.5.
+        reduction (str, optional): The method used to reduce the loss into
+            a scalar. Defaults to 'mean'.
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+    """
+    pred_sigmoid = pred.sigmoid()
+    target = target.type_as(pred)
+    pt = (1 - pred_sigmoid) * target + pred_sigmoid * (1 - target)
+    focal_weight = (alpha * target + (1 - alpha) *
+                    (1 - target)) * pt.pow(gamma)
+    loss = F.binary_cross_entropy_with_logits(
+        pred, target, reduction='none') * focal_weight
+    if weight is not None:
+        if weight.shape != loss.shape:
+            if weight.size(0) == loss.size(0):
+                # For most cases, weight is of shape (num_priors, ),
+                #  which means it does not have the second axis num_class
+                weight = weight.view(-1, 1)
+            else:
+                # Sometimes, weight per anchor per class is also needed. e.g.
+                #  in FSAF. But it may be flattened of shape
+                #  (num_priors x num_class, ), while loss is still of shape
+                #  (num_priors, num_class).
+                assert weight.numel() == loss.numel()
+                weight = weight.view(loss.size(0), -1)
+        assert weight.ndim == loss.ndim
+    loss = weight_reduce_loss(loss, weight, reduction, avg_factor)
+    return loss
+
+
+def sigmoid_focal_loss(pred,
+                       target,
+                       weight=None,
+                       gamma=2.0,
+                       alpha=0.5,
+                       reduction='mean',
+                       avg_factor=None):
+    r"""A warpper of cuda version `Focal Loss
+    <https://arxiv.org/abs/1708.02002>`_.
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, C), C is the number
+            of classes.
+        target (torch.Tensor): The learning label of the prediction. It's shape
+            should be (N, )
+        weight (torch.Tensor, optional): Sample-wise loss weight.
+        gamma (float, optional): The gamma for calculating the modulating
+            factor. Defaults to 2.0.
+        alpha (float, optional): A balanced form for Focal Loss.
+            Defaults to 0.5.
+        reduction (str, optional): The method used to reduce the loss into
+            a scalar. Defaults to 'mean'. Options are "none", "mean" and "sum".
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+    """
+    # Function.apply does not accept keyword arguments, so the decorator
+    # "weighted_loss" is not applicable
+    loss = _sigmoid_focal_loss(pred.contiguous(), target.contiguous(), gamma,
+                               alpha, None, 'none')
+    if weight is not None:
+        if weight.shape != loss.shape:
+            if weight.size(0) == loss.size(0):
+                # For most cases, weight is of shape (num_priors, ),
+                #  which means it does not have the second axis num_class
+                weight = weight.view(-1, 1)
+            else:
+                # Sometimes, weight per anchor per class is also needed. e.g.
+                #  in FSAF. But it may be flattened of shape
+                #  (num_priors x num_class, ), while loss is still of shape
+                #  (num_priors, num_class).
+                assert weight.numel() == loss.numel()
+                weight = weight.view(loss.size(0), -1)
+        assert weight.ndim == loss.ndim
+    loss = weight_reduce_loss(loss, weight, reduction, avg_factor)
+    return loss
+
+
+@LOSSES.register_module()
+class FocalLoss(nn.Module):
+
+    def __init__(self,
+                 use_sigmoid=True,
+                 gamma=2.0,
+                 alpha=0.5,
+                 reduction='mean',
+                 loss_weight=1.0,
+                 loss_name='loss_focal'):
+        """`Focal Loss <https://arxiv.org/abs/1708.02002>`_
+        Args:
+            use_sigmoid (bool, optional): Whether to the prediction is
+                used for sigmoid or softmax. Defaults to True.
+            gamma (float, optional): The gamma for calculating the modulating
+                factor. Defaults to 2.0.
+            alpha (float, optional): A balanced form for Focal Loss.
+                Defaults to 0.5.
+            reduction (str, optional): The method used to reduce the loss into
+                a scalar. Defaults to 'mean'. Options are "none", "mean" and
+                "sum".
+            loss_weight (float, optional): Weight of loss. Defaults to 1.0.
+            loss_name (str, optional): Name of the loss item. If you want this
+                loss item to be included into the backward graph, `loss_` must
+                be the prefix of the name. Defaults to 'loss_focal'.
+        """
+        super(FocalLoss, self).__init__()
+        assert use_sigmoid is True, \
+            'AssertionError: Only sigmoid focal loss supported now.'
+        assert reduction in ('none', 'mean', 'sum'), \
+            "AssertionError: reduction should be 'none', 'mean' or " \
+            "'sum'"
+        assert isinstance(alpha, float), \
+            'AssertionError: alpha should be of type float'
+        assert isinstance(gamma, float), \
+            'AssertionError: gamma should be of type float'
+        assert isinstance(loss_weight, float), \
+            'AssertionError: loss_weight should be of type float'
+        assert isinstance(loss_name, str), \
+            'AssertionError: loss_name should be of type str'
+        self.use_sigmoid = use_sigmoid
+        self.gamma = gamma
+        self.alpha = alpha
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+        self._loss_name = loss_name
+
+    def forward(self,
+                pred,
+                target,
+                weight=None,
+                avg_factor=None,
+                reduction_override=None,
+                **kwargs):
+        """Forward function.
+
+        Args:
+            pred (torch.Tensor): The prediction with shape
+                (N, C) where C = number of classes, or
+                (N, C, d_1, d_2, ..., d_K) with K≥1 in the
+                case of K-dimensional loss.
+            target (torch.Tensor): The ground truth. If containing class
+                indices, shape (N) where each value is 0≤targets[i]≤C−1,
+                or (N, d_1, d_2, ..., d_K) with K≥1 in the case of
+                K-dimensional loss. If containing class probabilities,
+                same shape as the input.
+            weight (torch.Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to
+                average the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used
+                to override the original reduction method of the loss.
+                Options are "none", "mean" and "sum".
+        Returns:
+            torch.Tensor: The calculated loss
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum'), \
+            "AssertionError: reduction should be 'none', 'mean' or " \
+            "'sum'"
+        assert pred.shape == target.shape or \
+               (pred.size(0) == target.size(0) and
+                pred.shape[2:] == target.shape[1:]), \
+               "The shape of pred doesn't match the shape of target"
+
+        none_reduce_shape = pred.shape
+
+        # transform the pred's shape into [N, C]
+        new_dim = [i for i in range(pred.dim()) if i != 1] + [1]
+        pred = pred.permute(*new_dim).contiguous()
+        pred = pred.view(-1, pred.size(-1))
+
+        if none_reduce_shape == target.shape:
+            # target with shape [B, C, d_1, d_2, ...]
+            # transform it's shape into [N, C]
+            target = target.permute(*new_dim).contiguous()
+            target = target.view(-1, target.size(-1))
+        else:
+            # target with shape [B, d_1, d_2, ...]
+            # transform it's shape into [N, ]
+            target = target.view(-1)
+
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        if self.use_sigmoid:
+            if torch.cuda.is_available() and pred.is_cuda:
+                if target.dim() > 1:
+                    target = target.argmax(dim=1)
+                calculate_loss_func = sigmoid_focal_loss
+            else:
+                num_classes = pred.size(1)
+                if target.dim() == 1:
+                    target = F.one_hot(target, num_classes=num_classes + 1)
+                    target = target[:, :num_classes]
+                calculate_loss_func = py_sigmoid_focal_loss
+
+            loss_cls = self.loss_weight * calculate_loss_func(
+                pred,
+                target,
+                weight,
+                gamma=self.gamma,
+                alpha=self.alpha,
+                reduction=reduction,
+                avg_factor=avg_factor)
+
+            if reduction == 'none':
+                loss_cls = loss_cls.view(*none_reduce_shape).contiguous()
+
+        else:
+            raise NotImplementedError
+        return loss_cls
+
+    @property
+    def loss_name(self):
+        """Loss Name.
+
+        This function must be implemented and will return the name of this
+        loss function. This name will be used to combine different loss items
+        by simple sum operation. In addition, if you want this loss item to be
+        included into the backward graph, `loss_` must be the prefix of the
+        name.
+        Returns:
+            str: The name of this loss item.
+        """
+        return self._loss_name
---
+++ tests/test_models/test_losses/test_focal_loss.py
@@ -0,0 +1,131 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import pytest
+import torch
+import torch.nn.functional as F
+
+from mmseg.models import build_loss
+
+
+# test focal loss with use_sigmoid=False
+def test_use_sigmoid():
+    # can't init with use_sigmoid=True
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', use_sigmoid=False)
+        build_loss(loss_cfg)
+
+    # can't forward with use_sigmoid=True
+    with pytest.raises(NotImplementedError):
+        loss_cfg = dict(type='FocalLoss', use_sigmoid=True)
+        focal_loss = build_loss(loss_cfg)
+        focal_loss.use_sigmoid = False
+        fake_pred = torch.rand(3, 2, 2, 2)
+        fake_target = torch.rand(3, 2, 2)
+        focal_loss(fake_pred, fake_target)
+
+
+# reduction type must be 'none', 'mean' or 'sum'
+def test_wrong_reduction_type():
+    # can't init with wrong reduction
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', reduction='test')
+        build_loss(loss_cfg)
+
+    # can't forward with wrong reduction override
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss')
+        focal_loss = build_loss(loss_cfg)
+        fake_pred = torch.rand(3, 2, 2, 2)
+        fake_target = torch.rand(3, 2, 2)
+        focal_loss(fake_pred, fake_target, reduction_override='test')
+
+
+# test focal loss can handle input parameters with
+# unacceptable types
+def test_unacceptable_parameters():
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', gamma='test')
+        build_loss(loss_cfg)
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', alpha='test')
+        build_loss(loss_cfg)
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', loss_weight='test')
+        build_loss(loss_cfg)
+    with pytest.raises(AssertionError):
+        loss_cfg = dict(type='FocalLoss', loss_name=123)
+        build_loss(loss_cfg)
+
+
+# test if focal loss can be correctly initialize
+def test_init_focal_loss():
+    loss_cfg = dict(
+        type='FocalLoss',
+        use_sigmoid=True,
+        gamma=3.0,
+        alpha=3.0,
+        reduction='sum')
+    focal_loss = build_loss(loss_cfg)
+    assert focal_loss.use_sigmoid is True
+    assert focal_loss.gamma == 3.0
+    assert focal_loss.alpha == 3.0
+    assert focal_loss.reduction == 'sum'
+    assert focal_loss.loss_weight == 1.0
+    assert focal_loss.loss_name == 'loss_focal'
+
+
+# test reduction override
+def test_reduction_override():
+    loss_cfg = dict(type='FocalLoss')
+    focal_loss = build_loss(loss_cfg)
+    fake_pred = torch.rand(3, 2, 2, 2)
+    fake_target = torch.randint(0, 2, (3, 2, 2))
+    focal_loss(fake_pred, fake_target, reduction_override='sum')
+
+
+# test wrong pred and target shape
+def test_wrong_pred_and_target_shape():
+    loss_cfg = dict(type='FocalLoss')
+    focal_loss = build_loss(loss_cfg)
+    fake_pred = torch.rand(3, 2, 2, 2)
+    fake_target = torch.rand(3, 2, 2, 3)
+    with pytest.raises(AssertionError):
+        focal_loss(fake_pred, fake_target)
+
+
+# test forward with different shape of target
+def test_forward_with_different_shape_of_target():
+    loss_cfg = dict(type='FocalLoss')
+    focal_loss = build_loss(loss_cfg)
+
+    fake_pred = torch.rand(3, 2, 2, 2)
+    fake_target = F.one_hot(torch.randint(0, 2, (3, 2, 2)), num_classes=2)
+    focal_loss(fake_pred, fake_target)
+
+    fake_target = torch.randint(0, 2, (3, 2, 2))
+    focal_loss(fake_pred, fake_target)
+
+
+# test forward with weight
+def test_forward_with_weight():
+    loss_cfg = dict(type='FocalLoss')
+    focal_loss = build_loss(loss_cfg)
+    fake_pred = torch.rand(3, 2, 2, 2)
+    fake_target = torch.randint(0, 2, (3, 2, 2))
+    weight = torch.rand(12, 1)
+    focal_loss(fake_pred, fake_target, weight=weight)
+
+    weight = torch.rand(12)
+    focal_loss(fake_pred, fake_target, weight=weight)
+
+    weight = torch.rand(12 * 2)
+    focal_loss(fake_pred, fake_target, weight=weight)
+
+
+# test none reduction type
+def test_none_reduction_type():
+    loss_cfg = dict(type='FocalLoss', reduction='none')
+    focal_loss = build_loss(loss_cfg)
+    fake_pred = torch.rand(3, 2, 2, 2)
+    fake_target = torch.randint(0, 2, (3, 2, 2))
+    loss = focal_loss(fake_pred, fake_target)
+    assert loss.shape == fake_pred.shape
