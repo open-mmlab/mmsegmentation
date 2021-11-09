a4e0037ff98758d3ccf230d767b4a40c15f98303
MengzhangLI <mcmong@pku.edu.cn>
2021-11-09 16:06:18 +0800

    change config_names & delete redundant keys

--- configs/bisenetv1/bisenetv1_r101-d32_lr5e-3_in1k-pre_4x4_512x512_160k_coco-stuff164k.py
+++ configs/bisenetv1/bisenetv1_r101-d32_lr5e-3_in1k-pre_4x4_512x512_160k_coco-stuff164k.py
@@ -1,4 +1,4 @@
-_base_ = './bisenetv1_r101-d32_lr5e3_4x4_512x512_160k_coco-stuff164k.py'
+_base_ = './bisenetv1_r101-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k.py'
 model = dict(
     backbone=dict(
         backbone_cfg=dict(
--- configs/bisenetv1/bisenetv1_r50-d32_lr5e-3_in1k-pre_4x4_512x512_160k_coco-stuff164k.py
+++ configs/bisenetv1/bisenetv1_r50-d32_lr5e-3_in1k-pre_4x4_512x512_160k_coco-stuff164k.py
@@ -1,4 +1,4 @@
-_base_ = './bisenetv1_r50-d32_lr5e3_4x4_512x512_160k_coco-stuff164k.py'
+_base_ = './bisenetv1_r50-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k.py'
 
 model = dict(
     backbone=dict(
