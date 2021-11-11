0e603d32ca0c85c92bcf6ada7f484ede9e46a1c3
MengzhangLI <mcmong@pku.edu.cn>
2021-11-11 13:33:09 +0800

    remove fp16 folder

--- configs/fcn/fcn_r101-d8_fp16_512x1024_80k_cityscapes.py
+++ configs/fcn/fcn_r101-d8_fp16_512x1024_80k_cityscapes.py
@@ -1,4 +1,4 @@
-_base_ = '../deeplabv3/deeplabv3_r101-d8_512x1024_80k_cityscapes.py'
+_base_ = './fcn_r101-d8_512x1024_80k_cityscapes.py'
 # fp16 settings
 optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
 # fp16 placeholder
