3d60c64a61cc68af958de24984373fb995786025
linxinyang <linxinyang@meituan.com>
2021-11-11 22:12:07 +0800

    debug

--- tools/model_converters/twins2mmseg.py
+++ tools/model_converters/twins2mmseg.py
@@ -28,8 +28,8 @@ def convert_vit(ckpt):
                                   dim=0)
             elif 'attn.proj.' in k:
                 new_k = k.replace('proj.', 'attn.out_proj.')
-            elif '.proj.' in k:
-                new_k = k.replace('.proj.', '.projection.')
+            elif 'patch_embeds.'in k and 'proj.' in k:
+                new_k = k.replace('proj.', 'projection.')
             else:
                 new_k = k
         else:
