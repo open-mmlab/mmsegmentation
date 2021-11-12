924d8acb8f5886bb3c5b4369356c21a1c6cc95a6
MengzhangLI <mcmong@pku.edu.cn>
2021-11-12 14:07:05 +0800

    remove redundancy in bisenetv1_r50-d32

--- configs/bisenetv1/bisenetv1_r50-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k.py
+++ configs/bisenetv1/bisenetv1_r50-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k.py
@@ -3,40 +3,16 @@ _base_ = [
     '../_base_/datasets/coco-stuff164k.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k.py'
 ]
-norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
-    type='EncoderDecoder',
     backbone=dict(
-        type='BiSeNetV1',
         context_channels=(512, 1024, 2048),
         spatial_channels=(256, 256, 256, 512),
         out_channels=1024,
         backbone_cfg=dict(type='ResNet', depth=50)),
-    decode_head=dict(
-        type='FCNHead',
-        in_channels=1024,
-        in_index=0,
-        channels=1024,
-        num_classes=171),
+    decode_head=dict(in_channels=1024, channels=1024, num_classes=171),
     auxiliary_head=[
-        dict(
-            type='FCNHead',
-            in_channels=512,
-            channels=256,
-            num_convs=1,
-            num_classes=171,
-            in_index=1,
-            norm_cfg=norm_cfg,
-            concat_input=False),
-        dict(
-            type='FCNHead',
-            in_channels=512,
-            channels=256,
-            num_convs=1,
-            num_classes=171,
-            in_index=2,
-            norm_cfg=norm_cfg,
-            concat_input=False),
+        dict(in_channels=512, channels=256, num_classes=171),
+        dict(in_channels=512, channels=256, num_classes=171),
     ])
 lr_config = dict(warmup='linear', warmup_iters=1000)
-optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(lr=0.005)
