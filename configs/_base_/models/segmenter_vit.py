18d32ad28db4346fd8a1838f7b31f16ec2f741b9
rstrudel <rstrudel@gmail.com>
2021-11-26 13:26:23 +0100

    apply precommit

--- configs/_base_/models/segmenter_vit.py
+++ configs/_base_/models/segmenter_vit.py
@@ -1,10 +1,10 @@
 # model settings
-backbone_norm_cfg = dict(type="LN", eps=1e-6, requires_grad=True)
+backbone_norm_cfg = dict(type='LN', eps=1e-6, requires_grad=True)
 model = dict(
-    type="EncoderDecoder",
-    pretrained="pretrain/vit_small_p16_384.pth",
+    type='EncoderDecoder',
+    pretrained='pretrain/vit_small_p16_384.pth',
     backbone=dict(
-        type="VisionTransformer",
+        type='VisionTransformer',
         img_size=(512, 512),
         patch_size=16,
         in_channels=3,
@@ -17,17 +17,18 @@ model = dict(
         final_norm=True,
         norm_cfg=backbone_norm_cfg,
         with_cls_token=True,
-        interpolate_mode="bicubic",
+        interpolate_mode='bicubic',
     ),
     decode_head=dict(
-        type="SegmenterLinearHead",
+        type='SegmenterLinearHead',
         in_channels=384,
         channels=384,
         num_classes=19,
         dropout_ratio=0.0,
         in_index=-1,
         align_corners=False,
-        loss_decode=dict(type="CrossEntropyLoss", use_sigmoid=False, loss_weight=1.0),
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
     ),
-    test_cfg=dict(mode="slide", crop_size=(512, 512), stride=(480, 480)),
+    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(480, 480)),
 )
--- mmseg/models/decode_heads/segmenter_mask_head.py
+++ mmseg/models/decode_heads/segmenter_mask_head.py
@@ -12,25 +12,25 @@ from .decode_head import BaseDecodeHead
 
 @HEADS.register_module()
 class SegmenterMaskTransformerHead(BaseDecodeHead):
+
     def __init__(
-        self,
-        in_channels,
-        num_layers,
-        num_heads,
-        embed_dims,
-        mlp_ratio=4,
-        drop_path_rate=0.1,
-        drop_rate=0.0,
-        attn_drop_rate=0.0,
-        num_fcs=2,
-        qkv_bias=True,
-        act_cfg=dict(type="GELU"),
-        norm_cfg=dict(type="LN"),
-        **kwargs,
+            self,
+            in_channels,
+            num_layers,
+            num_heads,
+            embed_dims,
+            mlp_ratio=4,
+            drop_path_rate=0.1,
+            drop_rate=0.0,
+            attn_drop_rate=0.0,
+            num_fcs=2,
+            qkv_bias=True,
+            act_cfg=dict(type='GELU'),
+            norm_cfg=dict(type='LN'),
+            **kwargs,
     ):
         super(SegmenterMaskTransformerHead, self).__init__(
-            in_channels=in_channels, **kwargs
-        )
+            in_channels=in_channels, **kwargs)
 
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, num_layers)]
         self.layers = ModuleList()
@@ -48,17 +48,19 @@ class SegmenterMaskTransformerHead(BaseDecodeHead):
                     act_cfg=act_cfg,
                     norm_cfg=norm_cfg,
                     batch_first=True,
-                )
-            )
+                ))
 
         self.proj_dec = nn.Linear(in_channels, embed_dims)
 
-        self.cls_emb = nn.Parameter(torch.randn(1, self.num_classes, embed_dims))
+        self.cls_emb = nn.Parameter(
+            torch.randn(1, self.num_classes, embed_dims))
         self.proj_patch = nn.Parameter(torch.randn(embed_dims, embed_dims))
         self.proj_classes = nn.Parameter(torch.randn(embed_dims, embed_dims))
 
-        _, self.decoder_norm = build_norm_layer(norm_cfg, embed_dims, postfix=1)
-        _, self.mask_norm = build_norm_layer(norm_cfg, self.num_classes, postfix=2)
+        _, self.decoder_norm = build_norm_layer(
+            norm_cfg, embed_dims, postfix=1)
+        _, self.mask_norm = build_norm_layer(
+            norm_cfg, self.num_classes, postfix=2)
 
         trunc_normal_(self.cls_emb, std=0.02)
         trunc_normal_(self.proj_patch, std=0.02)
@@ -67,7 +69,7 @@ class SegmenterMaskTransformerHead(BaseDecodeHead):
     def forward(self, inputs):
         x = self._transform_inputs(inputs)
         GS = x.shape[-1]
-        x = rearrange(x, "b n h w -> b (h w) n")
+        x = rearrange(x, 'b n h w -> b (h w) n')
 
         x = self.proj_dec(x)
         cls_emb = self.cls_emb.expand(x.size(0), -1, -1)
@@ -76,7 +78,8 @@ class SegmenterMaskTransformerHead(BaseDecodeHead):
             x = layer(x)
         x = self.decoder_norm(x)
 
-        patches, cls_seg_feat = x[:, : -self.num_classes], x[:, -self.num_classes :]
+        patches, cls_seg_feat = x[:, :-self.num_classes], x[:,
+                                                            -self.num_classes:]
         patches = patches @ self.proj_patch
         cls_seg_feat = cls_seg_feat @ self.proj_classes
 
@@ -85,6 +88,6 @@ class SegmenterMaskTransformerHead(BaseDecodeHead):
 
         masks = patches @ cls_seg_feat.transpose(1, 2)
         masks = self.mask_norm(masks)
-        masks = rearrange(masks, "b (h w) n -> b n h w", h=int(GS))
+        masks = rearrange(masks, 'b (h w) n -> b n h w', h=int(GS))
 
         return masks
