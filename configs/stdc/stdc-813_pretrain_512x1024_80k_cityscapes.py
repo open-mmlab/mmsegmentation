e646dd0dd0dbb3231daaece66be0dcb8235268ba
MengzhangLI <mcmong@pku.edu.cn>
2021-11-27 16:37:38 +0800

    delete redundant keys in config

--- configs/stdc/stdc-813_pretrain_512x1024_80k_cityscapes.py
+++ configs/stdc/stdc-813_pretrain_512x1024_80k_cityscapes.py
@@ -1,10 +1,7 @@
-_base_ = [
-    '../_base_/models/stdc.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
-]
+_base_ = './stdc-813_512x1024_80k_cityscapes.py'
 model = dict(
     backbone=dict(
         stdc_cfg=dict(
             init_cfg=dict(
                 type='Pretrained',
-                checkpoint='./pretrained/pretrained_stdc-813.pth'), )))
+                checkpoint='./pretrained/pretrained_stdc-813.pth'))))
