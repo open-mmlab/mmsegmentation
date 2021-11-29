68bdb88fa6370f135ed6be6a7255eddc9c5874cd
linxinyang <linxinyang@meituan.com>
2021-11-29 12:33:55 +0800

    git

--- tools/model_converters/twins2mmseg.py
+++ tools/model_converters/twins2mmseg.py
@@ -42,6 +42,8 @@ def convert_vit(args, ckpt):
             else:
                 if 'attn.proj.' in k:
                     k_lst = k.split('.')
+                    import pdb
+                    pdb.set_trace()
                     if int(k_lst[3]) % 2 == 1:
                         new_k = k.replace('proj.', 'attn.out_proj.')
                     else:
