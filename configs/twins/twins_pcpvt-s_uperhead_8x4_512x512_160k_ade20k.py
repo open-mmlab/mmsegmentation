0c38a41461b09097b049d91bb15cad64736a9319
linxinyang <linxinyang@meituan.com>
2021-11-30 14:07:48 +0800

    git

--- configs/twins/twins_pcpvt-s_uperhead_8x4_512x512_160k_ade20k.py
+++ configs/twins/twins_pcpvt-s_uperhead_8x4_512x512_160k_ade20k.py
@@ -33,5 +33,5 @@ model = dict(
             checkpoint='/home/hadoop-automl/cephfs/'
             'data/linxinyang/models/twins/'
             'mmsegPR_convert_backbone_'
-            'pcpvt_s.pth')))
+            'svt_s.pth')))
 evaluation = dict(interval=1000, metric='mIoU', pre_eval=True)
