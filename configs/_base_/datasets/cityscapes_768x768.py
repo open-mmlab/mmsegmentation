80557b16bfd4c7f955fceefb9d8703e2ec18c9e2
RockeyCoss <1350291943@qq.com>
2021-11-30 21:47:21 -0800

    remove redundant keys

--- configs/_base_/datasets/cityscapes_768x768.py
+++ configs/_base_/datasets/cityscapes_768x768.py
@@ -18,7 +18,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        img_scale=(2049, 1025),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
