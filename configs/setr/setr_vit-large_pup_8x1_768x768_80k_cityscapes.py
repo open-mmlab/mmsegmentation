29182206672d33a54ed10b8a258dff262e6e87f8
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-12-03 16:18:50 +0800

    [Feature] add setr cityscapes benchmark (#1087)
    * [Feature] add setr cityscapes benchmark
    
    * change pretrain
    
    * Update configs/_base_/datasets/cityscapes_768x768.py
    
    Co-authored-by: Junjun2016 <hejunjun@sjtu.edu.cn>
    
    * remove redundant keys
    
    * remove redundant keys
    
    * fix lint error
    
    * update readme
    
    * update pretrain
    
    Co-authored-by: Junjun2016 <hejunjun@sjtu.edu.cn>

--- configs/setr/setr_vit-large_pup_8x1_768x768_80k_cityscapes.py
+++ configs/setr/setr_vit-large_pup_8x1_768x768_80k_cityscapes.py
@@ -1,27 +1,27 @@
 _base_ = [
-    '../_base_/models/setr_naive.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+    '../_base_/models/setr_pup.py', '../_base_/datasets/cityscapes_768x768.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
+
 norm_cfg = dict(type='SyncBN', requires_grad=True)
+crop_size = (768, 768)
 model = dict(
     pretrained=None,
     backbone=dict(
-        img_size=(512, 512),
         drop_rate=0.,
         init_cfg=dict(type='Pretrained', checkpoint='mmcls://vit_large_p16')),
-    decode_head=dict(num_classes=150),
     auxiliary_head=[
         dict(
             type='SETRUPHead',
             in_channels=1024,
             channels=256,
             in_index=0,
-            num_classes=150,
+            num_classes=19,
             dropout_ratio=0,
             norm_cfg=norm_cfg,
-            act_cfg=dict(type='ReLU'),
             num_convs=2,
-            kernel_size=1,
+            up_scale=4,
+            kernel_size=3,
             align_corners=False,
             loss_decode=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
@@ -30,12 +30,12 @@ model = dict(
             in_channels=1024,
             channels=256,
             in_index=1,
-            num_classes=150,
+            num_classes=19,
             dropout_ratio=0,
             norm_cfg=norm_cfg,
-            act_cfg=dict(type='ReLU'),
             num_convs=2,
-            kernel_size=1,
+            up_scale=4,
+            kernel_size=3,
             align_corners=False,
             loss_decode=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
@@ -44,23 +44,20 @@ model = dict(
             in_channels=1024,
             channels=256,
             in_index=2,
-            num_classes=150,
+            num_classes=19,
             dropout_ratio=0,
             norm_cfg=norm_cfg,
-            act_cfg=dict(type='ReLU'),
             num_convs=2,
-            kernel_size=1,
+            up_scale=4,
+            kernel_size=3,
             align_corners=False,
             loss_decode=dict(
                 type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4))
     ],
-    test_cfg=dict(mode='slide', crop_size=(512, 512), stride=(341, 341)),
-)
+    test_cfg=dict(mode='slide', crop_size=crop_size, stride=(512, 512)))
 
 optimizer = dict(
-    lr=0.01,
     weight_decay=0.0,
     paramwise_cfg=dict(custom_keys={'head': dict(lr_mult=10.)}))
 
-# num_gpus: 8 -> batch_size: 16
-data = dict(samples_per_gpu=2)
+data = dict(samples_per_gpu=1)
