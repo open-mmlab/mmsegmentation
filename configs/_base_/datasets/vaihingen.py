0958ea056f995dc2c2da3bdf2d1dab8c0e826910
kingdrone <kingdrone@whu.edu.cn>
2021-12-05 20:00:08 +0800

    add vaihingen dataset

--- configs/_base_/datasets/vaihingen.py
+++ configs/_base_/datasets/vaihingen.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'ISPRSDataset'
-data_root = 'data/potsdam'
+data_root = 'data/vaihingen'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
