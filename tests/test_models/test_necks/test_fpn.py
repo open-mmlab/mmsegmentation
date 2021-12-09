0ad0303ebc6100089f5a6557519df8f2ffeaab28
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2021-12-09 12:12:31 +0800

    [Fix] Remove the inplace operation in  uper_head and fpn_neck (#1103)
    * [Fix] Remove the inplace operation in  uper_head
    
    * remove the inplace operation in fpn neck
    
    * fix conflict
    
    * increase the coverage

--- tests/test_models/test_necks/test_fpn.py
+++ tests/test_models/test_necks/test_fpn.py
@@ -17,3 +17,14 @@ def test_fpn():
     assert outputs[1].shape == torch.Size([1, 64, 28, 28])
     assert outputs[2].shape == torch.Size([1, 64, 14, 14])
     assert outputs[3].shape == torch.Size([1, 64, 7, 7])
+
+    fpn = FPN(
+        in_channels,
+        64,
+        len(in_channels),
+        upsample_cfg=dict(mode='nearest', scale_factor=2.0))
+    outputs = fpn(inputs)
+    assert outputs[0].shape == torch.Size([1, 64, 56, 56])
+    assert outputs[1].shape == torch.Size([1, 64, 28, 28])
+    assert outputs[2].shape == torch.Size([1, 64, 14, 14])
+    assert outputs[3].shape == torch.Size([1, 64, 7, 7])
