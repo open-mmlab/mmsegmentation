587439f6251ee6177fae4e95f03d35799cf59733
RockeyCoss <1350291943@qq.com>
2021-12-12 04:18:40 -0800

    change the parameter's name

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -22,9 +22,9 @@ def parse_args():
         '--log-interval', type=int, default=50, help='interval of logging')
     parser.add_argument(
         '--work-dir',
-        help=('if specified, the evaluation metric results will be dumped'
+        help=('if specified, the results will be dumped'
               'into the directory as json'))
-    parser.add_argument('--repeat_times', type=int, default=1)
+    parser.add_argument('--repeat-times', type=int, default=1)
     args = parser.parse_args()
     return args
 
