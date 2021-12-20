b0a69209d642b61e138cd969cc4f6a75a0e7544d
rstrudel <rstrudel@gmail.com>
2021-12-20 12:13:22 +0100

    configs: add vit t/b/l

--- configs/segmenter/segmenter_vit-l_mask_8x1_512x512_160k_ade20k.py
+++ configs/segmenter/segmenter_vit-l_mask_8x1_512x512_160k_ade20k.py
@@ -4,17 +4,35 @@ _base_ = [
     '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k.py',
 ]
+
 backbone_norm_cfg = dict(type='LN', eps=1e-6, requires_grad=True)
 model = dict(
-    backbone=dict(img_size=(512, 512)),
+    pretrained='pretrain/vit_large_p16_384.pth',
+    backbone=dict(
+        type='VisionTransformer',
+        img_size=(512, 512),
+        patch_size=16,
+        in_channels=3,
+        embed_dims=1024,
+        num_layers=24,
+        num_heads=16,
+        drop_path_rate=0.1,
+        attn_drop_rate=0.0,
+        drop_rate=0.0,
+        final_norm=True,
+        norm_cfg=backbone_norm_cfg,
+        with_cls_token=True,
+        interpolate_mode='bicubic',
+    ),
     decode_head=dict(
-        type='FCNHead',
-        in_channels=384,
-        channels=384,
-        num_convs=0,
-        dropout_ratio=0.0,
-        concat_input=False,
+        type='SegmenterMaskTransformerHead',
+        in_channels=1024,
+        channels=1024,
         num_classes=150,
+        num_layers=2,
+        num_heads=16,
+        embed_dims=1024,
+        dropout_ratio=0.0,
         loss_decode=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
     ),
@@ -23,8 +41,6 @@ model = dict(
 optimizer = dict(lr=0.001, weight_decay=0.0)
 
 # TODO: handle img_norm_cfg
-# img_norm_cfg = dict(mean=[127.5, 127.5, 127.5],
-# std=[127.5, 127.5, 127.5], to_rgb=True)
 img_norm_cfg = dict(
     mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
 # crop_size = (512, 512)
