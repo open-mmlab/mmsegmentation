63b13cd5ff731a564b8dd1c44c077d7b084e256c
MengzhangLI <mcmong@pku.edu.cn>
2022-01-07 18:05:07 +0800

    fix unittest

--- tests/test_models/test_heads/test_segmenter_mask_head.py
+++ tests/test_models/test_heads/test_segmenter_mask_head.py
@@ -7,16 +7,17 @@ from .utils import _conv_has_norm, to_cuda
 
 def test_uper_head():
     head = SegmenterMaskTransformerHead(
-        in_channels=192,
-        channels=192,
+        in_channels=2,
+        channels=2,
         num_classes=150,
         num_layers=2,
         num_heads=3,
         embed_dims=192,
         dropout_ratio=0.0)
     assert _conv_has_norm(head, sync_bn=True)
+    head.init_weights()
 
-    inputs = [torch.randn(1, 192, 32, 32)]
+    inputs = [torch.randn(1, 2, 32, 32)]
     if torch.cuda.is_available():
         head, inputs = to_cuda(head, inputs)
     outputs = head(inputs)
