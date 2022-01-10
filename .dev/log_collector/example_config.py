5f55fa5549116b73fac47043630f9aa00463cba5
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2022-01-10 11:56:29 +0800

    Update .dev/log_collector/example_config.py
    Co-authored-by: Miao Zheng <76149310+MeowZheng@users.noreply.github.com>

--- .dev/log_collector/example_config.py
+++ .dev/log_collector/example_config.py
@@ -1,14 +1,15 @@
 work_dir = '../mmsegmentation-master/work_dirs'
 metric = 'mIoU'
-# Don't specify the log_items and ignore_keywords at the same time
-log_items = [
-    'segformer_mit-b5_512x512_160k_ade20k_cnn_lr_with_warmup',
-    'segformer_mit-b5_512x512_160k_ade20k_cnn_no_wramup_lr',
-    'segformer_mit-b5_512x512_160k_ade20k_mit_trans_lr',
-    'segformer_mit-b5_512x512_160k_ade20k_swin_trans_lr'
-]
-# or
+# the folder containing `'segformer'` won't be collected
 ignore_keywords = ['segformer']
+
+# or specify the log files we would like to collect in `log_items`
+# log_items = [
+#     'segformer_mit-b5_512x512_160k_ade20k_cnn_lr_with_warmup',
+#     'segformer_mit-b5_512x512_160k_ade20k_cnn_no_wramup_lr',
+#     'segformer_mit-b5_512x512_160k_ade20k_mit_trans_lr',
+#     'segformer_mit-b5_512x512_160k_ade20k_swin_trans_lr'
+# ]
 
 # should not include metric
 other_info_keys = ['other_key']
