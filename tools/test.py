a2017bed8a84fe3e09fea904f143e46ee5ac7384
MengzhangLI <mcmong@pku.edu.cn>
2022-01-28 17:16:20 +0800

    fix typo

--- tools/test.py
+++ tools/test.py
@@ -155,8 +155,8 @@ def main():
     cfg.model.pretrained = None
     cfg.data.test.test_mode = True
 
-    if args.gpu_ids is not None:
-        cfg.gpu_ids = [args.gpu_ids]
+    if args.gpu_id is not None:
+        cfg.gpu_ids = [args.gpu_id]
 
     # init distributed env first, since logger depends on the dist info.
     if args.launcher == 'none':
