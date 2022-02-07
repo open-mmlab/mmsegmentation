1e14918241ec92d9903a1b31a3ebe3b91c03dcb1
MengzhangLI <mcmong@pku.edu.cn>
2022-02-07 18:08:58 +0800

    add load_annotations

--- mmseg/datasets/iSAID.py
+++ mmseg/datasets/iSAID.py
@@ -35,3 +35,45 @@ class ISAIDDataset(CustomDataset):
             ignore_index=255,
             **kwargs)
         assert osp.exists(self.img_dir)
+
+    def load_annotations(self, img_dir, img_suffix, ann_dir, seg_map_suffix,
+                         split):
+        """Load annotation from directory.
+
+        Args:
+            img_dir (str): Path to image directory
+            img_suffix (str): Suffix of images.
+            ann_dir (str|None): Path to annotation directory.
+            seg_map_suffix (str|None): Suffix of segmentation maps.
+            split (str|None): Split txt file. If split is specified, only file
+                with suffix in the splits will be loaded. Otherwise, all images
+                in img_dir/ann_dir will be loaded. Default: None
+
+        Returns:
+            list[dict]: All image info of dataset.
+        """
+
+        img_infos = []
+        if split is not None:
+            with open(split) as f:
+                for line in f:
+                    name = line.strip()
+                    img_name = name
+                    img_info = dict(filename=img_name + img_suffix)
+                    if ann_dir is not None:
+                        ann_name = name + '_instance_color_RGB'
+                        seg_map = ann_name + seg_map_suffix
+                        img_info['ann'] = dict(seg_map=seg_map)
+                    img_infos.append(img_info)
+        else:
+            for img in mmcv.scandir(img_dir, img_suffix, recursive=True):
+                img_info = dict(filename=img)
+                if ann_dir is not None:
+                    seg_img = img
+                    seg_map = seg_img.replace(
+                        img_suffix, '_instance_color_RGB' + seg_map_suffix)
+                    img_info['ann'] = dict(seg_map=seg_map)
+                img_infos.append(img_info)
+
+        print_log(f'Loaded {len(img_infos)} images', logger=get_root_logger())
+        return img_infos
