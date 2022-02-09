548eec95808d7fd2db2d2ebd877e33d6abdb77da
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2022-02-09 00:52:42 -0500

    [Improve] Use MMCV load_state_dict func in ViT/Swin (#1272)
    * [Improve] Use MMCV load_state_dict func in ViT/Swin
    
    * use CheckpointLoader instead

--- mmseg/models/backbones/swin.py
+++ mmseg/models/backbones/swin.py
@@ -11,7 +11,8 @@ from mmcv.cnn import build_norm_layer
 from mmcv.cnn.bricks.transformer import FFN, build_dropout
 from mmcv.cnn.utils.weight_init import (constant_init, trunc_normal_,
                                         trunc_normal_init)
-from mmcv.runner import BaseModule, ModuleList, _load_checkpoint
+from mmcv.runner import (BaseModule, CheckpointLoader, ModuleList,
+                         load_state_dict)
 from mmcv.utils import to_2tuple
 
 from ...utils import get_root_logger
@@ -678,7 +679,7 @@ class SwinTransformer(BaseModule):
                                                   f'specify `Pretrained` in ' \
                                                   f'`init_cfg` in ' \
                                                   f'{self.__class__.__name__} '
-            ckpt = _load_checkpoint(
+            ckpt = CheckpointLoader.load_checkpoint(
                 self.init_cfg['checkpoint'], logger=logger, map_location='cpu')
             if 'state_dict' in ckpt:
                 _state_dict = ckpt['state_dict']
@@ -732,7 +733,7 @@ class SwinTransformer(BaseModule):
                         nH2, L2).permute(1, 0).contiguous()
 
             # load state_dict
-            self.load_state_dict(state_dict, False)
+            load_state_dict(self, state_dict, strict=False, logger=logger)
 
     def forward(self, x):
         x, hw_shape = self.patch_embed(x)
