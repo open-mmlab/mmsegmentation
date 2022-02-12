1390ed919771d600c390c37c457386edffbeae1a
Robin Karlsson <robin.karlsson0@gmail.com>
2022-02-12 14:03:23 +0900

    FIX 18 class palette and strings

--- mmseg/datasets/a2d2_18cls.py
+++ mmseg/datasets/a2d2_18cls.py
@@ -35,69 +35,20 @@ class A2D2Dataset18Classes(CustomDataset):
     The ``img_suffix`` is fixed to '.png' and ``seg_map_suffix`` is
     fixed to '_18LabelTrainIds.png' for the 18 class A2D2 dataset.
 
-    Application of Cityscapes-like color palette:
-        RGB              Cityscapes         A2D2 (18 classes)
-        [128, 64, 128]   road           <-- Road
-        [244, 35, 232]   sidewalk       <-- Side walk
-        [70, 70, 70]     building       <-- Buildings
-        [102, 102, 156]  wall           <-- Curb stones
-        [190, 153, 153]  fence          <-- Irrelevant
-        [153, 153, 153]  pole           <-- Poles
-        [250, 170, 30]   traffic light  <-- Lane lines
-        [220, 220, 0]    traffic sign   <-- Traffic Info
-        [107, 142, 35]   vegetation     <-- Nature
-        [152, 251, 152]  terrain        <-- Parking area
-        [70, 130, 180]   sky            <-- Sky
-        [220, 20, 60]    person         <-- Pedestrians
-        [255, 0, 0]      rider          <-- Grid structure
-        [0, 0, 142]      car            <-- Cars
-        [0, 0, 70]       truck          <-- Trucks
-        [0, 60, 100]     bus            <-- Background
-        [0, 80, 100]     train          <-- Obstacles / trash on road
-        [0, 0, 230]      motorcycle     <-- Ego car
-        [119, 11, 32]    bicycle        <-- Small traffic participants
-
     Ref: https://www.a2d2.audi/a2d2/en/dataset.html
     """
 
-    CLASSES = ('Road'
-               'Sky'
-               'Nature'
-               'Poles'
-               'Cars'
-               'Lane lines'
-               'Buildings'
-               'Irrelevant'
-               'Traffic Info'
-               'Curb stones'
-               'Side walk'
-               'Trucks'
-               'Grid structure'
-               'Obstacles / trash on road'
-               'Pedestrians'
-               'Ego car'
-               'Small traffic participants')
+    CLASSES = ('Road', 'Sky', 'Nature', 'Poles', 'Cars', 'Lane lines',
+               'Buildings', 'Irrelevant', 'Traffic Info', 'Curb stones',
+               'Side walk', 'Trucks', 'Grid structure',
+               'Obstacles / trash on road', 'Pedestrians', 'Ego car',
+               'Small traffic participants', 'Parking area')
 
-    PALETTE = [
-        [128, 64, 128],
-        [70, 130, 180],
-        [107, 142, 35],
-        [153, 153, 153],
-        [0, 0, 142],
-        [255, 255, 255],
-        [70, 70, 70],
-        [250, 170, 30],
-        [220, 220, 0],
-        [230, 150, 140],
-        [244, 35, 232],
-        [190, 153, 153],
-        [102, 102, 156],
-        [0, 0, 230],
-        [220, 20, 60],
-        [0, 0, 70],
-        [119, 11, 32],
-        [250, 170, 160],
-    ]
+    PALETTE = [[128, 64, 128], [70, 130, 180], [107, 142, 35], [153, 153, 153],
+               [0, 0, 142], [255, 255, 255], [70, 70, 70], [250, 170, 30],
+               [220, 220, 0], [230, 150, 140], [244, 35, 232], [190, 153, 153],
+               [102, 102, 156], [0, 0, 230], [220, 20, 60], [0, 0, 70],
+               [119, 11, 32], [250, 170, 160]]
 
     def __init__(self, **kwargs):
         super(A2D2Dataset18Classes, self).__init__(
