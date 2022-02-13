6a5592af64b8dcfeeec841c928c9f727cc50ae00
MengzhangLI <mcmong@pku.edu.cn>
2022-02-13 23:28:12 +0800

    fix bugs in base_1k

--- configs/convnext/upernet_convnext_base_512x512_160k_ade20k.py
+++ configs/convnext/upernet_convnext_base_512x512_160k_ade20k.py
@@ -4,6 +4,7 @@ _base_ = [
 ]
 crop_size = (512, 512)
 model = dict(
+    decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=150),
     auxiliary_head=dict(in_channels=512, num_classes=150),
     test_cfg=dict(mode='slide', crop_size=crop_size, stride=(341, 341)),
 )
