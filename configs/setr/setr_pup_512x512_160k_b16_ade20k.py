14b35f7efa606e0dadbb02d17c090bb0f57eb131
RockeyCoss <1350291943@qq.com>
2022-02-16 15:39:03 +0800

    [Fix] Fix the bug that setr cannot load pretrain

--- configs/setr/setr_pup_512x512_160k_b16_ade20k.py
+++ configs/setr/setr_pup_512x512_160k_b16_ade20k.py
@@ -8,7 +8,8 @@ model = dict(
     backbone=dict(
         img_size=(512, 512),
         drop_rate=0.,
-        init_cfg=dict(type='Pretrained', checkpoint='mmcls://vit_large_p16')),
+        init_cfg=dict(
+            type='Pretrained', checkpoint='pretrain/vit_large_p16.pth')),
     decode_head=dict(num_classes=150),
     auxiliary_head=[
         dict(
