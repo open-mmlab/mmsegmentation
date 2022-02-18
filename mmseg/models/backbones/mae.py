3a76bf2ff005930c14587ee67168712dfefccb0c
seuyou <3463423099@qq.com>
2022-02-18 17:50:03 +0800

    [Feature]: Add interpolate pe when loading

--- mmseg/models/backbones/mae.py
+++ mmseg/models/backbones/mae.py
@@ -4,6 +4,7 @@ import warnings
 
 import torch
 import torch.nn as nn
+import torch.distributed as dist
 from mmcv.cnn import build_norm_layer
 from mmcv.cnn.bricks.drop import build_dropout
 from mmcv.cnn.bricks.transformer import FFN
@@ -341,6 +342,10 @@ class MAE(BaseModule):
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dims))
         self.drop_after_pos = nn.Dropout(p=drop_rate)
 
+        self.num_patches = window_size[0] * window_size[1]
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1, self.num_patches + 1, embed_dims))
+
         if isinstance(out_indices, int):
             if out_indices == -1:
                 out_indices = num_layers - 1
@@ -415,13 +420,40 @@ class MAE(BaseModule):
             else:
                 state_dict = checkpoint
 
+        if 'pos_embed' in state_dict:
+            pos_embed_checkpoint = state_dict['pos_embed']
+            embedding_size = pos_embed_checkpoint.shape[-1]
+            num_extra_tokens = self.pos_embed.shape[-2] - self.num_patches
+            # height (== width) for the checkpoint position embedding
+            orig_size = int(
+                (pos_embed_checkpoint.shape[-2] - num_extra_tokens)**0.5)
+            # height (== width) for the new position embedding
+            new_size = int(self.num_patches**0.5)
+            # class_token and dist_token are kept unchanged
+            if orig_size != new_size:
+                if dist.get_rank() == 0:
+                    print("Position interpolate from %dx%d to %dx%d" %
+                          (orig_size, orig_size, new_size, new_size))
+                extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
+                # only the position tokens are interpolated
+                pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
+                pos_tokens = pos_tokens.reshape(-1, orig_size, orig_size,
+                                                embedding_size).permute(
+                                                    0, 3, 1, 2)
+                pos_tokens = torch.nn.functional.interpolate(
+                    pos_tokens,
+                    size=(new_size, new_size),
+                    mode='bicubic',
+                    align_corners=False)
+                pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
+                new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
+                state_dict['pos_embed'] = new_pos_embed
+
             self.load_state_dict(state_dict, False)
 
         elif self.init_cfg is not None:
             super(MAE, self).init_weights()
         else:
-            # We only implement the 'jax_impl' initialization implemented at
-            # https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py#L353  # noqa: E501
             trunc_normal_(self.pos_embed, std=.02)
             trunc_normal_(self.cls_token, std=.02)
             for n, m in self.named_modules():
