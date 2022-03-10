17b500f9dff04802d3ab1fe09d4666d38dbe20e5
Rockey <41846794+RockeyCoss@users.noreply.github.com>
2022-03-10 20:27:28 +0800

    [Feature] add nlc2nchw2nlc and nchw2nlc2nchw (#1249)
    * [Feature] add nlc2nchw2nlc and nchw2nlc2nchw
    
    * add example
    
    * add test, add **kwargs to make it more universal

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -5,11 +5,12 @@ from .make_divisible import make_divisible
 from .res_layer import ResLayer
 from .se_layer import SELayer
 from .self_attention_block import SelfAttentionBlock
-from .shape_convert import nchw_to_nlc, nlc_to_nchw
+from .shape_convert import (nchw2nlc2nchw, nchw_to_nlc, nlc2nchw2nlc,
+                            nlc_to_nchw)
 from .up_conv_block import UpConvBlock
 
 __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
     'UpConvBlock', 'InvertedResidualV3', 'SELayer', 'PatchEmbed',
-    'nchw_to_nlc', 'nlc_to_nchw'
+    'nchw_to_nlc', 'nlc_to_nchw', 'nchw2nlc2nchw', 'nlc2nchw2nlc'
 ]
