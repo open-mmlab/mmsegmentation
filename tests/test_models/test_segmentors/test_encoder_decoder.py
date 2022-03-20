74a83177ff9bce54cf3cdd1a763e0d8660b8cca9
MeowZheng <meowzheng@outlook.com>
2022-03-20 15:50:17 +0800

    lint

--- tests/test_models/test_segmentors/test_encoder_decoder.py
+++ tests/test_models/test_segmentors/test_encoder_decoder.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmcv import ConfigDict
 import pytest
+from mmcv import ConfigDict
+
 from mmseg.models import build_segmentor
 from .utils import _segmentor_forward_train_test
 
