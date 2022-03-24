895049c167a3f12ddfa443496e34158f0194c65a
MengzhangLI <mcmong@pku.edu.cn>
2022-03-24 12:47:10 +0800

    move mmseg/utils/dist_util.py to mmseg/core/utils/dist_util.py

--- mmseg/utils/__init__.py
+++ mmseg/utils/__init__.py
@@ -1,11 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .collect_env import collect_env
-from .dist_util import check_dist_init, sync_random_seed
 from .logger import get_root_logger
 from .misc import find_latest_checkpoint
 from .set_env import setup_multi_processes
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
-    'setup_multi_processes', 'check_dist_init', 'sync_random_seed'
+    'setup_multi_processes'
 ]
