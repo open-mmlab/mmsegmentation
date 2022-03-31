f71cef5756e510219eb9a14a5b8c5f96de77ee95
linfangjian01 <linfangjian01@163.com>
2022-04-01 00:21:17 +0800

    move layer_decay_optimizer_constructor

--- mmseg/core/__init__.py
+++ mmseg/core/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .evaluation import *  # noqa: F401, F403
-from .layer_decay_optimizer_constructor import \
-    LayerDecayOptimizerConstructor  # noqa: F401
+from .optimizers import *  # noqa: F401, F403
 from .seg import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
