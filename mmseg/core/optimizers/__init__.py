6aa86d282ad1196ff70de9569b3bb030a906e050
linfangjian01 <linfangjian01@163.com>
2022-04-01 00:24:08 +0800

    fix

--- mmseg/core/optimizers/__init__.py
+++ mmseg/core/optimizers/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .layer_decay_optimizer_constructor import \
-    LearningRateDecayOptimizerConstructor, LayerDecayOptimizerConstructor
+from .layer_decay_optimizer_constructor import (
+    LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
 
 __all__ = [
     'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor'
