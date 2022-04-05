df47283dc39e26e35b86a37106932e55a6d925e2
MengzhangLI <mcmong@pku.edu.cn>
2022-04-05 11:34:53 +0800

    [Feature] Support Resnet strikes back (#1390)
    * [Feature] Support Resnet strikes back
    
    * fix url
    
    * [Feature] Add multi machine `dist_train`. (#1383)
    
    * Add training startup documentation
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * modify R-50b rsb
    
    Co-authored-by: FangjianLin <93248678+linfangjian01@users.noreply.github.com>

---
+++ configs/pspnet/pspnet_r50-d32_512x1024_80k_cityscapes.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+model = dict(backbone=dict(dilations=(1, 1, 2, 4), strides=(1, 2, 2, 2)))
