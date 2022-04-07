021e797a516b806912551c1c07736ea70195b283
RockeyCoss <1350291943@qq.com>
2022-04-07 02:32:45 -0700

    modify implementation

--- mmseg/models/losses/cross_entropy_loss.py
+++ mmseg/models/losses/cross_entropy_loss.py
@@ -65,23 +65,33 @@ def cross_entropy(pred,
 
 def _expand_onehot_labels(labels, label_weights, target_shape, ignore_index):
     """Expand onehot labels to match the size of prediction."""
-    bin_labels = labels.new_zeros(target_shape)
-    valid_mask = (labels >= 0) & (labels != ignore_index)
-    inds = torch.nonzero(valid_mask, as_tuple=True)
-
-    if inds[0].numel() > 0:
-        if labels.dim() == 3:
-            bin_labels[inds[0], labels[valid_mask], inds[1], inds[2]] = 1
+    if target_shape[1] == 1:
+        # For binary class segmentation, the shape of `pred` is
+        # [N, 1, H, W] and that of `label` is [N, H, W].
+        bin_labels = labels.unsqueeze(1)
+        valid_mask = ((bin_labels >= 0) & (bin_labels != ignore_index)).float()
+        if label_weights is not None:
+            bin_label_weights = label_weights.unsqueeze(1).float() * valid_mask
         else:
-            bin_labels[inds[0], labels[valid_mask]] = 1
-
-    valid_mask = valid_mask.unsqueeze(1).expand(target_shape).float()
-
-    if label_weights is None:
-        bin_label_weights = valid_mask
+            bin_label_weights = valid_mask
     else:
-        bin_label_weights = label_weights.unsqueeze(1).expand(target_shape)
-        bin_label_weights *= valid_mask
+        bin_labels = labels.new_zeros(target_shape)
+        valid_mask = (labels >= 0) & (labels != ignore_index)
+        inds = torch.nonzero(valid_mask, as_tuple=True)
+
+        if inds[0].numel() > 0:
+            if labels.dim() == 3:
+                bin_labels[inds[0], labels[valid_mask], inds[1], inds[2]] = 1
+            else:
+                bin_labels[inds[0], labels[valid_mask]] = 1
+
+        valid_mask = valid_mask.unsqueeze(1).expand(target_shape).float()
+
+        if label_weights is None:
+            bin_label_weights = valid_mask
+        else:
+            bin_label_weights = label_weights.unsqueeze(1).expand(target_shape)
+            bin_label_weights *= valid_mask
 
     return bin_labels, bin_label_weights, valid_mask
 
@@ -120,21 +130,10 @@ def binary_cross_entropy(pred,
                 pred.dim() == 4 and label.dim() == 3), \
             'Only pred shape [N, C], label shape [N] or pred shape [N, C, ' \
             'H, W], label shape [N, H, W] are supported'
-
-        if pred.size(1) == 1:
-            # For binary class segmentation, the shape of `pred` is
-            # [N, 1, H, W] and that of `label` is [N, H, W].
-            label = label.unsqueeze(1)
-            valid_mask = ((label >= 0) & (label != ignore_index)).float()
-            if weight is not None:
-                weight = weight.unsqueeze(1).float() * valid_mask
-            else:
-                weight = valid_mask
-        else:
-            # `weight` returned from `_expand_onehot_labels`
-            # has been treated for valid (non-ignore) pixels
-            label, weight, valid_mask = _expand_onehot_labels(
-                label, weight, pred.shape, ignore_index)
+        # `weight` returned from `_expand_onehot_labels`
+        # has been treated for valid (non-ignore) pixels
+        label, weight, valid_mask = _expand_onehot_labels(
+            label, weight, pred.shape, ignore_index)
     else:
         # should mask out the ignored elements
         valid_mask = ((label >= 0) & (label != ignore_index)).float()
