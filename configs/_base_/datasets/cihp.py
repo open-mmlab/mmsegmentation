183570905152b62332e771d06a68e305aed59060
linfangjian01 <linfangjian01@163.com>
2022-04-21 10:23:44 +0800

    fix lint

--- configs/_base_/datasets/cihp.py
+++ configs/_base_/datasets/cihp.py
@@ -1,6 +1,6 @@
 # dataset settings
-dataset_type = 'LIPDataset'
-data_root = 'data/LIP'
+dataset_type = 'CIHPDataset'
+data_root = 'data/CIHP'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 img_scale = (520, 520)
--- mmseg/datasets/cihp.py
+++ mmseg/datasets/cihp.py
@@ -8,20 +8,22 @@ from .custom import CustomDataset
 class CIHPDataset(CustomDataset):
     """CIHP dataset.
 
-    In segmentation map annotation for CIHP, 0 stands for background,
-    which is included in 20 categories. ``reduce_zero_label`` is fixed to
-    False. The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is
-    fixed to '.png'.
+    In segmentation map annotation for CIHP, 0 stands for background, which is
+    included in 20 categories. ``reduce_zero_label`` is fixed to False. The
+    ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
+    '.png'.
     """
-    CLASSES = ('background', 'hat', 'hair', 'glove', 'sunglasses', 'upperclothes',
-        'dress', 'coat', 'socks', 'pants', 'torsoSkin', 'scarf', 'skirt', 'face', 
-        'leftArm', 'rightArm', 'leftLeg', 'rightLeg', 'leftShoe', 'rightShoe')
+    CLASSES = ('background', 'hat', 'hair', 'glove', 'sunglasses',
+               'upperclothes', 'dress', 'coat', 'socks', 'pants', 'torsoSkin',
+               'scarf', 'skirt', 'face', 'leftArm', 'rightArm', 'leftLeg',
+               'rightLeg', 'leftShoe', 'rightShoe')
 
-    PALETTE = [[0, 0, 0], [128, 0, 0], [255, 0, 0], [0, 85, 0],
-               [170, 0, 51], [255, 85, 0], [0, 0, 85], [0, 119, 221], 
-               [85, 85, 0], [0, 85, 85], [85, 51, 0], [52, 86, 128], 
-               [0, 128, 0], [0, 0, 255], [51, 170, 221], [0, 255, 255], 
-               [85, 255, 170], [170, 255, 85], [255, 255, 0], [255, 170, 0]]
+    PALETTE = [[0, 0, 0], [128, 0, 0], [255, 0, 0], [0, 85, 0], [170, 0, 51],
+               [255, 85, 0], [0, 0, 85], [0, 119, 221], [85, 85,
+                                                         0], [0, 85, 85],
+               [85, 51, 0], [52, 86, 128], [0, 128, 0], [0, 0, 255],
+               [51, 170, 221], [0, 255, 255], [85, 255, 170], [170, 255, 85],
+               [255, 255, 0], [255, 170, 0]]
 
     def __init__(self, **kwargs):
         super(CIHPDataset, self).__init__(
@@ -29,4 +31,3 @@ class CIHPDataset(CustomDataset):
             seg_map_suffix='.png',
             reduce_zero_label=False,
             **kwargs)
-
