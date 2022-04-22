cd569fd3d103224586cf98a0937944c104858e3f
linfangjian01 <linfangjian01@163.com>
2022-04-22 13:52:13 +0800

    fix lip data aug

--- configs/_base_/datasets/lip.py
+++ configs/_base_/datasets/lip.py
@@ -7,15 +7,20 @@ img_scale = (520, 520)
 crop_size = (473, 473)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', reduce_zero_label=True),
-    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='Resize',
+        img_scale=img_scale,
+        keep_ratio=False,
+        ratio_range=(0.75, 1.25)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
+    dict(type='RandomRotate', prob=0.6, degree=30),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -25,8 +30,7 @@ test_pipeline = [
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
+            dict(type='Resize', keep_ratio=False),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
