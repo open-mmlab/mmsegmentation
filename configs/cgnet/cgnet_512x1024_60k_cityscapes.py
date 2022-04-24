1fc34c1f833973e627ee8bb713c510b6aacb16f0
MeowZheng <meowzheng@outlook.com>
2022-04-24 18:02:25 +0800

    [Fix] Revise high workers_per_gpus

--- configs/cgnet/cgnet_512x1024_60k_cityscapes.py
+++ configs/cgnet/cgnet_512x1024_60k_cityscapes.py
@@ -45,7 +45,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=8,
-    workers_per_gpu=8,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         data_root=data_root,
