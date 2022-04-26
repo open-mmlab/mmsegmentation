97fb9997d336c700c5148cc8facbf709159ad88c
MeowZheng <meowzheng@outlook.com>
2022-04-26 13:51:39 +0800

    fix ut

--- tests/test_core/test_layer_decay_optimizer_constructor.py
+++ tests/test_core/test_layer_decay_optimizer_constructor.py
@@ -12,7 +12,7 @@ decay_rate = 2
 base_wd = 0.05
 weight_decay = 0.05
 
-stage_wise_gt_lst = [{
+stage_wise_gt_lr_wd_convnext = [{
     'weight_decay': 0.0,
     'lr_scale': 128
 }, {
@@ -50,7 +50,7 @@ stage_wise_gt_lst = [{
     'lr_scale': 1
 }]
 
-layer_wise_gt_lst = [{
+layer_wise_gt_lr_wd_convnext = [{
     'weight_decay': 0.0,
     'lr_scale': 128
 }, {
@@ -88,7 +88,8 @@ layer_wise_gt_lst = [{
     'lr_scale': 1
 }]
 
-layer_wise_wd_lr = [{
+# 1 layer (cls_token and patch_embed) + 3 layers * 2 (w, b) = 9 layers
+layer_wise_gt_wd_lr_beit = [{
     'weight_decay': 0.0,
     'lr_scale': 16
 }, {
@@ -109,10 +110,16 @@ layer_wise_wd_lr = [{
 }, {
     'weight_decay': 0.0,
     'lr_scale': 2
+}, {
+    'weight_decay': 0.05,
+    'lr_scale': 1
+}, {
+    'weight_decay': 0.0,
+    'lr_scale': 1
 }]
 
 
-class ConvNeXtExampleModel(nn.Module):
+class ToyConvNeXt(nn.Module):
 
     def __init__(self):
         super().__init__()
@@ -133,116 +140,100 @@ class ConvNeXtExampleModel(nn.Module):
         self.lin = nn.Parameter(torch.ones(1))
         self.lin.requires_grad = False
         self.downsample_layers = nn.ModuleList()
-        for i in range(4):
+        for _ in range(4):
             stage = nn.Sequential(nn.Conv2d(3, 4, kernel_size=1, bias=True))
             self.downsample_layers.append(stage)
 
-        self.decode_head = nn.Conv2d(2, 2, kernel_size=1, groups=2)
 
-
-class PseudoDataParallel1(nn.Module):
+class ToyBEiT(nn.Module):
 
     def __init__(self):
-        super().__init__()
-        self.backbone = ConvNeXtExampleModel()
-
-    def forward(self, x):
-        return x
-
-
-class BEiTExampleModel(nn.Module):
-
-    def __init__(self, depth):
         super().__init__()
         # add some variables to meet unit test coverate rate
         self.cls_token = nn.Parameter(torch.ones(1))
         self.patch_embed = nn.Parameter(torch.ones(1))
         self.layers = nn.ModuleList()
-        for _ in range(depth):
+        for _ in range(3):
             layer = nn.Conv2d(3, 3, 1)
             self.layers.append(layer)
 
 
-class PseudoDataParallel2(nn.Module):
+class ToySegmentor(nn.Module):
 
-    def __init__(self, depth):
+    def __init__(self, backbone):
         super().__init__()
-        self.backbone = BEiTExampleModel(depth)
-
-    def forward(self, x):
-        return x
+        self.backbone = backbone
+        self.decode_head = nn.Conv2d(2, 2, kernel_size=1, groups=2)
 
 
-class ViTExampleModel(nn.Module):
+class PseudoDataParallel(nn.Module):
+
+    def __init__(self, model):
+        super().__init__()
+        self.module = model
+
+
+class ToyViT(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.backbone = nn.ModuleList()
-        self.backbone.cls_token = nn.Parameter(torch.ones(1))
-        self.backbone.patch_embed = nn.Parameter(torch.ones(1))
 
 
-def check_convnext_adamw_optimizer(optimizer, gt_lst):
+def check_optimizer_lr_wd(optimizer, gt_lr_wd):
     assert isinstance(optimizer, torch.optim.AdamW)
     assert optimizer.defaults['lr'] == base_lr
     assert optimizer.defaults['weight_decay'] == base_wd
     param_groups = optimizer.param_groups
-    assert len(param_groups) == 12
+    print(param_groups)
+    assert len(param_groups) == len(gt_lr_wd)
     for i, param_dict in enumerate(param_groups):
-        assert param_dict['weight_decay'] == gt_lst[i]['weight_decay']
-        assert param_dict['lr_scale'] == gt_lst[i]['lr_scale']
+        assert param_dict['weight_decay'] == gt_lr_wd[i]['weight_decay']
+        assert param_dict['lr_scale'] == gt_lr_wd[i]['lr_scale']
         assert param_dict['lr_scale'] == param_dict['lr']
 
 
-def check_beit_adamw_optimizer(optimizer, gt_lst):
-    assert isinstance(optimizer, torch.optim.AdamW)
-    assert optimizer.defaults['lr'] == 1
-    assert optimizer.defaults['weight_decay'] == 0.05
-    param_groups = optimizer.param_groups
-    # 1 layer (cls_token and patch_embed) + 3 layers * 2 (w, b) = 9 layers
-    assert len(param_groups) == 7
-    for i, param_dict in enumerate(param_groups):
-        assert param_dict['weight_decay'] == gt_lst[i]['weight_decay']
-        assert param_dict['lr_scale'] == gt_lst[i]['lr_scale']
-        assert param_dict['lr_scale'] == param_dict['lr']
+@pytest.mark.parametrize('backbone', (ToyBEiT(), ))
+def test_learning_rate_decay_optimizer_constructor(backbone):
 
-
-def test_learning_rate_decay_optimizer_constructor():
-
-    # paramwise_cfg with ConvNeXtExampleModel
-    model = PseudoDataParallel1()
+    # Test lr wd for ConvNeXT
+    backbone = ToyConvNeXt()
+    model = PseudoDataParallel(ToySegmentor(backbone))
     optimizer_cfg = dict(
         type='AdamW', lr=base_lr, betas=(0.9, 0.999), weight_decay=0.05)
+    # stagewise decay
     stagewise_paramwise_cfg = dict(
         decay_rate=decay_rate, decay_type='stage_wise', num_layers=6)
     optim_constructor = LearningRateDecayOptimizerConstructor(
         optimizer_cfg, stagewise_paramwise_cfg)
     optimizer = optim_constructor(model)
-    check_convnext_adamw_optimizer(optimizer, stage_wise_gt_lst)
-
+    check_optimizer_lr_wd(optimizer, stage_wise_gt_lr_wd_convnext)
+    # layerwise decay
     layerwise_paramwise_cfg = dict(
         decay_rate=decay_rate, decay_type='layer_wise', num_layers=6)
     optim_constructor = LearningRateDecayOptimizerConstructor(
         optimizer_cfg, layerwise_paramwise_cfg)
     optimizer = optim_constructor(model)
-    check_convnext_adamw_optimizer(optimizer, layer_wise_gt_lst)
+    check_optimizer_lr_wd(optimizer, layer_wise_gt_lr_wd_convnext)
+
+    # Test lr wd for BEiT
+    backbone = ToyBEiT()
+    model = PseudoDataParallel(ToySegmentor(backbone))
 
     layerwise_paramwise_cfg = dict(
         decay_rate=decay_rate, decay_type='layer_wise', num_layers=3)
-    model = PseudoDataParallel2(depth=3)
     optim_constructor = LearningRateDecayOptimizerConstructor(
         optimizer_cfg, layerwise_paramwise_cfg)
     optimizer = optim_constructor(model)
-    check_beit_adamw_optimizer(optimizer, layer_wise_wd_lr)
+    check_optimizer_lr_wd(optimizer, layer_wise_gt_wd_lr_beit)
 
+    # Test invalidation of lr wd for Vit
+    backbone = ToyViT()
+    model = PseudoDataParallel(ToySegmentor(backbone))
     with pytest.raises(NotImplementedError):
-        model = ViTExampleModel()
         optim_constructor = LearningRateDecayOptimizerConstructor(
             optimizer_cfg, layerwise_paramwise_cfg)
         optimizer = optim_constructor(model)
-
     with pytest.raises(NotImplementedError):
-        model = ViTExampleModel()
         optim_constructor = LearningRateDecayOptimizerConstructor(
             optimizer_cfg, stagewise_paramwise_cfg)
         optimizer = optim_constructor(model)
@@ -251,11 +242,12 @@ def test_learning_rate_decay_optimizer_constructor():
 def test_beit_layer_decay_optimizer_constructor():
 
     # paramwise_cfg with BEiTExampleModel
-    model = PseudoDataParallel2(depth=3)
+    backbone = ToyBEiT()
+    model = PseudoDataParallel(ToySegmentor(backbone))
     optimizer_cfg = dict(
         type='AdamW', lr=1, betas=(0.9, 0.999), weight_decay=0.05)
     paramwise_cfg = dict(layer_decay_rate=2, num_layers=3)
     optim_constructor = LayerDecayOptimizerConstructor(optimizer_cfg,
                                                        paramwise_cfg)
     optimizer = optim_constructor(model)
-    check_beit_adamw_optimizer(optimizer, layer_wise_wd_lr)
+    check_optimizer_lr_wd(optimizer, layer_wise_gt_wd_lr_beit)
