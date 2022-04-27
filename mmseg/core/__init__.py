7c3bf22885eb781c6ef1e7e847255bb8a73cce44
FangjianLin <93248678+linfangjian01@users.noreply.github.com>
2022-04-27 10:43:25 +0800

    [Refactory] Merge BEiT and ConvNext 's LR decay optimizer constructors (#1438)
    * move layer_decay_optimizer_constructor
    
    * fix
    
    * fix
    
    * merge test_core
    
    * fix
    
    * add DeprecationWarning
    
    * fix DeprecationWarning
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix test
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix ut
    
    * fix
    
    * fix
    
    * Update tests/test_core/test_layer_decay_optimizer_constructor.py
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: MeowZheng <meowzheng@outlook.com>
    Co-authored-by: Miao Zheng <76149310+MeowZheng@users.noreply.github.com>

--- mmseg/core/__init__.py
+++ mmseg/core/__init__.py
@@ -2,12 +2,10 @@
 from .builder import (OPTIMIZER_BUILDERS, build_optimizer,
                       build_optimizer_constructor)
 from .evaluation import *  # noqa: F401, F403
-from .layer_decay_optimizer_constructor import \
-    LayerDecayOptimizerConstructor  # noqa: F401
+from .optimizers import *  # noqa: F401, F403
 from .seg import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
 
 __all__ = [
-    'LayerDecayOptimizerConstructor', 'OPTIMIZER_BUILDERS', 'build_optimizer',
-    'build_optimizer_constructor'
+    'OPTIMIZER_BUILDERS', 'build_optimizer', 'build_optimizer_constructor'
 ]
---
+++ mmseg/core/optimizers/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .layer_decay_optimizer_constructor import (
+    LayerDecayOptimizerConstructor, LearningRateDecayOptimizerConstructor)
+
+__all__ = [
+    'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor'
+]
