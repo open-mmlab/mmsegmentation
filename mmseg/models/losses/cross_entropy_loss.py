5a030cdbf3d78134be0042efc4c4c7d3f38ac48c
Dawn-bin <40346249+Dawn-bin@users.noreply.github.com>
2022-04-29 10:12:48 +0800

    [Fix] Fix the bug in binary_cross_entropy, add comments
    As the ignore_index often set as 255, so the binary class label check should mask out ignore_index.
    
    Co-authored-by: Miao Zheng <76149310+MeowZheng@users.noreply.github.com>

--- mmseg/models/losses/cross_entropy_loss.py
+++ mmseg/models/losses/cross_entropy_loss.py
@@ -118,6 +118,9 @@ def binary_cross_entropy(pred,
     if pred.size(1) == 1:
         # For binary class segmentation, the shape of pred is
         # [N, 1, H, W] and that of label is [N, H, W].
+        # As the ignore_index often set as 255, so the
+        # binary class label check should mask out 
+        # ignore_index
         assert label[label != ignore_index].max() <= 1, \
             'For pred with shape [N, 1, H, W], its label must have at ' \
             'most 2 classes'
