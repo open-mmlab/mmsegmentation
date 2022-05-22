a9ad562ccda683ad66aeb50b0db52eee44430130
Akide Liu <mail@llycloud.com>
2022-05-22 16:34:54 +0930

    add deeplabv3plus_r50

--- configs/deeplabv3plus/deeplabv3plus_r50-d8_256x864_40k_kitti.py
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_256x864_40k_kitti.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/deeplabv3plus_r50-d8.py',
-    '../_base_/datasets/cityscapes.py', '../_base_/default_runtime.py',
+    '../_base_/datasets/kitti_seg.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_40k.py'
 ]
