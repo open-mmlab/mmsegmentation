7ece01149ba757b3dcf9119170ec79cfd7b8c6f3
atinfinity <dandelion1124@gmail.com>
2022-05-24 21:26:40 +0900

    minor fix

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -10,5 +10,5 @@ mmseg_version=$1
 docker build -t mmsegmentation:${mmseg_version} \
              --build-arg GID=$(id -g) \
              --build-arg UID=$(id -u) \
-             --build-arg MMSEG_VERISON=v${mmseg_version} \
+             --build-arg MMSEG=v${mmseg_version} \
              .
