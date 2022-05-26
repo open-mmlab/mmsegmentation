0ca30ef84579ce4c62aa3aac1ab3769672b389bc
Yi Zhao <zhaoyi9499@gmail.com>
2022-05-26 07:32:25 +0000

    fix beit model converter

--- tools/model_converters/beit2mmseg.py
+++ tools/model_converters/beit2mmseg.py
@@ -12,9 +12,6 @@ def convert_beit(ckpt):
     new_ckpt = OrderedDict()
 
     for k, v in ckpt.items():
-        if k.startswith('patch_embed'):
-            new_key = k.replace('patch_embed.proj', 'patch_embed.projection')
-            new_ckpt[new_key] = v
         if k.startswith('blocks'):
             new_key = k.replace('blocks', 'layers')
             if 'norm' in new_key:
@@ -23,10 +20,14 @@ def convert_beit(ckpt):
                 new_key = new_key.replace('mlp.fc1', 'ffn.layers.0.0')
             elif 'mlp.fc2' in new_key:
                 new_key = new_key.replace('mlp.fc2', 'ffn.layers.1')
-            new_ckpt[new_key] = v
+        elif k.startswith('patch_embed'):
+            new_key = k.replace('patch_embed.proj', 'patch_embed.projection')
         else:
             new_key = k
-            new_ckpt[new_key] = v
+        if k.startswith('fc_norm') or k.startswith('head'):
+            continue
+        new_key = 'backbone.' + new_key
+        new_ckpt[new_key] = v
 
     return new_ckpt
 
