1ad7bdcb5aeea00e27c5b0707433303bfbd54a65
MeowZheng <meowzheng@outlook.com>
2022-05-27 20:57:38 +0800

    [Feature] Add default scope and register modules

--- mmseg/utils/set_env.py
+++ mmseg/utils/set_env.py
@@ -1,9 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import datetime
 import os
 import platform
+import warnings
 
 import cv2
 import torch.multiprocessing as mp
+from mmengine import DefaultScope
 
 from ..utils import get_root_logger
 
@@ -53,3 +56,36 @@ def setup_multi_processes(cfg):
                 os.environ['MKL_NUM_THREADS'] = str(mkl_num_threads)
         else:
             logger.info(f'MKL num threads is {os.environ["MKL_NUM_THREADS"]}')
+
+
+def register_all_modules(init_default_scope: bool = True) -> None:
+    """Register all modules in mmseg into the registries.
+
+    Args:
+        init_default_scope (bool): Whether initialize the mmseg default scope.
+            When `init_default_scope=True`, the global default scope will be
+            set to `mmseg`, and all registries will build modules from mmseg's
+            registry node. To understand more about the registry, please refer
+            to https://github.com/open-mmlab/mmengine/blob/main/docs/en/tutorials/registry.md
+            Defaults to True.
+    """  # noqa
+    import mmseg.core  # noqa: F401,F403
+    import mmseg.datasets  # noqa: F401,F403
+    import mmseg.models  # noqa: F401,F403
+
+    if init_default_scope:
+        never_created = DefaultScope.get_current_instance() is None \
+                        or not DefaultScope.check_instance_created('mmseg')
+        if never_created:
+            DefaultScope.get_instance('mmseg', scope_name='mmseg')
+            return
+        current_scope = DefaultScope.get_current_instance()
+        if current_scope.scope_name != 'mmseg':
+            warnings.warn('The current default scope '
+                          f'"{current_scope.scope_name}" is not "mmseg", '
+                          '`register_all_modules` will force the current'
+                          'default scope to be "mmseg". If this is not '
+                          'expected, please set `init_default_scope=False`.')
+            # avoid name conflict
+            new_instance_name = f'mmseg-{datetime.datetime.now()}'
+            DefaultScope.get_instance(new_instance_name, scope_name='mmseg')
