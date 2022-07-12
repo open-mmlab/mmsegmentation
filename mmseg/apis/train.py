9bdf791a24d3f41f3707059ff1d415f25fae7492
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2022-07-12 17:14:16 +0200

    Keep train/valand eval configs separated
    When structuring the configuration, it's useful to keep the training, validation and evaluation configurations separated. In this case, it'd be useful to have a test configuration all alone, letting a separated validation configuration.

--- mmseg/apis/train.py
+++ mmseg/apis/train.py
@@ -152,7 +152,7 @@ def train_segmentor(model,
 
     # register eval hooks
     if validate:
-        val_dataset = build_dataset(cfg.data.val, dict(test_mode=True))
+        val_dataset = build_dataset(cfg.data.test, dict(test_mode=True))
         # The specific dataloader settings
         val_loader_cfg = {
             **loader_cfg,
