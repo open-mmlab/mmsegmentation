f2d712c59b949ba63891e36d72f8be88349d2a48
xiexinch <xiexinch@outlook.com>
2022-07-13 18:56:35 +0800

    minor change

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -55,7 +55,7 @@ mmcv_max_version = digit_version(MMCV_MAX)
 mmcv_version = digit_version(mmcv.__version__)
 
 
-assert (mmcv_min_version <= mmcv_version <= mmcv_max_version), \
+assert (mmcv_min_version <= mmcv_version < mmcv_max_version), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_min_version}, <{mmcv_max_version}.'
 
