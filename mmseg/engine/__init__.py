9aa415e0a9d04b9be33b3ccef1a9a6e4e9f03dc4
MengzhangLI <mcmong@pku.edu.cn>
2022-07-27 16:28:09 +0800

    [Feature] Add SegVisualizer (#1792)
    * [Feature] Add SegVisualizer
    
    * change name to visualizer_example
    
    * fix typo
    
    * refactor folder structure

--- mmseg/engine/__init__.py
+++ mmseg/engine/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .hooks import SegVisualizationHook
 from .optimizers import (LayerDecayOptimizerConstructor,
                          LearningRateDecayOptimizerConstructor)
+from .visualization import SegLocalVisualizer
 
 __all__ = [
-    'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor'
+    'LearningRateDecayOptimizerConstructor', 'LayerDecayOptimizerConstructor',
+    'SegVisualizationHook', 'SegLocalVisualizer'
 ]
