4079d6dfed29ffc476c6e841cdbe6dd44f7763e9
MengzhangLI <mcmong@pku.edu.cn>
2022-07-27 15:35:12 +0800

    [Fix] Use MMEngine BaseModule (#1829)
    * [Fix] Use MMEngine BaseModule
    
    * delete auto_fp16

--- mmseg/models/backbones/bisenetv1.py
+++ mmseg/models/backbones/bisenetv1.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 from mmcv.cnn import ConvModule
-from mmcv.runner import BaseModule
+from mmengine.model import BaseModule
 
 from mmseg.ops import resize
 from mmseg.registry import MODELS
