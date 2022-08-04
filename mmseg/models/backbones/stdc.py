be7c4632367b79d0052285c1f0cb8226f247f4de
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-08-04 09:35:05 +0800

    [Fix] Import BaseModule from mmengien (#1862)

--- mmseg/models/backbones/stdc.py
+++ mmseg/models/backbones/stdc.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import ConvModule
-from mmcv.runner.base_module import BaseModule, ModuleList, Sequential
+from mmengine.model import BaseModule, ModuleList, Sequential
 
 from mmseg.ops import resize
 from mmseg.registry import MODELS
