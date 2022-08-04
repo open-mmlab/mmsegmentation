e2f26e3fde35d8b70900414237d7b628196f432b
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-08-04 09:35:05 +0800

    [Fix] Import BaseModule from mmengien (#1862)

--- mmseg/models/backbones/vit.py
+++ mmseg/models/backbones/vit.py
@@ -9,9 +9,9 @@ from mmcv.cnn import build_norm_layer
 from mmcv.cnn.bricks.transformer import FFN, MultiheadAttention
 from mmcv.cnn.utils.weight_init import (constant_init, kaiming_init,
                                         trunc_normal_)
-from mmcv.runner import (BaseModule, CheckpointLoader, ModuleList,
-                         load_state_dict)
+from mmcv.runner import CheckpointLoader, load_state_dict
 from mmengine.logging import print_log
+from mmengine.model import BaseModule, ModuleList
 from torch.nn.modules.batchnorm import _BatchNorm
 from torch.nn.modules.utils import _pair as to_2tuple
 
