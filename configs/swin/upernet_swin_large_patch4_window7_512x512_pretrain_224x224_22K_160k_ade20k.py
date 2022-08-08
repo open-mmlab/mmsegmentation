1db5e7ded1e6858268012b45efa4da19a89e456d
MengzhangLI <mcmong@pku.edu.cn>
2022-08-08 21:14:00 +0800

    add swin large in 2.0

---
+++ configs/swin/upernet_swin_large_patch4_window7_512x512_pretrain_224x224_22K_160k_ade20k.py
@@ -0,0 +1,15 @@
+_base_ = [
+    'upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_'
+    'pretrain_224x224_1K.py'
+]
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_large_patch4_window7_224_22k_20220412-aeecf2aa.pth'  # noqa
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        pretrain_img_size=224,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=7),
+    decode_head=dict(in_channels=[192, 384, 768, 1536], num_classes=150),
+    auxiliary_head=dict(in_channels=768, num_classes=150))
