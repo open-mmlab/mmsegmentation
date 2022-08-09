d265778b6fb9de1f0bff0b7cd74de5ea6eb660ff
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-08-09 21:41:03 +0800

    [Fix] Import Sequential from mmengine (#1899)

--- mmseg/models/utils/res_layer.py
+++ mmseg/models/utils/res_layer.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.cnn import build_conv_layer, build_norm_layer
-from mmcv.runner import Sequential
+from mmengine.model import Sequential
 from torch import nn as nn
 
 
