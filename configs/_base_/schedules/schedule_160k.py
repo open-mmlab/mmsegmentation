95a78acf0816b0fdcb923c0a3cd8f8410150ad85
谢昕辰 <xiexinch@outlook.com>
2022-08-09 19:16:07 +0800

    fix logger cfg (#1894)

--- configs/_base_/schedules/schedule_160k.py
+++ configs/_base_/schedules/schedule_160k.py
@@ -18,7 +18,7 @@ val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=16000),
     sampler_seed=dict(type='DistSamplerSeedHook'),
