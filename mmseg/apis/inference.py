2f6c08a811824c00d4f66225b563c0592f2ec7c2
whooray <jinwonkim93@users.noreply.github.com>
2022-08-12 21:16:25 +0900

    Update mmseg/apis/inference.py
    Co-authored-by: Hakjin Lee <nijkah@gmail.com>

--- mmseg/apis/inference.py
+++ mmseg/apis/inference.py
@@ -84,17 +84,13 @@ def inference_segmentor(model, imgs):
     test_pipeline = [LoadImage()] + cfg.data.test.pipeline[1:]
     test_pipeline = Compose(test_pipeline)
     # prepare data
-    if isinstance(imgs, list):
-        data = []
-        for img in imgs:
-            img_data = dict(img=img)
-            img_data = test_pipeline(img_data)
-            data.append(img_data)
-        data = collate(data, samples_per_gpu=len(imgs))
-    else:
-        data = dict(img=imgs)
-        data = test_pipeline(data)
-        data = collate([data], samples_per_gpu=1)
+    data = []
+    imgs = imgs if isinstance(imgs, list) else [imgs]
+    for img in imgs:
+        img_data = dict(img=img)
+        img_data = test_pipeline(img_data)
+        data.append(img_data)
+    data = collate(data, samples_per_gpu=len(imgs))
     if next(model.parameters()).is_cuda:
         # scatter to specified GPU
         data = scatter(data, [device])[0]
