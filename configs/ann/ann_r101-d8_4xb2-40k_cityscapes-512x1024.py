eea2f1849f693bb73647886d2bf6a073e2882ede
xiexinch <xiexinch@outlook.com>
2022-08-25 18:46:24 +0800

    update ann readme

--- configs/ann/ann_r101-d8_4xb2-40k_cityscapes-512x1024.py
+++ configs/ann/ann_r101-d8_4xb2-40k_cityscapes-512x1024.py
@@ -1,2 +1,2 @@
-_base_ = './ann_r50-d8_769x769_40k_cityscapes.py'
+_base_ = './ann_r50-d8_4xb2-40k_cityscapes-512x1024.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
--- configs/ann/ann_r101-d8_4xb2-40k_cityscapes-769x769.py
+++ configs/ann/ann_r101-d8_4xb2-40k_cityscapes-769x769.py
@@ -1,2 +1,2 @@
-_base_ = './ann_r50-d8_512x1024_80k_cityscapes.py'
+_base_ = './ann_r50-d8_4xb2-40k_cityscapes-769x769.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
