fff0163b664d21291b579a45a54d8666e9492fed
MeowZheng <meowzheng@outlook.com>
2022-08-29 11:51:52 +0800

    [Fix] Add `by_epoch=False` for log processor

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -7,6 +7,7 @@ env_cfg = dict(
 vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+log_processor = dict(by_epoch=False)
 log_level = 'INFO'
 load_from = None
 resume = False
