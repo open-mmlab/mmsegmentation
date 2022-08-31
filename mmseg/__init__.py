f5b74be12b0e52d23172bdd2a3a6ee9c5a2766cd
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-08-31 18:46:43 +0800

    [Enhancement] version check codebase from openmmlab (#2006)

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -2,12 +2,14 @@
 import warnings
 
 import mmcv
+import mmengine
 from packaging.version import parse
 
 from .version import __version__, version_info
 
-MMCV_MIN = '2.0.0rc0'
-MMCV_MAX = '2.0.0'
+MMCV_MIN = '2.0.0rc1'
+MMCV_MAX = '2.1.0'
+MMENGINE_MIN = '0.5.0'
 
 
 def digit_version(version_str: str, length: int = 4):
@@ -58,5 +60,12 @@ mmcv_version = digit_version(mmcv.__version__)
 assert (mmcv_min_version <= mmcv_version < mmcv_max_version), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_min_version}, <{mmcv_max_version}.'
+
+mmengine_min_version = digit_version(MMENGINE_MIN)
+mmengine_version = digit_version(mmengine.__version__)
+
+assert (mmengine_min_version <= mmengine_version), \
+    f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
+    f'Please install mmengine>={mmengine_min_version}.'
 
 __all__ = ['__version__', 'version_info', 'digit_version']
