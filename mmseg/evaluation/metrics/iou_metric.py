eee92af6c7eae215a35e2bb1e94f07063eb136bd
yancong <xych6@outlook.com>
2022-09-01 13:25:58 +0800

    feat(IoUMetric): add DeprecationWarning of iou_metrics

--- mmseg/evaluation/metrics/iou_metric.py
+++ mmseg/evaluation/metrics/iou_metric.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
+import warnings
 import numpy as np
 from typing import Sequence
 from mmengine.logging import print_log
@@ -12,17 +13,22 @@ from mmseg.registry import METRICS
 @METRICS.register_module()
 class IoUMetric(MeanIoU):
     """A wrapper of ``mmeval.MeanIoU``.
-    
+
     This wrapper implements the `process` method that parses predictions and 
     labels from inputs. This enables ``mmengine.Evaluator`` to handle the data
     flow of different tasks through a unified interface.
-    
+
     In addition, this wrapper also implements the `evaluate` method that parses 
     metric results and print pretty tabel of metrics per class.
     """
 
     def __init__(self, verbose_results=True, **kwargs):
         """Changes the default value of `verbose_results` to True."""
+        iou_metrics = kwargs.pop('iou_metrics', None)
+        if iou_metrics is not None:
+            warnings.warn(
+                'DeprecationWarning: The `iou_metrics` parameter of '
+                '`IoUMetric` is deprecated, defaults return all metrics now!')
         super().__init__(verbose_results=verbose_results, **kwargs)
 
     def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
