b0596969173b7bffe6a6d021e644ce84d48069c5
yancong <xych6@outlook.com>
2022-09-26 12:06:50 +0800

    refine IoUMetric

--- mmseg/evaluation/metrics/iou_metric.py
+++ mmseg/evaluation/metrics/iou_metric.py
@@ -18,23 +18,39 @@ class IoUMetric(MeanIoU):
     labels from inputs. This enables ``mmengine.Evaluator`` to handle the data
     flow of different tasks through a unified interface.
 
-    In addition, this wrapper also implements the `evaluate` method that parses 
-    metric results and print pretty tabel of metrics per class.
+    In addition, this wrapper also implements the ``evaluate`` method that
+    parses metric results and print pretty tabel of metrics per class.
+
+    Args:
+        dist_backend (str | None): The name of the distributed communication
+            backend. Refer to :class:`mmeval.BaseMetric`.
+            Defaults to 'torch_cuda'.
+        **kwargs: Keyword parameters passed to :class:`mmeval.MeanIoU`.
     """
 
-    def __init__(self, verbose_results=True, **kwargs):
-        """Changes the default value of `verbose_results` to True."""
+    def __init__(self, dist_backend='torch_cuda', **kwargs):
         iou_metrics = kwargs.pop('iou_metrics', None)
         if iou_metrics is not None:
             warnings.warn(
                 'DeprecationWarning: The `iou_metrics` parameter of '
                 '`IoUMetric` is deprecated, defaults return all metrics now!')
-        super().__init__(verbose_results=verbose_results, **kwargs)
+        collect_device = kwargs.pop('collect_device', None)
+
+        if collect_device is not None:
+            warnings.warn(
+                'DeprecationWarning: The `collect_device` parameter of '
+                '`Accuracy` is deprecated, use `dist_backend` instead.')
+
+        # Changes the default value of `verbose_results` to True.
+        super().__init__(verbose_results=True,
+                         dist_backend=dist_backend,
+                         **kwargs)
 
     def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
         """Process one batch of data and data_samples.
 
-        Parse predictions and labels from `data_samples` and invoke `self.add`.
+        Parse predictions and labels from ``data_samples`` and invoke
+        ``self.add``.
 
         Args:
             data_batch (dict): A batch of data from the dataloader.
