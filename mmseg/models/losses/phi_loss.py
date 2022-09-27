5ec15d4552c579e0ad655ae46fab69ec3da47aed
androbaza <androbaza@tuta.io>
2022-09-27 17:00:14 +0200

    remove comments and unused vars

--- mmseg/models/losses/phi_loss.py
+++ mmseg/models/losses/phi_loss.py
@@ -3,12 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from ..builder import LOSSES
-from .utils import weight_reduce_loss, reduce_loss
+from .utils import reduce_loss
 from .cross_entropy_loss import _expand_onehot_labels
 
 
-def phi_loss(inputs, targets, gamma, smooth, weight=None, reduction='mean',
-                  avg_factor=None):
+def phi_loss(inputs, targets, gamma, smooth, weight=None, reduction='mean'):
     
     inputs = inputs.sigmoid()
     targets= targets.type_as(inputs)
@@ -17,13 +16,6 @@ def phi_loss(inputs, targets, gamma, smooth, weight=None, reduction='mean',
     inputs = inputs.reshape(inputs.size(0), -1)
     targets = targets.reshape(targets.size(0), -1)
     weight = weight.reshape(weight.size(0), -1)
-    
-    #removing .sum() to keep the tensor dimensions to [2,2,256,256] compatible with weight dimensions
-    # to use .sum with TP,FP and FN, weights must also be summed using .sum() in weighted reduction loss
-  
-    # print(gamma)
-    # print(inputs)
-    # print(targets)
     
     TP = (inputs * targets).sum()
     FP = ((1 - targets) * inputs).sum()
@@ -65,14 +57,11 @@ class PhiLoss(nn.Module):
                 targets,
                 weight=None,
                 reduction_override=None,
-                avg_factor=None,
                 ignore_index=255):
         """Forward function."""
         assert reduction_override in (None, 'none', 'mean', 'sum')
         reduction = (
             reduction_override if reduction_override else self.reduction)
-        # print(inputs.shape) #[2,2,256,256] => [N,C,H,W]
-        # print(targets.shape) #[2,256,256] => [N,H,W]
 
         if inputs.dim() != targets.dim():
             assert (inputs.dim() == 2 and targets.dim() == 1) or (
@@ -80,13 +69,10 @@ class PhiLoss(nn.Module):
             'Only pred shape [N, C], label shape [N] or pred shape [N, C, ' \
             'H, W], label shape [N, H, W] are supported'
             label, weight, _ = _expand_onehot_labels(targets, weight, inputs.shape,
-                                                ignore_index) #use imported version from cross_entropy_loss
+                                                ignore_index) 
         phi_loss_val= phi_loss(inputs, label, self.gamma, self.smooth,
-                               weight, reduction= reduction, avg_factor=avg_factor 
-                              )
-        # print(phi_loss_val)
+                               weight, reduction= reduction)
         loss = self.loss_weight * phi_loss_val
-        # print(loss)
         return loss
 
     @property
