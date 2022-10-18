8cd501503aa858b28eba6df8249831cf1e580ded
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-10-18 16:30:08 +0800

    [Feature] Add BioMedical data loading (#2176)
    * [WIP] Add BioMedical data loading
    
    * add depends nibabel
    
    * fix bug
    
    * fix ut
    
    * fix
    
    * add test data
    
    * xyz2zyx zyx2xyz
    
    * format
    
    * remove ignore empty
    
    * remove ignore empty
    
    * remove with seg in LoadBiomedicalAnnotation
    
    * float32
    
    * docstring
    
    * toxyz
    
    * docstring

--- mmseg/datasets/transforms/__init__.py
+++ mmseg/datasets/transforms/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .formatting import PackSegInputs
-from .loading import LoadAnnotations, LoadImageFromNDArray
+from .loading import (LoadAnnotations, LoadBiomedicalAnnotation,
+                      LoadBiomedicalData, LoadBiomedicalImageFromFile,
+                      LoadImageFromNDArray)
 from .transforms import (CLAHE, AdjustGamma, PhotoMetricDistortion, RandomCrop,
                          RandomCutOut, RandomMosaic, RandomRotate, Rerange,
                          ResizeToMultiple, RGB2Gray, SegRescale)
@@ -9,5 +11,6 @@ __all__ = [
     'LoadAnnotations', 'RandomCrop', 'SegRescale', 'PhotoMetricDistortion',
     'RandomRotate', 'AdjustGamma', 'CLAHE', 'Rerange', 'RGB2Gray',
     'RandomCutOut', 'RandomMosaic', 'PackSegInputs', 'ResizeToMultiple',
-    'LoadImageFromNDArray'
+    'LoadImageFromNDArray', 'LoadBiomedicalImageFromFile',
+    'LoadBiomedicalAnnotation', 'LoadBiomedicalData'
 ]
