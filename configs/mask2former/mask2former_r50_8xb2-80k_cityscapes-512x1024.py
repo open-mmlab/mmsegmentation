5fa9889dc0dabae353cea58e18f82e7766226821
xiexinch <xiexinch@outlook.com>
2022-10-20 20:17:35 +0800

    support training and evaluation

--- configs/mask2former/mask2former_r50_8xb2-80k_cityscapes-512x1024.py
+++ configs/mask2former/mask2former_r50_8xb2-80k_cityscapes-512x1024.py
@@ -1,8 +1,60 @@
-_base_ = ['../datasets/cityscapes.py']
+_base_ = ['../_base_/default_runtime.py']
+
+# dataset settings
+dataset_type = 'CityscapesDataset'
+data_root = 'data/cityscapes/'
+crop_size = (128, 256)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(2048, 1024),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(2048, 1024), keep_ratio=True),
+    # add loading annotation after ``Resize`` because ground truth
+    # does not need to do resize data transform
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='InfiniteSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='leftImg8bit/train', seg_map_path='gtFine/train'),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(
+            img_path='leftImg8bit/val', seg_map_path='gtFine/val'),
+        pipeline=test_pipeline))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
+test_evaluator = val_evaluator
 
 custom_imports = dict(imports='mmdet.models', allow_failed_imports=False)
 norm_cfg = dict(type='SyncBN', requires_grad=True)
-crop_size = (512, 1024)
+# crop_size = (32, 32)
 data_preprocessor = dict(
     type='SegDataPreProcessor',
     mean=[123.675, 116.28, 103.53],
@@ -21,7 +73,7 @@ model = dict(
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=-1,
-        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_cfg=dict(type='SyncBN', requires_grad=False),
         norm_eval=True,
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
@@ -101,19 +153,18 @@ model = dict(
                                  'ffn', 'norm')),
             init_cfg=None),
         loss_cls=dict(
-            type='CrossEntropyLoss',
+            type='mmdet.CrossEntropyLoss',
             use_sigmoid=False,
             loss_weight=2.0,
             reduction='mean',
-            # class_weight=[1.0] * 19 + [0.1]),
-            class_weight=[1.0] * 19),
+            class_weight=[1.0] * 19 + [0.1]),
         loss_mask=dict(
             type='mmdet.CrossEntropyLoss',
             use_sigmoid=True,
             reduction='mean',
             loss_weight=5.0),
         loss_dice=dict(
-            type='DiceLoss',
+            type='mmdet.DiceLoss',
             use_sigmoid=True,
             activate=True,
             reduction='mean',
@@ -121,7 +172,7 @@ model = dict(
             eps=1.0,
             loss_weight=5.0),
         train_cfg=dict(
-            num_points=64,
+            num_points=12544,
             oversample_ratio=3.0,
             importance_sample_ratio=0.75,
             assigner=dict(
@@ -141,3 +192,35 @@ model = dict(
             sampler=dict(type='mmdet.MaskPseudoSampler'))),
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
+
+# optimizer
+optimizer = dict(
+    type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.0005)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    clip_grad=None,
+    paramwise_cfg=dict(custom_keys={
+        'backbone': dict(lr_mult=0.1),
+    }))
+# learning policy
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=0,
+        power=0.9,
+        begin=0,
+        end=80000,
+        by_epoch=False)
+]
+# training schedule for 90k
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=90000, val_interval=9000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=9000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
--- mmseg/models/decode_heads/mask2former_head.py
+++ mmseg/models/decode_heads/mask2former_head.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Tuple
+from typing import List, Tuple
 
 import torch
 import torch.nn as nn
@@ -9,23 +9,27 @@ from mmengine.structures import InstanceData
 from torch import Tensor
 
 from mmseg.registry import MODELS
+from mmseg.structures.seg_data_sample import SegDataSample
 from mmseg.utils import ConfigType, SampleList
 
 
 @MODELS.register_module()
 class Mask2FormerHead(Mask2FormerHead_):
 
-    def __init__(self, num_classes, align_corners=False, **kwargs):
+    def __init__(self,
+                 num_classes,
+                 align_corners=False,
+                 ignore_index=255,
+                 **kwargs):
         super().__init__(**kwargs)
 
         self.num_classes = num_classes
-        # self.num_things_classes = num_classes
-        # self.num_stuff_classes = 0
         self.align_corners = align_corners
         self.out_channels = num_classes
+        self.ignore_index = ignore_index
 
         feat_channels = kwargs['feat_channels']
-        self.cls_embed = nn.Linear(feat_channels, self.num_classes)
+        self.cls_embed = nn.Linear(feat_channels, self.num_classes + 1)
 
     def loss(self, x: Tuple[Tensor], batch_data_samples: SampleList,
              train_cfg: ConfigType) -> dict:
@@ -35,17 +39,28 @@ class Mask2FormerHead(Mask2FormerHead_):
         for data_sample in batch_data_samples:
             batch_img_metas.append(data_sample.metainfo)
             gt_semantic_seg = data_sample.gt_sem_seg.data
-            gt_labels = torch.unique(
+            classes = torch.unique(
                 gt_semantic_seg,
                 sorted=False,
                 return_inverse=False,
                 return_counts=False)
-            gt_masks = torch.stack(
-                [gt_semantic_seg == label for label in gt_labels]).squeeze(1)
+
+            # remove ignored region
+            gt_labels = classes[classes != self.ignore_index]
+
+            masks = []
+            for class_id in gt_labels:
+                masks.append(gt_semantic_seg == class_id)
+
+            if len(masks) == 0:
+                gt_masks = torch.zeros(
+                    (0, gt_semantic_seg.shape[-2],
+                     gt_semantic_seg.shape[-1])).to(gt_semantic_seg)
+            else:
+                gt_masks = torch.stack(masks).squeeze(1)
 
             instance_data = InstanceData(labels=gt_labels, masks=gt_masks)
             batch_gt_instances.append(instance_data)
-            # batch_gt_semantic_segs.append(data_sample.gt_sem_seg)
 
         # forward
         all_cls_scores, all_mask_preds = self(x, batch_data_samples)
@@ -56,17 +71,16 @@ class Mask2FormerHead(Mask2FormerHead_):
 
         return losses
 
-    def predict(self, x: Tuple[Tensor], batch_data_samples: SampleList,
+    def predict(self, x: Tuple[Tensor], batch_img_metas: List[dict],
                 test_cfg: ConfigType) -> Tuple[Tensor]:
-
-        # batch_img_metas = [
-        #     data_sample.metainfo for data_sample in batch_data_samples
-        # ]
+        batch_data_samples = [
+            SegDataSample(metainfo=metainfo) for metainfo in batch_img_metas
+        ]
         all_cls_scores, all_mask_preds = self(x, batch_data_samples)
         mask_cls_results = all_cls_scores[-1]
         mask_pred_results = all_mask_preds[-1]
 
-        cls_score = F.softmax(mask_cls_results, dim=-1)[..., :-1]
+        cls_score = F.softmax(mask_cls_results, dim=-1)
         mask_pred = mask_pred_results.sigmoid()
-        sem_seg_mask = torch.einsum('bqc, qhw->bchw', cls_score, mask_pred)
+        sem_seg_mask = torch.einsum('bqc, bqhw->bchw', cls_score, mask_pred)
         return sem_seg_mask
