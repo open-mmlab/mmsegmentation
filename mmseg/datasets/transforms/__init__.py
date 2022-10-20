ac9ee8c3559e90b43ad2693656cdca4f83c4ccfa
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-10-20 16:45:31 +0800

    [Feature]Generate Edge for dataset (#2210)
    * [WIP]Generate Edge for dataset
    
    * add ut
    
    * add repr
    
    * add inti

--- mmseg/datasets/transforms/__init__.py
+++ mmseg/datasets/transforms/__init__.py
@@ -3,14 +3,15 @@ from .formatting import PackSegInputs
 from .loading import (LoadAnnotations, LoadBiomedicalAnnotation,
                       LoadBiomedicalData, LoadBiomedicalImageFromFile,
                       LoadImageFromNDArray)
-from .transforms import (CLAHE, AdjustGamma, PhotoMetricDistortion, RandomCrop,
-                         RandomCutOut, RandomMosaic, RandomRotate, Rerange,
-                         ResizeToMultiple, RGB2Gray, SegRescale)
+from .transforms import (CLAHE, AdjustGamma, GenerateEdge,
+                         PhotoMetricDistortion, RandomCrop, RandomCutOut,
+                         RandomMosaic, RandomRotate, Rerange, ResizeToMultiple,
+                         RGB2Gray, SegRescale)
 
 __all__ = [
     'LoadAnnotations', 'RandomCrop', 'SegRescale', 'PhotoMetricDistortion',
     'RandomRotate', 'AdjustGamma', 'CLAHE', 'Rerange', 'RGB2Gray',
     'RandomCutOut', 'RandomMosaic', 'PackSegInputs', 'ResizeToMultiple',
     'LoadImageFromNDArray', 'LoadBiomedicalImageFromFile',
-    'LoadBiomedicalAnnotation', 'LoadBiomedicalData'
+    'LoadBiomedicalAnnotation', 'LoadBiomedicalData', 'GenerateEdge'
 ]
