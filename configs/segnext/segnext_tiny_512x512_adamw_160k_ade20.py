c56d2437acf877f46f9a09e79e1954a925b2c040
Fabian Schuetze <fabian.schutze@eui.eu>
2022-11-02 19:04:35 +0100

    adjust config for better training

--- configs/segnext/segnext_tiny_512x512_adamw_160k_ade20.py
+++ configs/segnext/segnext_tiny_512x512_adamw_160k_ade20.py
@@ -87,7 +87,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=8,
+    samples_per_gpu=16,
     workers_per_gpu=4,
     train=dict(
         type='RepeatDataset',
@@ -111,10 +111,30 @@ data = dict(
         ann_dir='annotations/validation',
         pipeline=test_pipeline))
 
-optimizer = dict(type='AdamW', lr=0.0002, weight_decay=0.0001)
+# 0.00006 is the lr for bs 16, should use 0.00006/8 as lr (need to test)
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
 optimizer_config = dict()
 # learning policy
-lr_config = dict(policy='poly', power=0.9, min_lr=0.0, by_epoch=False)
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=160000)
 checkpoint_config = dict(by_epoch=False, interval=4000)
