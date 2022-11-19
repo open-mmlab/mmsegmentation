0e7e9184aa48b8170bb271164d9f93786569c958
xiexinch <xiexinch@outlook.com>
2022-11-19 17:03:10 +0800

    fix merge

--- mmseg/models/segmentors/encoder_decoder_tta.py
+++ mmseg/models/segmentors/encoder_decoder_tta.py
@@ -3,8 +3,10 @@ from typing import List
 
 import torch
 from mmengine.model import BaseTTAModel
+from mmengine.structures import PixelData
 
 from mmseg.registry import MODELS
+from mmseg.structures import SegDataSample
 from mmseg.utils import SampleList
 
 
@@ -23,19 +25,31 @@ class EncoderDecoderTTA(BaseTTAModel):
         """
         predictions = []
         for data_samples in data_samples_list:
+            seg_logits = torch.zeros(
+                data_samples[0].seg_logits.data.shape).cuda()
             for data_sample in data_samples:
                 # check flip
                 flip = data_sample.metainfo.get('flip', None)
+                seg_logit = data_sample.seg_logits.data
                 if flip:
-                    pred_sem_seg = data_sample.pred_sem_seg.data
-                    direction = data_sample.metainfo['flip_direction']
-                    if direction == 'vertical':
-                        dim = 0
-                    elif direction == 'horizontal':
-                        dim = 1
+                    flip_direction = data_sample.metainfo['flip_direction']
+                    assert flip_direction in ['horizontal', 'vertical']
+                    if flip_direction == 'horizontal':
+                        seg_logit = seg_logit.flip(dims=(2, ))
                     else:
-                        raise f'unexpected flip direction, got {direction}'
-                    pred_sem_seg = torch.flip(pred_sem_seg, dims=[dim])
-                    data_sample.pred_sem_seg.data = pred_sem_seg
-                predictions.append(data_sample)
+                        seg_logit = seg_logit.flip(dims=(1, ))
+                seg_logits += seg_logit
+            seg_logits /= len(data_samples)
+            if self.module.out_channels == 1:
+                seg_pred = (seg_logits > self.module.decode_head.threshold
+                            ).to(seg_logits).squeeze(1)
+            else:
+                seg_pred = seg_logits.argmax(dim=0)
+            data_sample = SegDataSample(
+                **{
+                    'seg_logits': PixelData(data=seg_logits),
+                    'pred_sem_seg': PixelData(data=seg_pred),
+                    'gt_sem_seg': data_samples[0].gt_sem_seg
+                })
+            predictions.append(data_sample)
         return predictions
