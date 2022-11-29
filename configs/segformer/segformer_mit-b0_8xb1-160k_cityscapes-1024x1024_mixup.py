d6125617728dab8be23030ea07d6a4db01881173
xiexinch <xiexinch@outlook.com>
2022-11-29 11:45:23 +0800

    fix segformer

--- configs/segformer/segformer_mit-b0_8xb1-160k_cityscapes-1024x1024_mixup.py
+++ configs/segformer/segformer_mit-b0_8xb1-160k_cityscapes-1024x1024_mixup.py
@@ -1,13 +1,51 @@
 _base_ = [
-    '../_base_/models/segformer_mit-b0.py', '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_160k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
+
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
 crop_size = (1024, 1024)
-data_preprocessor = dict(size=crop_size)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=crop_size)
 model = dict(
+    type='EncoderDecoder',
     data_preprocessor=data_preprocessor,
+    pretrained=None,
     backbone=dict(
+        type='MixVisionTransformer',
+        in_channels=3,
+        embed_dims=32,
+        num_stages=4,
+        num_layers=[2, 2, 2, 2],
+        num_heads=[1, 2, 5, 8],
+        patch_sizes=[7, 3, 3, 3],
+        sr_ratios=[8, 4, 2, 1],
+        out_indices=(0, 1, 2, 3),
+        mlp_ratio=4,
+        qkv_bias=True,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.1,
         init_cfg=dict(type='Pretrained', checkpoint='pretrain/mit_b0.pth')),
+    decode_head=dict(
+        type='SegformerHead',
+        in_channels=[32, 64, 160, 256],
+        in_index=[0, 1, 2, 3],
+        channels=256,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_soft=True, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
     test_cfg=dict(mode='slide', crop_size=(1024, 1024), stride=(768, 768)))
 
 optim_wrapper = dict(
@@ -73,7 +111,7 @@ train_dataset = dict(
     pipeline=train_pipeline)
 
 train_dataloader = dict(
-    batch_size=1,
+    batch_size=2,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
