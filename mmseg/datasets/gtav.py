6d216023459e032952fb11203c9ed572e0407e4e
Rachel ZHeng <rachelzh@andrew.cmu.edu>
2022-12-02 19:07:29 -0500

    custom gtav dataset configs

---
+++ mmseg/datasets/gtav.py
@@ -0,0 +1,24 @@
+import os.path as osp
+
+from mmseg.datasets.builder import DATASETS
+from mmseg.datasets.custom import CustomDataset
+
+
+@DATASETS.register_module()
+class GTAVDataset(CustomDataset):
+    
+    CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+               'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
+               'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
+               'bicycle')
+
+    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+               [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
+               [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
+               [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
+               [0, 80, 100], [0, 0, 230], [119, 11, 32]]
+    
+    def __init__(self, split, **kwargs):
+        super().__init__(img_suffix='.png', seg_map_suffix='.png', 
+                     split=split, **kwargs)
+        assert osp.exists(self.img_dir) and self.split is not None
