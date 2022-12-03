adaf376bedd21472f500bb624a3003fb1795be91
Rachel ZHeng <rachelzh@andrew.cmu.edu>
2022-12-02 20:37:19 -0500

    change data root

--- configs/_base_/datasets/gtav.py
+++ configs/_base_/datasets/gtav.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'GTAVDataset'
-data_root = 'data/gtav/'
+data_root = 'raw_data/gtav/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 1024)
