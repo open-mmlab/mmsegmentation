f314c8a1a1dd5a0e0f6124f792aed59d15725881
Rachel ZHeng <rachelzh@andrew.cmu.edu>
2022-12-02 21:03:05 -0500

    add pretrained weight checkpoint

--- configs/pspnet/pspnet_r50-d8_4x4_512x1024_40k_gtav.py
+++ configs/pspnet/pspnet_r50-d8_4x4_512x1024_40k_gtav.py
@@ -2,3 +2,4 @@ _base_ = [
     '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/gtav.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
 ]
+load_from = '../../checkpoints/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
