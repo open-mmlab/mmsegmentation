f1eca19c52ce6e6d82882f7259f58bc37c318052
Rachel ZHeng <rachelzh@andrew.cmu.edu>
2022-12-03 15:16:42 -0500

    add wandb hook and change to 20k iter

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -3,6 +3,12 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='MMSegWandbHook',
+            init_kwargs={'project': 'mmsegmentation'},
+            interval=100,
+            log_checkpoint=True,
+            log_checkpoint_metadata=True,
+            num_eval_images=100)
         # dict(type='TensorboardLoggerHook')
         # dict(type='PaviLoggerHook') # for internal services
     ])
--- configs/pspnet/pspnet_r50-d8_4x4_512x1024_40k_gtav.py
+++ configs/pspnet/pspnet_r50-d8_4x4_512x1024_40k_gtav.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/gtav.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
-load_from = '../../checkpoints/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
+load_from = 'mmsegmentation/checkpoints/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
