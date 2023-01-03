9625c9b89c53a9eae201e217fe274b830c2f9dec
Vinson <461158649@qq.com>
2023-01-03 00:19:44 -0500

    Fix the docstring error in the class PascalContextDataset59 (#2450)
    ## Motivation
    
    The docstring in the class PascalContextDataset59 is misleading. Try to fix it.
    
    ## Modification
    
    The docstring in the class PascalContextDataset59 is changed.

--- mmseg/datasets/pascal_context.py
+++ mmseg/datasets/pascal_context.py
@@ -58,10 +58,10 @@ class PascalContextDataset(CustomDataset):
 class PascalContextDataset59(CustomDataset):
     """PascalContext dataset.
 
-    In segmentation map annotation for PascalContext, 0 stands for background,
-    which is included in 60 categories. ``reduce_zero_label`` is fixed to
-    False. The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is
-    fixed to '.png'.
+    In segmentation map annotation for PascalContext59, background is not
+    included in 59 categories. ``reduce_zero_label`` is fixed to True.
+    The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed
+    to '.png'.
 
     Args:
         split (str): Split txt file for PascalContext.
