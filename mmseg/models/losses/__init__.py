1d33cece951e3bf216df166b5c60dd527bdc618b
a839290771 <839290771@qq.com>
2023-01-03 12:53:56 +0800

    pass pre-commit

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -4,9 +4,9 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
 from .dice_loss import DiceLoss
 from .focal_loss import FocalLoss
+from .hiera_triplet_loss_cityscape import HieraTripletLossCityscape  # Hieraseg
 from .lovasz_loss import LovaszLoss
 from .tversky_loss import TverskyLoss
-from .hiera_triplet_loss_cityscape import HieraTripletLossCityscape  # Hieraseg
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
