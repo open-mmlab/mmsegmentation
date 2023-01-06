b062e9cd952fcdf193e0ebbe0bc14101f34f58a1
MengzhangLI <mcmong@pku.edu.cn>
2023-01-06 15:55:27 +0800

    fix typo

--- mmseg/datasets/synapse.py
+++ mmseg/datasets/synapse.py
@@ -8,8 +8,8 @@ class SynapseDataset(BaseSegDataset):
     """Synapse dataset.
 
     Before dataset preprocess of Synapse, there are total 13 categories of
-    foreground which does not include background. After preprocess, 8
-    foreground categories are kept while other 5 foreground categories are
+    foreground which does not include background. After preprocessing, 8
+    foreground categories are kept while the other 5 foreground categories are
     handled as background. The ``img_suffix`` is fixed to '.jpg' and
     ``seg_map_suffix`` is fixed to '.png'.
     """
