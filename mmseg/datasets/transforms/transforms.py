6521e368dd80ff833759780012ca8d08d66891b3
Wencheng Wu <41542251+274869388@users.noreply.github.com>
2023-01-11 11:51:29 +0800

    [Feature] Add `gt_edge_map` field. (#2466)
    ## Motivation
    
    The motivation of this PR is to add `gt_edge_map` field to support
    boundary loss.
    
    ## Modification
    
    - GenerateEdge
    Modify `gt_edge` field to `gt_edge_map`.
    
    - PackSegInputs
    Add `gt_edge_map` to data_sample.
    
    - stack_batch
    Pad `gt_edge_map` to max_shape.
    
    ## BC-breaking (Optional)
    
    No
    
    ## Use cases (Optional)
    
    Reference `GenerateEdge`.

--- mmseg/datasets/transforms/transforms.py
+++ mmseg/datasets/transforms/transforms.py
@@ -1245,7 +1245,7 @@ class GenerateEdge(BaseTransform):
         - gt_seg_map
 
     Added Keys:
-        - gt_edge (np.ndarray, uint8): The edge annotation generated from the
+        - gt_edge_map (np.ndarray, uint8): The edge annotation generated from the
             seg map by extracting border between different semantics.
 
     Args:
@@ -1296,7 +1296,7 @@ class GenerateEdge(BaseTransform):
                                            (self.edge_width, self.edge_width))
         edge = cv2.dilate(edge, kernel)
 
-        results['gt_edge'] = edge
+        results['gt_edge_map'] = edge
         results['edge_width'] = self.edge_width
 
         return results
