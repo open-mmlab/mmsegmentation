e7d1648cd3d1f32f87272d136e1bfaaa4b34da3b
jqqq <59837016+ZephyRUXS@users.noreply.github.com>
2023-01-13 03:18:50 +0800

    Add files via upload

--- mmseg/models/decode_heads/protoseg_head.py
+++ mmseg/models/decode_heads/protoseg_head.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 from torch.nn.functional import interpolate
 from einops import rearrange, repeat
 from mmseg.registry import MODELS
-
+import torch.distributed as dist
 
 class ModuleHelper(object):
 
@@ -60,80 +60,95 @@ class ProjectionHead(nn.Module):
     def forward(self, x):
         return F.normalize(self.proj(x), p=2, dim=1)
 
-class Protoseg_head(nn.Module):
-    def __init__(self,in_channels):
-        super(Hrnet,self).__init__()
-        self.in_channel=in_channels
-        self.num_prototype=10
-        self.update_prototype=True
-        self.gamma=0.999
-        self.pretrain_prototype=False
-        self.num_classes=19
+
+def distributed_sinkhorn(out, sinkhorn_iterations=3, epsilon=0.05):
+    Q = torch.exp(out / epsilon).t()
+    B = Q.shape[1]
+    K = Q.shape[0]
+    sum_Q = torch.sum(Q)
+    Q /= sum_Q
+
+    for _ in range(sinkhorn_iterations):
+        sum_of_rows = torch.sum(Q, dim=1, keepdim=True)
+        Q /= sum_of_rows
+        Q /= K
+        Q /= torch.sum(Q, dim=0, keepdim=True)
+        Q /= B
+
+    Q *= B
+    Q = Q.t()
+
+    indexs = torch.argmax(Q, dim=1)
+    Q = F.gumbel_softmax(Q, tau=0.5, hard=True)
+
+    return Q, indexs
+
+
+class Proto_decoding(nn.Module):
+    def __init__(self, in_channels, num_prototype=10, update_prototype=True, gamma=0.999, pretrain_prototype=False,
+                 num_classes=19, bn_type="torchbn", use_prototype=True):
+        super().__init__()
+        self.num_prototype = num_prototype
+        self.update_prototype = update_prototype
+        self.gamma = gamma
+        self.use_prototype = use_prototype
+        self.pretrain_prototype = pretrain_prototype
+        self.num_classes = num_classes
+        self.bn_type = bn_type
         self.cls_head = nn.Sequential(
             nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
-            ModuleHelper.BNReLU(in_channels, bn_type="torchbn"),
+            ModuleHelper.BNReLU(in_channels, bn_type=bn_type),
             nn.Dropout2d(0.10)
         )
-        
+
         self.prototypes = nn.Parameter(torch.zeros(self.num_classes, self.num_prototype, in_channels),
                                        requires_grad=True)
 
         self.proj_head = ProjectionHead(in_channels, in_channels)
+
         self.feat_norm = nn.LayerNorm(in_channels)
         self.mask_norm = nn.LayerNorm(self.num_classes)
+
     def prototype_learning(self, _c, out_seg, gt_seg, masks):
         pred_seg = torch.max(out_seg, 1)[1]
         mask = (gt_seg == pred_seg.view(-1))
-        cosine_similarity = torch.mm(_c, self.prototypes.view(-1, self.prototypes.shape[-1]).t())
-        proto_logits = cosine_similarity
+        proto_logits = torch.mm(_c, self.prototypes.view(-1, self.prototypes.shape[-1]).t())
         proto_target = gt_seg.clone().float()
 
         protos = self.prototypes.data.clone()
         for k in range(self.num_classes):
             init_q = masks[..., k]
             init_q = init_q[gt_seg == k, ...]
+            print(init_q.shape)
             if init_q.shape[0] == 0:
                 continue
 
             q, indexs = distributed_sinkhorn(init_q)
-
             m_k = mask[gt_seg == k]
-
             c_k = _c[gt_seg == k, ...]
-
             m_k_tile = repeat(m_k, 'n -> n tile', tile=self.num_prototype)
-
             m_q = q * m_k_tile
-
             c_k_tile = repeat(m_k, 'n -> n tile', tile=c_k.shape[-1])
-
-            c_q = c_k * c_k_tile  # n x embedding_dim
-
-            f = m_q.transpose(0, 1) @ c_q  # self.num_prototype x embedding_dim
-
+            c_q = c_k * c_k_tile
+            f = m_q.transpose(0, 1) @ c_q
             n = torch.sum(m_q, dim=0)
-
             if torch.sum(n) > 0 and self.update_prototype is True:
                 f = F.normalize(f, p=2, dim=-1)
-
                 new_value = momentum_update(old_value=protos[k, n != 0, :], new_value=f[n != 0, :],
-                                                momentum=self.gamma, debug=False)
+                                            momentum=self.gamma, debug=False)
                 protos[k, n != 0, :] = new_value
-
             proto_target[gt_seg == k] = indexs.float() + (self.num_prototype * k)
-
         self.prototypes = nn.Parameter(l2_normalize(protos),
-                                           requires_grad=False)
-
+                                       requires_grad=False)
         if dist.is_available() and dist.is_initialized():
             protos = self.prototypes.data.clone()
             dist.all_reduce(protos.div_(dist.get_world_size()))
             self.prototypes = nn.Parameter(protos, requires_grad=False)
 
         return proto_logits, proto_target
-    
-    def forward(self,x,feats,gt_semantic_seg=None, pretrain_prototype=False):
-        _, _, h, w = x[0].size()
+
+    def forward(self, x, gt_semantic_seg=1, pretrain_prototype=False):
+        c = self.cls_head(x)
 
         c = self.proj_head(c)
         _c = rearrange(c, 'b c h w -> (b h w) c')
