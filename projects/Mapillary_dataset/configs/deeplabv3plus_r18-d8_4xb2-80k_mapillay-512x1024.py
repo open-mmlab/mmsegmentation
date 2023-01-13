ad076b6072857672de275a2803d621e2b8e34d3e
a839290771 <839290771@qq.com>
2023-01-13 18:01:47 +0800

    fix bug

--- projects/Mapillary_dataset/configs/deeplabv3plus_r18-d8_4xb2-80k_mapillay-512x1024.py
+++ projects/Mapillary_dataset/configs/deeplabv3plus_r18-d8_4xb2-80k_mapillay-512x1024.py
@@ -94,14 +94,14 @@ param_scheduler = [
         end=80000,
         by_epoch=False)
 ]
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=80000, val_interval=8000)
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=2000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=8000),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='SegVisualizationHook'))
 launcher = 'pytorch'
