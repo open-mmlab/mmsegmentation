217ab6c09209369020341f5b3c1077be0849dfdb
a839290771 <839290771@qq.com>
2023-01-17 21:51:41 +0800

    changes configs

--- projects/Mapillary_dataset/configs/deeplabv3plus_r101-d8_4xb2-80k_mapillay-512x1024.py
+++ projects/Mapillary_dataset/configs/deeplabv3plus_r101-d8_4xb2-80k_mapillay-512x1024.py
@@ -1,24 +1,79 @@
-_base_ = [
-    '../../../configs/deeplabv3plus/deeplabv3plus_\
-        r50-d8_4xb2-80k_cityscapes-512x1024.py',
-    './_base_/datasets/mapillary_v2_0.py',  # v2.0 labels
-]
-
+_base_ = ['./_base_/datasets/mapillary_v1_2.py']  # v 1.2 labels
+# _base_ = ['./_base_/datasets/mapillary_v2_0.py']  # v2.0 labels
 custom_imports = dict(imports=[
     'projects.Mapillary_dataset.mmseg.datasets.mapillary_v1_2',
     'projects.Mapillary_dataset.mmseg.datasets.mapillary_v2_0',
 ])
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+    size=(512, 1024))
+
 model = dict(
-    backbone=dict(type='ResNet', depth=101),
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    pretrained=None,
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
     decode_head=dict(
-        # num_classes=66,  # v1.2
-        num_classes=124,  # v2.0
-    ),
+        type='DepthwiseSeparableASPPHead',
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        dropout_ratio=0.1,
+        num_classes=66,  # v1.2
+        # num_classes=124, # v2.0
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     auxiliary_head=dict(
-        # num_classes=66,  # v1.2
-        num_classes=124,  # v2.0
-        train_cfg=dict(
-            type='IterBasedTrainLoop', max_iters=240000, val_interval=24000)))
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=66,  # v1.2
+        # num_classes=124, # v2.0
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+default_scope = 'mmseg'
+env_cfg = dict(
+    cudnn_benchmark=True,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='SegLocalVisualizer',
+    vis_backends=[dict(type='LocalVisBackend')],
+    name='visualizer')
+log_processor = dict(by_epoch=False)
+log_level = 'INFO'
+load_from = None
+resume = False
+tta_model = dict(type='SegTTAModel')
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optim_wrapper = dict(
     type='OptimWrapper',
@@ -33,6 +88,10 @@ param_scheduler = [
         end=240000,
         by_epoch=False)
 ]
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=240000, val_interval=24000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
