cc9dec2f8b581359c6c44da6083065c9f46fea14
a839290771 <839290771@qq.com>
2023-01-17 22:56:16 +0800

    change config

--- projects/Mapillary_dataset/configs/_base_/datasets/mapillary_v2_0.py
+++ projects/Mapillary_dataset/configs/_base_/datasets/mapillary_v2_0.py
@@ -40,7 +40,7 @@ tta_pipeline = [
         ])
 ]
 train_dataloader = dict(
-    batch_size=4,
+    batch_size=2,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
