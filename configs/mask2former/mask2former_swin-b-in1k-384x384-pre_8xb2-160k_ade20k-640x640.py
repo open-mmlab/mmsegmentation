2a8b5dc0204050c590522b85a022abfbf22e0ec7
Li-Qingyun <962537281@qq.com>
2023-01-30 20:34:38 +0800

    fix config error

--- configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-640x640.py
+++ configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-640x640.py
@@ -49,7 +49,7 @@ model = dict(
         num_transformer_feat_level=3,
         align_corners=False,
         pixel_decoder=dict(
-            type='MSDeformAttnPixelDecoder',
+            type='mmdet.MSDeformAttnPixelDecoder',
             num_outs=3,
             norm_cfg=dict(type='GN', num_groups=32),
             act_cfg=dict(type='ReLU'),
