105726bfdca25314d3b7d93db99f86ace6953d7b
Siddharth Ancha <siddancha@users.noreply.github.com>
2023-01-31 21:26:44 -0500

    Backward merge #2535 from `1.x` into `dev-1.x` (#2546)
    ## Motivation
    
    This is essentially #2535 that I had intended to submit to the `dev-1.x`
    branch but accidentally submitted it directly to the `1.x` branch
    (apologies!). This also got approved possibly because the core devs also
    didn't realize this.
    
    The problem is that now `1.x` and `dev-1.x` are out of sync -- the
    changes introduced by #2535 will never be reflected in `dev-1.x`.
    
    ## Modification
    
    I'm proposing this "backward-merge" so that `1.x` and `dev-1.x` can be
    in sync again. If you look at "files changed", they are exactly the
    changes introduced by #2535.
    
    Co-authored-by: MeowZheng <meowzheng@outlook.com>

--- mmseg/models/segmentors/encoder_decoder.py
+++ mmseg/models/segmentors/encoder_decoder.py
@@ -120,7 +120,7 @@ class EncoderDecoder(BaseSegmentor):
         return x
 
     def encode_decode(self, inputs: Tensor,
-                      batch_img_metas: List[dict]) -> List[Tensor]:
+                      batch_img_metas: List[dict]) -> Tensor:
         """Encode images with backbone and decode into a semantic segmentation
         map of the same size as input."""
         x = self.extract_feat(inputs)
