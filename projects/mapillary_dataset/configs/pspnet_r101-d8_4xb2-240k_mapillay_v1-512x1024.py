7bc2efcb29bed98586f55106c27183ec77eab584
a839290771 <839290771@qq.com>
2023-02-09 11:14:54 +0800

    add mapillary to docs/user_guides/zh_cn/2_dataset_prepare.md

--- projects/mapillary_dataset/configs/pspnet_r101-d8_4xb2-240k_mapillay_v1-512x1024.py
+++ projects/mapillary_dataset/configs/pspnet_r101-d8_4xb2-240k_mapillay_v1-512x1024.py
@@ -1,7 +1,6 @@
 _base_ = [
     '../../../configs/_base_/models/pspnet_r50-d8.py',
-    '../_base_/datasets/mapillary_v1_2.py',
-    '../../../_base_/default_runtime.py'
+    './_base_/datasets/mapillary_v1_2.py', '../../../_base_/default_runtime.py'
 ]
 custom_imports = dict(
     imports=['projects.mapillary_dataset.mmseg.datasets.mapillary'])
