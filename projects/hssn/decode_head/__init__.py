f491633bcd9238ca7afb88af3004f45b0855dc86
Ma Chenglong <masaaki75@qq.com>
2023-02-22 15:08:58 +0800

    Delete projects/hssn directory

--- projects/hssn/decode_head/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .sep_aspp_contrast_head import DepthwiseSeparableASPPContrastHead
-
-__all__ = ['DepthwiseSeparableASPPContrastHead']
--- projects/hssn/losses/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .hiera_triplet_loss_cityscape import HieraTripletLossCityscape
-
-__all__ = ['HieraTripletLossCityscape']
