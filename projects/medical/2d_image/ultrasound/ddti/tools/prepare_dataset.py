7108e4a55084399f82a55fd61401a93dae67b3af
gsz745134809 <745134809@qq.com>
2023-02-22 13:32:16 +0800

    add DDTI dataset configs

---
+++ projects/medical/2d_image/ultrasound/ddti/tools/prepare_dataset.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+
+def map_func(num_map={255: 1, 0: 0}, im_path='./masks/train', suffix='png'):
+
+    imgs = glob.glob(os.path.join(im_path, f'*.{suffix}'))
+
+    for im_path in tqdm(imgs):
+        im = Image.open(im_path)
+        imn = np.array(im)
+        for num in num_map:
+            imn[imn == num] = num_map[num]
+        new_im = Image.fromarray(imn)
+        new_im.save(im_path)
+
+
+if __name__ == '__main__':
+    num_map = {255: 1, 0: 0}
+    im_path = './masks/train'
+    suffix = 'png'
+    map_func(num_map, im_path, suffix)
+    map_func(num_map, './masks/test', suffix)
