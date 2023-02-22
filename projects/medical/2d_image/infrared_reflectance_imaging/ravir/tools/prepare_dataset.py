cd194cfec28d645fa8c3042d4243deedc9966400
gsz745134809 <745134809@qq.com>
2023-02-22 10:22:29 +0800

    add Ravir dataset configs

---
+++ projects/medical/2d_image/infrared_reflectance_imaging/ravir/tools/prepare_dataset.py
@@ -0,0 +1,19 @@
+import os
+import glob
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+
+
+# map = {255:2, 128:1, 0:0}
+
+imgs = glob.glob(os.path.join('./masks/train', '*.png'))
+
+for im_path in tqdm(imgs):
+    im = Image.open(im_path)
+    imn = np.array(im)
+    imn[imn == 255] = 2
+    imn[imn == 128] = 1
+    imn[imn == 0] = 0
+    new_im = Image.fromarray(imn)
+    new_im.save(im_path)
