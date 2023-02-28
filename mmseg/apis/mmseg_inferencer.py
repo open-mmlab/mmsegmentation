200765a236c09bd3a54f0b282f0981a43fdc5d87
MeowZheng <meowzheng@outlook.com>
2023-02-28 23:05:38 +0800

    [Enhancement] Modify interface of MMSeginferencer and add docs

--- mmseg/apis/mmseg_inferencer.py
+++ mmseg/apis/mmseg_inferencer.py
@@ -1,15 +1,20 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os.path as osp
+import warnings
 from typing import List, Optional, Sequence, Union
 
 import mmcv
 import mmengine
 import numpy as np
+import torch.nn as nn
 from mmcv.transforms import Compose
 from mmengine.infer.infer import BaseInferencer, ModelType
+from mmengine.runner.checkpoint import _load_checkpoint_to_model
+from PIL import Image
 
 from mmseg.structures import SegDataSample
-from mmseg.utils import ConfigType, SampleList, register_all_modules
+from mmseg.utils import (ConfigType, SampleList, get_classes, get_palette,
+                         register_all_modules)
 from mmseg.visualization import SegLocalVisualizer
 
 InputType = Union[str, np.ndarray]
@@ -23,41 +28,48 @@ class MMSegInferencer(BaseInferencer):
 
     Args:
         model (str, optional): Path to the config file or the model name
-            defined in metafile. For example, it could be
-            "fcn_r50-d8_4xb2-40k_cityscapes-512x1024" or
-            "configs/fcn/fcn_r50-d8_4xb2-40k_cityscapes-512x1024.py"
+            defined in metafile. Task the `mmseg metafile <https://github.com/open-mmlab/mmsegmentation/blob/dev-1.x/configs/fcn/fcn.yml>`_
+            as an example the `model` could be
+            "fcn_r50-d8_4xb2-40k_cityscapes-512x1024", and the weights of model
+            will be download automatically. If use config file, like
+            "configs/fcn/fcn_r50-d8_4xb2-40k_cityscapes-512x1024.py", the
+            `weights` should be defined.
         weights (str, optional): Path to the checkpoint. If it is not specified
             and model is a model name of metafile, the weights will be loaded
             from metafile. Defaults to None.
-        palette (List[List[int]], optional): The palette of
-                segmentation map.
-        classes (Tuple[str], optional): Category information.
-        dataset_name (str, optional): Name of the datasets supported in mmseg.
+        classes (list, optional): Input classes for result rendering, as the
+            prediction of segmentation model is a segment map with label 
+            indices, `classes` is a list which includes items responding to the 
+            label indices. If classes is not defined, visualizer will take 
+            `cityscapes` classes by default. Defaults to None.
+        palette (list, optional): Input palette for result rendering, which is
+            a list of color palette responding to the classes. Defaults to None.
+        dataset_name (str, optional): `Dataset name or alias <https://github.com/open-mmlab/mmsegmentation/blob/a35e1c4232a03503a2d549206ca703d3850d5244/mmseg/utils/class_names.py#L302-L317>`_
+            visulizer will use the meta information of the dataset i.e. classes
+            and palette, but the `classes` and `palette` have higher priority.
+            Defaults to None.
         device (str, optional): Device to run inference. If None, the available
             device will be automatically used. Defaults to None.
-        scope (str, optional): The scope of the model. Defaults to None.
-    """
+        scope (str, optional): The scope of the model. Defaults to 'mmseg'.
+    """ # noqa
 
     preprocess_kwargs: set = set()
     forward_kwargs: set = {'mode', 'out_dir'}
-    visualize_kwargs: set = {
-        'show', 'wait_time', 'draw_pred', 'img_out_dir', 'opacity'
-    }
-    postprocess_kwargs: set = {
-        'pred_out_dir', 'return_datasample', 'save_mask', 'mask_dir'
-    }
+    visualize_kwargs: set = {'show', 'wait_time', 'img_out_dir', 'opacity'}
+    postprocess_kwargs: set = {'pred_out_dir', 'return_datasample'}
 
     def __init__(self,
                  model: Union[ModelType, str],
                  weights: Optional[str] = None,
-                 palette: Optional[Union[str, List]] = None,
                  classes: Optional[Union[str, List]] = None,
+                 palette: Optional[Union[str, List]] = None,
                  dataset_name: Optional[str] = None,
                  device: Optional[str] = None,
                  scope: Optional[str] = 'mmseg') -> None:
         # A global counter tracking the number of images processes, for
         # naming of the output images
         self.num_visualized_imgs = 0
+        self.num_pred_imgs = 0
         register_all_modules()
         super().__init__(
             model=model, weights=weights, device=device, scope=scope)
@@ -65,48 +77,110 @@ class MMSegInferencer(BaseInferencer):
         assert isinstance(self.visualizer, SegLocalVisualizer)
         self.visualizer.set_dataset_meta(palette, classes, dataset_name)
 
+    def _load_weights_to_model(self, model: nn.Module,
+                               checkpoint: Optional[dict],
+                               cfg: Optional[ConfigType]) -> None:
+        """Loading model weights and meta information from cfg and checkpoint.
+
+        Subclasses could override this method to load extra meta information
+        from ``checkpoint`` and ``cfg`` to model.
+
+        Args:
+            model (nn.Module): Model to load weights and meta information.
+            checkpoint (dict, optional): The loaded checkpoint.
+            cfg (Config or ConfigDict, optional): The loaded config.
+        """
+
+        if checkpoint is not None:
+            _load_checkpoint_to_model(model, checkpoint)
+            checkpoint_meta = checkpoint.get('meta', {})
+            # save the dataset_meta in the model for convenience
+            if 'dataset_meta' in checkpoint_meta:
+                # mmsegmentation 1.x
+                model.dataset_meta = {
+                    'classes': checkpoint_meta['dataset_meta'].get('classes'),
+                    'palette': checkpoint_meta['dataset_meta'].get('palette')
+                }
+            elif 'CLASSES' in checkpoint_meta:
+                # mmsegmentation 0.x
+                classes = checkpoint_meta['CLASSES']
+                palette = checkpoint_meta.get('PALETTE', None)
+                model.dataset_meta = {'classes': classes, 'palette': palette}
+            else:
+                warnings.warn(
+                    'dataset_meta or class names are not saved in the '
+                    'checkpoint\'s meta data, use classes of Cityscapes by '
+                    'default.')
+                model.dataset_meta = {
+                    'classes': get_classes('cityscapes'),
+                    'palette': get_palette('cityscapes')
+                }
+        else:
+            warnings.warn('Checkpoint is not loaded, and the inference '
+                          'result is calculated by the randomly initialized '
+                          'model!')
+            warnings.warn(
+                'weights is None, use cityscapes classes by default.')
+            model.dataset_meta = {
+                'classes': get_classes('cityscapes'),
+                'palette': get_palette('cityscapes')
+            }
+
     def __call__(self,
                  inputs: InputsType,
                  return_datasamples: bool = False,
                  batch_size: int = 1,
                  show: bool = False,
                  wait_time: int = 0,
-                 draw_pred: bool = True,
                  out_dir: str = '',
-                 save_mask: bool = False,
-                 mask_dir: str = 'mask',
+                 img_out_dir: str = 'vis',
+                 pred_out_dir: str = 'pred',
                  **kwargs) -> dict:
         """Call the inferencer.
 
         Args:
-            inputs (Union[str, np.ndarray]): Inputs for the inferencer.
+            inputs (Union[list, str, np.ndarray]): Inputs for the inferencer.
             return_datasamples (bool): Whether to return results as
                 :obj:`SegDataSample`. Defaults to False.
             batch_size (int): Batch size. Defaults to 1.
-            show (bool): Whether to display the image in a popup window.
-                Defaults to False.
+            show (bool): Whether to display the rendering color segmentation
+                mask in a popup window. Defaults to False.
             wait_time (float): The interval of show (s). Defaults to 0.
-            draw_pred (bool): Whether to draw Prediction SegDataSample.
-                Defaults to True.
-            out_dir (str): Output directory of inference results. Defaults: ''.
-            save_mask (bool): Whether save pred mask as a file.
-            mask_dir (str): Sub directory of `pred_out_dir`, used to save pred
-                mask file.
+            out_dir (str): Output directory of inference results. Defaults
+                to ''.
+            img_out_dir (str): Subdirectory of `out_dir`, used to save
+                rendering color segmentation mask, so `out_dir` must be defined
+                if you would like to save predicted mask. Defaults to 'vis'.
+            pred_out_dir (str): Subdirectory of `out_dir`, used to save
+                predicted mask file, so `out_dir` must be defined if you would
+                like to save predicted mask. Defaults to 'pred'.
+
+            **kwargs: Other keyword arguments passed to :meth:`preprocess`,
+                :meth:`forward`, :meth:`visualize` and :meth:`postprocess`.
+                Each key in kwargs should be in the corresponding set of
+                ``preprocess_kwargs``, ``forward_kwargs``, ``visualize_kwargs``
+                and ``postprocess_kwargs``.
+
 
         Returns:
             dict: Inference and visualization results.
         """
+
+        if out_dir != '':
+            pred_out_dir = osp.join(out_dir, pred_out_dir)
+            img_out_dir = osp.join(out_dir, img_out_dir)
+        else:
+            pred_out_dir = ''
+            img_out_dir = ''
+
         return super().__call__(
             inputs=inputs,
             return_datasamples=return_datasamples,
             batch_size=batch_size,
             show=show,
             wait_time=wait_time,
-            draw_pred=draw_pred,
-            img_out_dir=out_dir,
-            pred_out_dir=out_dir,
-            save_mask=save_mask,
-            mask_dir=mask_dir,
+            img_out_dir=img_out_dir,
+            pred_out_dir=pred_out_dir,
             **kwargs)
 
     def visualize(self,
@@ -114,7 +188,6 @@ class MMSegInferencer(BaseInferencer):
                   preds: List[dict],
                   show: bool = False,
                   wait_time: int = 0,
-                  draw_pred: bool = True,
                   img_out_dir: str = '',
                   opacity: float = 0.8) -> List[np.ndarray]:
         """Visualize predictions.
@@ -125,9 +198,8 @@ class MMSegInferencer(BaseInferencer):
             show (bool): Whether to display the image in a popup window.
                 Defaults to False.
             wait_time (float): The interval of show (s). Defaults to 0.
-            draw_pred (bool): Whether to draw Prediction SegDataSample.
-                Defaults to True.
-            img_out_dir (str): Output directory of drawn images. Defaults: ''
+            img_out_dir (str): Output directory of rendering prediction i.e.
+                color segmentation mask. Defaults: ''
             opacity (int, float): The transparency of segmentation mask.
                 Defaults to 0.8.
 
@@ -140,7 +212,7 @@ class MMSegInferencer(BaseInferencer):
         if getattr(self, 'visualizer') is None:
             raise ValueError('Visualization needs the "visualizer" term'
                              'defined in the config, but got None')
-
+        self.visualizer.set_dataset_meta(**self.model.dataset_meta)
         self.visualizer.alpha = opacity
 
         results = []
@@ -153,7 +225,7 @@ class MMSegInferencer(BaseInferencer):
                 img_name = osp.basename(single_input)
             elif isinstance(single_input, np.ndarray):
                 img = single_input.copy()
-                img_num = str(self.num_visualized_imgs).zfill(8)
+                img_num = str(self.num_visualized_imgs).zfill(8) + '_vis'
                 img_name = f'{img_num}.jpg'
             else:
                 raise ValueError('Unsupported input type:'
@@ -169,7 +241,7 @@ class MMSegInferencer(BaseInferencer):
                 show=show,
                 wait_time=wait_time,
                 draw_gt=False,
-                draw_pred=draw_pred,
+                draw_pred=True,
                 out_file=out_file)
             results.append(self.visualizer.get_image())
             self.num_visualized_imgs += 1
@@ -180,63 +252,61 @@ class MMSegInferencer(BaseInferencer):
                     preds: PredType,
                     visualization: List[np.ndarray],
                     return_datasample: bool = False,
-                    mask_dir: str = 'mask',
-                    save_mask: bool = True,
                     pred_out_dir: str = '') -> dict:
         """Process the predictions and visualization results from ``forward``
         and ``visualize``.
 
         This method should be responsible for the following tasks:
 
-        1. Convert datasamples into a json-serializable dict if needed.
-        2. Pack the predictions and visualization results and return them.
-        3. Dump or log the predictions.
+        1. Pack the predictions and visualization results and return them.
+        2. Save the predictions, if it needed.
 
         Args:
             preds (List[Dict]): Predictions of the model.
-            visualization (np.ndarray): Visualized predictions.
+            visualization (List[np.ndarray]): The list of rendering color
+                segmentation mask.
             return_datasample (bool): Whether to return results as datasamples.
                 Defaults to False.
             pred_out_dir: File to save the inference results w/o
                 visualization. If left as empty, no file will be saved.
                 Defaults to ''.
-            mask_dir (str): Sub directory of `pred_out_dir`, used to save pred
-                mask file.
-            save_mask (bool): Whether save pred mask as a file.
 
         Returns:
             dict: Inference and visualization results with key ``predictions``
             and ``visualization``
 
             - ``visualization (Any)``: Returned by :meth:`visualize`
-            - ``predictions`` (dict or DataSample): Returned by
+            - ``predictions`` (List[np.ndarray], np.ndarray): Returned by
               :meth:`forward` and processed in :meth:`postprocess`.
-              If ``return_datasample=False``, it usually should be a
-              json-serializable dict containing only basic data elements such
-              as strings and numbers.
+              If ``return_datasample=False``, it will be the segmentation mask
+              with label indice.
         """
+
         results_dict = {}
 
-        results_dict['predictions'] = preds
-        results_dict['visualization'] = visualization
+        results_dict['predictions'] = []
+        results_dict['visualization'] = []
 
-        if pred_out_dir != '':
-            mmengine.mkdir_or_exist(pred_out_dir)
-            if save_mask:
-                preds = [preds] if isinstance(preds, SegDataSample) else preds
-                for pred in preds:
-                    mmcv.imwrite(
-                        pred.pred_sem_seg.numpy().data[0],
-                        osp.join(pred_out_dir, mask_dir,
-                                 osp.basename(pred.metainfo['img_path'])))
-            else:
-                mmengine.dump(results_dict,
-                              osp.join(pred_out_dir, 'results.pkl'))
+        for pred, vis in zip(preds, visualization):
+            pred_data = pred.pred_sem_seg.numpy().data[0]
+            results_dict['predictions'].append(pred_data)
+            results_dict['visualization'].append(vis)
+            if pred_out_dir != '':
+                mmengine.mkdir_or_exist(pred_out_dir)
+                img_name = str(self.num_pred_imgs).zfill(8) + '_pred.png'
+                img_path = osp.join(pred_out_dir, img_name)
+                output = Image.fromarray(pred_data.astype(np.uint8))
+                output.save(img_path)
+            self.num_pred_imgs += 1
 
         if return_datasample:
             return preds
-
-        return results_dict
+        else:
+            if len(results_dict['predictions']) == 1:
+                results_dict['predictions'] = results_dict['predictions'][0]
+                results_dict['visualization'] = \
+                    results_dict['visualization'][0]
+            return results_dict
 
     def _init_pipeline(self, cfg: ConfigType) -> Compose:
         """Initialize the test pipeline.
