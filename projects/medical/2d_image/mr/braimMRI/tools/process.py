e9a1b9a733f988f5b277712530a163ff67aed67f
gsz745134809 <745134809@qq.com>
2023-04-06 19:24:56 +0800

    brainMRI

---
+++ projects/medical/2d_image/mr/braimMRI/tools/process.py
@@ -0,0 +1,66 @@
+# import os
+# import glob
+# from PIL import Image
+# from tqdm import tqdm
+# import numpy as np
+
+# images = glob.glob(os.path.join('/mnt/petrelfs/guosizheng/gsz/2d_fenge_todo/braimMRI/images/train', '*.tif'))  # noqa
+
+# masks = glob.glob(os.path.join('/mnt/petrelfs/guosizheng/gsz/2d_fenge_todo/braimMRI/masks/train', '*.tif'))  # noqa
+
+# for i in tqdm(images):
+#     img = Image.open(i)
+#     img.save(i.replace('.tif', '.png'))
+
+# for i in tqdm(masks):
+#     img = Image.open(i)
+#     img = np.array(img)
+#     img[img>0] = 1
+#     img = Image.fromarray(img)
+#     img.save(i.replace('_mask.tif', '.png'))
+#     # img.save(i)
+
+# from sklearn.model_selection import train_test_split
+# import argparse
+# import os
+# import glob
+
+# def save_anno(img_list, file_path, remove_suffix=True):
+#     if remove_suffix:#（文件路径从data/${image/masks}之后的相对路径开始）
+#         img_list = ["/".join(img_path.split('/')[-2:]) for img_path in img_list]  # noqa
+#         img_list = [".".join(img_path.split('.')[:-1]) for img_path in img_list]  # noqa
+#     with open(file_path, 'w') as file_:
+#         for x in list(img_list):
+#             file_.write(x + '\n')
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("data_root", default="data/")
+#     args = parser.parse_args()
+#     data_root = args.data_root
+#     if os.path.exists(os.path.join(data_root, "masks/val")):
+#         x_val = sorted(glob.glob(data_root + "/images/val/*.png"))
+#         save_anno(x_val, data_root + '/val.txt')
+#     if os.path.exists(os.path.join(data_root, "masks/test")):
+#         x_test = sorted(glob.glob(data_root + "/images/test/*.png"))
+#         save_anno(x_test, data_root + '/test.txt')
+#     if not os.path.exists(os.path.join(data_root, "masks/val")) and not os.path.exists(os.path.join(data_root, "masks/test")):  # noqa
+#         all_imgs= sorted(glob.glob(data_root + "/images/train/*.png"))
+#         x_train, x_val = train_test_split(all_imgs, test_size=0.2, random_state=0)  # noqa
+#         save_anno(x_train, data_root + '/train.txt')
+#         save_anno(x_val, data_root + '/val.txt')
+#     else:
+#         x_train = sorted(glob.glob(data_root + "/images/train/*.png"))
+#         save_anno(x_train, data_root + '/train.txt')
+#     # --------生成md5值以及包含无标签image的list，pr时该部分代码将被删除---------
+#     import hashlib
+#     all_imgs = []
+#     for fpath, dirname, fnames in os.walk(os.path.join(data_root, "images")):  # noqa
+#         for fname in fnames:
+#             all_imgs.append(os.path.join(fpath, fname))
+#     f_ = open(data_root+ "/images_md5_list.txt", "w")
+#     for img in sorted(all_imgs):
+#         with open(img, 'rb') as fd:
+#             fmd5 = hashlib.md5(fd.read()).hexdigest()
+#         f_.write(fmd5 + "\t" + '/'.join(img.split('/')[-2:]) + '\n')
+#     f_.close()
