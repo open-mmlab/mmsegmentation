bd624481c96f2420eb6551fbe003e505e635ac2f
Mengyang Liu <myliu@seu.edu.cn>
2023-05-06 16:11:40 +0800

    Init SwinTransformerV2

---
+++ configs/swinv2/README.md
@@ -0,0 +1,1 @@
+# SwinTransformerV2
---
+++ mmseg/models/backbones/swinv2.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class SwinTransformerV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        
+        return x
