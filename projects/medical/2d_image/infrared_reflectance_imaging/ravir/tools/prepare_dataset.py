05c5e704f3acc729f5cbcc752fa4999bcc1c832a
gsz745134809 <745134809@qq.com>
2023-05-16 19:41:37 +0800

    update data path

--- projects/medical/2d_image/infrared_reflectance_imaging/ravir/tools/prepare_dataset.py
+++ projects/medical/2d_image/infrared_reflectance_imaging/ravir/tools/prepare_dataset.py
@@ -7,7 +7,21 @@ from tqdm import tqdm
 
 # map = {255:2, 128:1, 0:0}
 
-imgs = glob.glob(os.path.join('./masks/train', '*.png'))
+os.makedirs('data/ravir/images/train', exist_ok=True)
+os.makedirs('data/ravir/images/test', exist_ok=True)
+os.makedirs('data/ravir/masks/train', exist_ok=True)
+
+os.system(
+    r'cp data/ravir/RAVIR\ Dataset/train/training_images/* data/ravir/images/train'  # noqa
+)
+os.system(
+    r'cp data/ravir/RAVIR\ Dataset/train/training_masks/* data/ravir/masks/train'  # noqa
+)
+os.system(r'cp data/ravir/RAVIR\ Dataset/test/* data/ravir/images/test')
+
+os.system(r'rm -rf data/ravir/RAVIR\ Dataset')
+
+imgs = glob.glob(os.path.join('data/ravir/masks/train', '*.png'))
 
 for im_path in tqdm(imgs):
     im = Image.open(im_path)
