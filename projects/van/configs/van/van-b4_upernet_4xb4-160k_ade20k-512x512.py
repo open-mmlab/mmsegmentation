bfd41574455c51e419229e2f1367711e3bf90861
xiexinch <xiexinch@outlook.com>
2023-05-22 19:43:38 +0800

    --fix=fix config

--- projects/van/configs/van/van-b4_upernet_4xb4-160k_ade20k-512x512.py
+++ projects/van/configs/van/van-b4_upernet_4xb4-160k_ade20k-512x512.py
@@ -1,4 +1,4 @@
-_base_ = './van-b2_pre1k_upernet_4xb2-160k_ade20k-512x512.py'
+_base_ = './van-b2_upernet_4xb2-160k_ade20k-512x512.py'
 ckpt_path = 'https://download.openmmlab.com/mmsegmentation/v0.5/van_3rdparty/van-b4_3rdparty_20230522-1d71c077.pth'  # noqa
 model = dict(
     backbone=dict(
