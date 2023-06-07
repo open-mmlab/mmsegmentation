0beaf6904700692f5ae959fd0ec0d4675396e4aa
Yinlei Sun <ginray0215@gmail.com>
2023-06-07 15:30:24 +0800

    [Enhance] Enable full precision training on Ascend NPU. (#3085)
    ## Motivation
    
    We will support full precision training on the next generation Ascend
    NPU, so there is no need to enable mixed precision by default.
    
    ## Modification
    
    Determine whether the current chip supports full precision training, and
    automatically enable mixed precision.
    
    ## BC-breaking (Optional)
    
    Not affected.
    
    ## Use cases (Optional)
    
    We have verified the correctness on the Ascend NPU.

--- mmseg/utils/__init__.py
+++ mmseg/utils/__init__.py
@@ -3,9 +3,11 @@ from .collect_env import collect_env
 from .logger import get_root_logger
 from .misc import find_latest_checkpoint
 from .set_env import setup_multi_processes
-from .util_distribution import build_ddp, build_dp, get_device
+from .util_distribution import (build_ddp, build_dp, get_device,
+                                is_npu_support_full_precision)
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
-    'setup_multi_processes', 'build_ddp', 'build_dp', 'get_device'
+    'setup_multi_processes', 'build_ddp', 'build_dp', 'get_device',
+    'is_npu_support_full_precision'
 ]
