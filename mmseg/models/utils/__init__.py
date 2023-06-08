6b5740e02a4b00ca56e1e45c8a6caf04e4f9dea3
SheffieldCao <1751899@tongji.edu.cn>
2023-06-08 10:08:50 +0000

    Add CLIP model

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -7,7 +7,8 @@ from .make_divisible import make_divisible
 from .ppm import DAPPM, PAPPM
 from .res_layer import ResLayer
 from .se_layer import SELayer
-from .self_attention_block import SelfAttentionBlock
+from .self_attention_block import (FullAttention, LinearAttention,
+                                   SelfAttentionBlock)
 from .shape_convert import (nchw2nlc2nchw, nchw_to_nlc, nlc2nchw2nlc,
                             nlc_to_nchw)
 from .up_conv_block import UpConvBlock
@@ -17,5 +18,6 @@ __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
     'UpConvBlock', 'InvertedResidualV3', 'SELayer', 'PatchEmbed',
     'nchw_to_nlc', 'nlc_to_nchw', 'nchw2nlc2nchw', 'nlc2nchw2nlc', 'Encoding',
-    'Upsample', 'resize', 'DAPPM', 'PAPPM', 'BasicBlock', 'Bottleneck'
+    'Upsample', 'resize', 'DAPPM', 'PAPPM', 'BasicBlock', 'Bottleneck',
+    'FullAttention', 'LinearAttention'
 ]
