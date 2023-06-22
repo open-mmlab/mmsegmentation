fe6592da0e6c20449422bd8e4a5762d4e482a6d8
xiexinch <xiexinch@outlook.com>
2023-06-22 11:02:42 +0800

    --other=fix path

--- projects/medical/2d_image/dermoscopy/isic2017_task1/configs/fcn-unet-s5-d16_unet_1xb16-0.0001-20k_isic2017-task1-512x512.py
+++ projects/medical/2d_image/dermoscopy/isic2017_task1/configs/fcn-unet-s5-d16_unet_1xb16-0.0001-20k_isic2017-task1-512x512.py
@@ -3,7 +3,7 @@ _base_ = [
     'mmseg::_base_/default_runtime.py',
     'mmseg::_base_/schedules/schedule_20k.py'
 ]
-custom_imports = dict(imports='datasets.isic2017_task1')
+custom_imports = dict(imports='datasets.isic2017-task1_dataset')
 img_scale = (512, 512)
 data_preprocessor = dict(size=img_scale)
 optimizer = dict(lr=0.0001)
