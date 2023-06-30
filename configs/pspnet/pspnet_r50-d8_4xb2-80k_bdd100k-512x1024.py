f01446c3fd73aea13e6dbab263c27656fc64543c
CastleDream <hs8023hfp@gmail.com>
2023-06-30 23:59:34 +0800

    modify Config pspnet_r50-d8_4xb2-80k_bdd100k-512x1024.py

--- configs/pspnet/pspnet_r50-d8_4xb2-80k_bdd100k-512x1024.py
+++ configs/pspnet/pspnet_r50-d8_4xb2-80k_bdd100k-512x1024.py
@@ -1,23 +1,7 @@
 _base_ = [
-    '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/models/pspnet_r50-d8.py', '../_base_/datasets/bdd100k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 crop_size = (512, 1024)
 data_preprocessor = dict(size=crop_size)
 model = dict(data_preprocessor=data_preprocessor)
-
-train_dataloader = dict(
-    dataset=dict(
-        type='BDD100KDataset',
-        data_root='data/bdd100k/',
-        data_prefix=dict(
-            img_path='images/10k/train',
-            seg_map_path='labels/sem_seg/masks/train')))
-val_dataloader = dict(
-    dataset=dict(
-        type='BDD100KDataset',
-        data_root='data/bdd100k/',
-        data_prefix=dict(
-            img_path='images/10k/val',
-            seg_map_path='labels/sem_seg/masks/val')))
-test_dataloader = None
