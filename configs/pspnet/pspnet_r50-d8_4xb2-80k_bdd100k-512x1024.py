ce2d593461a824261023e8f57c6351a69f8fe4b9
CastleDream <hs8023hfp@gmail.com>
2023-06-30 21:32:41 +0800

    modify Config XXX_bdd200_XXX.py

--- configs/pspnet/pspnet_r50-d8_4xb2-80k_bdd100k-512x1024.py
+++ configs/pspnet/pspnet_r50-d8_4xb2-80k_bdd100k-512x1024.py
@@ -1,19 +1,1 @@
 _base_ = './pspnet_r50-d8_4xb2-80k_cityscapes-512x1024.py'
-
-train_dataloader = dict(
-    dataset=dict(
-        type='BDD100KDataset',
-        data_root='bdd100k',
-        data_prefix=dict(
-            img_path='images/10k/train',
-            seg_map_path='labels/sem_seg/masks/train'),
-    ))
-val_dataloader = dict(
-    dataset=dict(
-        type='BDD100KDataset',
-        data_root='bdd100k',
-        data_prefix=dict(
-            img_path='images/10k/val',
-            seg_map_path='labels/sem_seg/masks/val'),
-    ))
-test_dataloader = val_dataloader
