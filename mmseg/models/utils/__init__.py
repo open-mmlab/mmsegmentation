c0cad61231c049da6ca284005f057345e39c5a12
Angie_mydesk <angiecao68@gmail.com>
2023-07-25 13:47:20 +0800

    fix circular import

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -6,13 +6,15 @@ from .inverted_residual import InvertedResidual, InvertedResidualV3
 from .make_divisible import make_divisible
 from .ppm import DAPPM, PAPPM
 from .res_layer import ResLayer
-from .san_layers import MLP, LayerNorm, cross_attn_layer
 from .se_layer import SELayer
 from .self_attention_block import SelfAttentionBlock
 from .shape_convert import (nchw2nlc2nchw, nchw_to_nlc, nlc2nchw2nlc,
                             nlc_to_nchw)
 from .up_conv_block import UpConvBlock
+
+# isort: off
 from .wrappers import Upsample, resize
+from .san_layers import MLP, LayerNorm, cross_attn_layer
 
 __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
