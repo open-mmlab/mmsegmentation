c7f3167a7d56df34426ba07ede0a96e44a8ea08b
Angie_mydesk <angiecao68@gmail.com>
2023-07-25 12:25:49 +0800

    Add configs for SAN

--- mmseg/models/__init__.py
+++ mmseg/models/__init__.py
@@ -7,6 +7,7 @@ from .decode_heads import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
 from .segmentors import *  # noqa: F401,F403
+from .text_encoder import *  # noqa: F401,F403
 
 __all__ = [
     'BACKBONES', 'HEADS', 'LOSSES', 'SEGMENTORS', 'build_backbone',
--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -2,8 +2,10 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
+from .multimodel_encoder_decoder import MultimodelEncoderDecoder
 from .seg_tta import SegTTAModel
 
 __all__ = [
-    'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SegTTAModel'
+    'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SegTTAModel',
+    'MultimodelEncoderDecoder'
 ]
