7bec537287c9d7637135e74fe7c6d4a0f128336a
Angie_mydesk <angiecao68@gmail.com>
2023-08-16 12:28:02 +0800

    fix dataset configs

--- configs/san/san-vit-b16_voc12aug-640x640.py
+++ configs/san/san-vit-b16_voc12aug-640x640.py
@@ -5,24 +5,31 @@ _base_ = [
 ]
 crop_size = (640, 640)
 
+metainfo = dict(
+    classes=('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
+             'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+             'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'),
+    palette=[[128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
+             [128, 0, 128], [0, 128, 128], [128, 128, 128], [64, 0, 0],
+             [192, 0, 0], [64, 128, 0], [192, 128, 0], [64, 0, 128],
+             [192, 0, 128], [64, 128, 128], [192, 128, 128], [0, 64, 0],
+             [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128]])
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='ResizeShortestEdge', scale=crop_size, max_size=2560),
     dict(type='LoadAnnotations'),
     dict(type='PackSegInputs')
 ]
-
 # By default, models are trained on 8 GPUs with 2 images per GPU
 train_dataloader = dict(batch_size=2)
 val_dataloader = dict(
-    batch_size=1, dataset=dict(reduce_zero_label=True, pipeline=test_pipeline))
+    batch_size=1, dataset=dict(metainfo=metainfo, pipeline=test_pipeline))
 test_dataloader = val_dataloader
 
 data_preprocessor = dict(
-    size=crop_size,
     mean=[122.7709, 116.7460, 104.0937],
     std=[68.5005, 66.6322, 70.3232],
-    size_divisor=32,
+    size_divisor=640,
     test_cfg=dict(size_divisor=32))
 model = dict(
     data_preprocessor=data_preprocessor,
