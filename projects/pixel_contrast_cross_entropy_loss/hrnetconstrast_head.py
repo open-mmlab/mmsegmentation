d7a4bd39aaac7b9dfb1c6ae49414aead3e102cc3
BLUE-coconut <1367012021@qq.com>
2023-09-06 22:17:56 +0800

    modify pixel_contrast_cross_entropy_loss

--- projects/pixel_contrast_cross_entropy_loss/hrnetconstrast_head.py
+++ projects/pixel_contrast_cross_entropy_loss/hrnetconstrast_head.py
@@ -5,18 +5,37 @@ blob/2c8c35a8949fef74599f5ec557d340a14415f20d/
 paddleseg/models/hrnet_contrast.py(Apache-2.0 License)"""
 
 from typing import List, Tuple
-
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import ConvModule
 from torch import Tensor
 
-from mmseg.models.builder import HEADS, build_head
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 from mmseg.models.losses import accuracy
 from mmseg.registry import MODELS
 from mmseg.utils import ConfigType, SampleList
+import warnings
 
+def resize(input,
+           size=None,
+           scale_factor=None,
+           mode='nearest',
+           align_corners=None,
+           warning=True):
+    if warning:
+        if size is not None and align_corners:
+            input_h, input_w = tuple(int(x) for x in input.shape[2:])
+            output_h, output_w = tuple(int(x) for x in size)
+            if output_h > input_h or output_w > output_h:
+                if ((output_h > 1 and output_w > 1 and input_h > 1
+                     and input_w > 1) and (output_h - 1) % (input_h - 1)
+                        and (output_w - 1) % (input_w - 1)):
+                    warnings.warn(
+                        f'When align_corners={align_corners}, '
+                        'the output would more aligned if '
+                        f'input size {(input_h, input_w)} is `x+1` and '
+                        f'out size {(output_h, output_w)} is `nx+1`')
+    return F.interpolate(input, size, scale_factor, mode, align_corners)
 
 class ProjectionHead(nn.Module):
     """The projection head used by contrast learning.
@@ -30,51 +49,25 @@ class ProjectionHead(nn.Module):
             only support 'linear' and 'convmlp'. Default: 'convmlp'.
     """
 
-    def __init__(self, in_channels: int, out_channels=256, proj='convmlp'):
+    def __init__(self, in_channels: int, proj_n=256, proj_mode='convmlp'):
         super().__init__()
-        if proj == 'linear':
-            self.proj = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        elif proj == 'convmlp':
+        if proj_mode == 'linear':
+            self.proj = nn.Conv2d(in_channels, proj_n, kernel_size=1)
+        elif proj_mode == 'convmlp':
             self.proj = nn.Sequential(
                 ConvModule(in_channels, in_channels, kernel_size=1),
-                nn.Conv2d(in_channels, out_channels, kernel_size=1),
+                nn.Conv2d(in_channels, proj_n, kernel_size=1),
             )
         else:
             raise KeyError("The type of project head only support 'linear' \
-                        and 'convmlp', but got {}.".format(proj))
+                        and 'convmlp', but got {}.".format(proj_mode))
 
     def forward(self, x: Tensor) -> Tensor:
         return F.normalize(self.proj(x), p=2.0, dim=1)
 
 
-class SegmentationHead(nn.Module):
-    """The segmentation head used by contrast learning.
-
-    Args:
-        dim_in (int):
-            The dimensions of input features.
-        proj_dim (int, optional):
-            The output dimensions of projection head. Default: 256.
-        proj (str, optional):
-            The type of projection head,
-            only support 'linear' and 'convmlp'. Default: 'convmlp'.
-    """
-
-    def __init__(self, in_channels: int, out_channels=19, drop_prob=0.1):
-        super().__init__()
-
-        self.seg = nn.Sequential(
-            ConvModule(in_channels, in_channels, kernel_size=1),
-            nn.Dropout2d(drop_prob),
-            nn.Conv2d(in_channels, out_channels, kernel_size=1),
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        return self.seg(x)
-
-
 @MODELS.register_module()
-class ContrastHead(nn.Module):
+class ContrastHead(BaseDecodeHead):
     """The segmentation head used by contrast learning.
 
     Args:
@@ -87,86 +80,61 @@ class ContrastHead(nn.Module):
     """
 
     def __init__(self,
-                 type='ContrastHead',
-                 in_channels=[18, 36, 72, 144],
-                 in_index=(0, 1, 2, 3),
-                 channels=sum([18, 36, 72, 144]),
-                 input_transform='resize_concat',
+                 in_channels,
+                 channels,
+                 num_classes,
                  proj_n=256,
                  proj_mode='convmlp',
                  drop_p=0.1,
-                 dropout_ratio=-1,
-                 num_classes=19,
-                 norm_cfg=norm_cfg,
-                 align_corners=False,
-                 loss_decode=[
-                     dict(
-                         type='PixelContrastCrossEntropyLoss',
-                         base_temperature=0.07,
-                         temperature=0.1,
-                         ignore_index=255,
-                         max_samples=1024,
-                         max_views=100,
-                         loss_weight=0.1),
-                     dict(type='CrossEntropyLoss', loss_weight=1.0)
-                 ],
-                 fcn_head=dict(
+                 seg_head=dict(
                      type='FCNHead',
-                     in_channels=2048,
-                     in_index=3,
-                     channels=512,
-                     num_convs=2,
-                     concat_input=True,
-                     dropout_ratio=0.1,
-                     num_classes=150,
-                     norm_cfg=norm_cfg,
-                     align_corners=False,
-                     loss_decode=dict(
-                         type='CrossEntropyLoss',
-                         use_sigmoid=False,
-                         loss_weight=1.0))**kwargs):
-        super().__init__(**kwargs)
+                     in_channels=[18, 36, 72, 144],
+                     in_index=(0, 1, 2, 3),
+                     channels=sum([18, 36, 72, 144]),
+                     input_transform='resize_concat',
+                     kernel_size=1,
+                     num_convs=1,
+                     concat_input=False,
+                     dropout_ratio=-1,
+                     num_classes=19,
+                     norm_cfg=dict(type='SyncBN', requires_grad=True),
+                     align_corners=False),
+                 loss_decode=[
+                    dict(type='PixelContrastCrossEntropyLoss',
+                        base_temperature=0.07,
+                        temperature=0.1,
+                        ignore_index=255,
+                        max_samples=1024,
+                        max_views=100,
+                        loss_weight=0.1),
+                    dict(type='CrossEntropyLoss', loss_weight=1.0)
+                    ],
+                    **kwargs):
+        super().__init__(in_channels,
+                 channels,
+                 num_classes = num_classes,
+                 loss_decode = loss_decode,
+                 **kwargs)
+
+
         if proj_n <= 0:
             raise KeyError('proj_n must >0')
         if drop_p < 0 or drop_p > 1 or not isinstance(drop_p, float):
             raise KeyError('drop_p must be a float >=0')
         self.proj_n = proj_n
-
-        self.seghead = MODELS.build(fcn_head)
+            
+        self.seghead = MODELS.build(seg_head)
         self.projhead = ProjectionHead(
-            in_channels=self.in_channels, out_channels=proj_n, proj=proj_mode)
-
-    def _transform_inputs(self, inputs):
-        """Transform inputs for decoder.
-
-        Args:
-            inputs (list[Tensor]): List of multi-level img features.
-
-        Returns:
-            Tensor: The transformed inputs
-        """
-
-        if self.input_transform == 'resize_concat':
-            inputs = [inputs[i] for i in self.in_index]
-            upsampled_inputs = [
-                resize(
-                    input=x,
-                    size=inputs[0].shape[2:],
-                    mode='bilinear',
-                    align_corners=self.align_corners) for x in inputs
-            ]
-            inputs = torch.cat(upsampled_inputs, dim=1)
-        elif self.input_transform == 'multiple_select':
-            inputs = [inputs[i] for i in self.in_index]
-        else:
-            inputs = inputs[self.in_index]
-
-        return inputs
+            in_channels=self.in_channels, proj_n=proj_n, proj_mode=proj_mode)
+        
+    def cls_seg(self):
+        '''Remove cls_seg, or distributed training will encounter an error'''
+        pass
 
     def forward(self, inputs):
-        inputs = self._transform_inputs(inputs)
         output = []
         output.append(self.seghead(inputs))
+        inputs = self._transform_inputs(inputs)
         output.append(self.projhead(inputs))
 
         return output
@@ -202,25 +170,39 @@ class ContrastHead(nn.Module):
             losses_decode = self.loss_decode
 
         for loss_decode in losses_decode:
-            if loss_decode.loss_name == 'loss_ce':
+            if loss_decode.loss_name in ['loss_ce']:
                 pred = F.interpolate(
                     input=seg_logits[0],
                     size=seg_label.shape[-2:],
                     mode='bilinear',
                     align_corners=True)
-                loss[loss_decode.loss_name] = loss_decode(
-                    pred,
-                    seg_label,
-                    weight=seg_weight,
-                    ignore_index=self.ignore_index)
+                if loss_decode.loss_name not in loss:
+                    loss[loss_decode.loss_name] = loss_decode(
+                        pred,
+                        seg_label,
+                        weight=seg_weight,
+                        ignore_index=self.ignore_index)
+                else:
+                    loss[loss_decode.loss_name] += loss_decode(
+                        pred,
+                        seg_label,
+                        weight=seg_weight,
+                        ignore_index=self.ignore_index)
             elif loss_decode.loss_name == 'loss_pixel_contrast_cross_entropy':
-                loss[loss_decode.loss_name] = loss_decode(
-                    seg_logits, seg_label)
+                if loss_decode.loss_name not in loss:
+                    loss[loss_decode.loss_name] = loss_decode(
+                        seg_logits, seg_label)
+                else:
+                    loss[loss_decode.loss_name] += loss_decode(
+                        seg_logits, seg_label)
             else:
                 raise KeyError('loss_name not matched')
 
         loss['acc_seg'] = accuracy(
-            F.interpolate(seg_logits[0], seg_label.shape[1:]),
+            F.interpolate(seg_logits[0], 
+                          size=seg_label.shape[-2:],
+                          mode='bilinear',
+                          align_corners=True),
             seg_label,
             ignore_index=self.ignore_index)
         return loss
@@ -257,7 +239,7 @@ class ContrastHead(nn.Module):
         """
 
         seg_logits = resize(
-            input=seg_logits,
+            input=seg_logits[0],
             size=batch_img_metas[0]['img_shape'],
             mode='bilinear',
             align_corners=self.align_corners)
