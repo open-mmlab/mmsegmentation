ec72867bb3ea5b6b4ff199f7c0897468bc1c7e57
CastleDream <hs8023hfp@gmail.com>
2023-09-29 19:59:28 +0800

    update unit test

--- tests/test_models/test_losses/test_inverseform_loss.py
+++ tests/test_models/test_losses/test_inverseform_loss.py
@@ -1,7 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import urllib
+
+import pytest
 import torch
 
-# from mmseg.models import build_loss
 from mmseg.models.losses import InverseFormLoss
 
 
@@ -15,9 +17,17 @@ def test_inverseform_loss():
         print(f'pred is:{pred[i]}, \n target is {target[i]}')
 
     # test inverseform_loss
+    pretraind_model_url = 'https://github.com/Qualcomm-AI-research/InverseForm\
+        /releases/download/v1.0/distance_measures_regressor.pth'
+
     inverseNet_path = './checkpoints/distance_measures_regressor.pth'
-    # loss_cfg = dict(type='InverseFormLoss', inverseNet_path=inverseNet_path)
-    # inverseform_loss = build_loss(loss_cfg)
+    urllib.request.urlretrieve(pretraind_model_url, inverseNet_path)
+
     loss_class = InverseFormLoss(inverseNet_path=inverseNet_path)
-    loss = loss_class(pred, target)
+    with pytest.raises(AssertionError):
+        loss = loss_class(pred, target)
     assert isinstance(loss, torch.Tensor)
+
+
+# if __name__ == "__main__":
+#     test_inverseform_loss()
