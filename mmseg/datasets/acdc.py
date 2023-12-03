b98927ca50e52f0b7eba6b375b654aeace0843a8
Tonyzhang2000 <zhangshutong2000@gmail.com>
2023-12-03 03:54:21 -0500

    Add script for acdc dataset.

---
+++ mmseg/datasets/acdc.py
@@ -0,0 +1,15 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .cityscapes import CityscapesDataset
+
+
+@DATASETS.register_module()
+class ACDCDataset(CityscapesDataset):
+    """ACDCDataset dataset."""
+
+    def __init__(self,
+                 img_suffix='_rgb_anon.png',
+                 seg_map_suffix='_gt_labelTrainIds.png',
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
