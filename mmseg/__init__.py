e2e2b80d72d24262b66ecc537a985b78b9f53432
Rabin Adhikari <rabin.adk1@gmail.com>
2024-05-09 14:50:24 +0545

    Bump the MMCV_MAX to 2.3.0 and avoid changing the comparision logic to pass the tests

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -8,7 +8,7 @@ from packaging.version import parse
 from .version import __version__, version_info
 
 MMCV_MIN = '2.0.0rc4'
-MMCV_MAX = '2.2.0'
+MMCV_MAX = '2.3.0'
 MMENGINE_MIN = '0.5.0'
 MMENGINE_MAX = '1.0.0'
 
@@ -62,9 +62,9 @@ assert (mmcv_version >= mmcv_min_version), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={MMCV_MIN}.'
 
-assert (mmcv_version <= mmcv_max_version), \
+assert (mmcv_version < mmcv_max_version), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv<={MMCV_MAX}.'
+    f'Please install mmcv<{MMCV_MAX}.'
 
 mmengine_min_version = digit_version(MMENGINE_MIN)
 mmengine_max_version = digit_version(MMENGINE_MAX)
@@ -74,7 +74,7 @@ assert (mmengine_version >= mmengine_version), \
     f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
     f'Please install mmengine>={MMENGINE_MIN}.'
 
-assert (mmengine_version <= mmengine_max_version), \
+assert (mmengine_version < mmengine_max_version), \
     f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
-    f'Please install mmengine<={MMENGINE_MAX}.'
+    f'Please install mmengine<{MMENGINE_MAX}.'
 __all__ = ['__version__', 'version_info', 'digit_version']
