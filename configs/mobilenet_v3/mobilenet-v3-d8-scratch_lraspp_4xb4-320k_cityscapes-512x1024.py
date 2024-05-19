244d0bce0246a51c77ce9710e4b4fe75b0b1cd82
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-05-19 23:04:55 +0800

    fix old MMCV config file format for mobilenetv3

--- configs/mobilenet_v3/mobilenet-v3-d8-scratch_lraspp_4xb4-320k_cityscapes-512x1024.py
+++ configs/mobilenet_v3/mobilenet-v3-d8-scratch_lraspp_4xb4-320k_cityscapes-512x1024.py
@@ -4,10 +4,17 @@ _base_ = [
 ]
 crop_size = (512, 1024)
 data_preprocessor = dict(size=crop_size)
+
+#optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+
 # Re-config the data sampler.
 model = dict(data_preprocessor=data_preprocessor)
 train_dataloader = dict(batch_size=4, num_workers=4)
 val_dataloader = dict(batch_size=1, num_workers=4)
 test_dataloader = val_dataloader
 
-runner = dict(type='IterBasedRunner', max_iters=320000)
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=320000, val_interval=32000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
--- configs/mobilenet_v3/mobilenet-v3-d8_lraspp_4xb4-320k_cityscapes-512x1024.py
+++ configs/mobilenet_v3/mobilenet-v3-d8_lraspp_4xb4-320k_cityscapes-512x1024.py
@@ -8,9 +8,15 @@ model = dict(
     data_preprocessor=data_preprocessor,
     pretrained='open-mmlab://contrib/mobilenet_v3_large')
 
+#optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+
 # Re-config the data sampler.
 train_dataloader = dict(batch_size=4, num_workers=4)
 val_dataloader = dict(batch_size=1, num_workers=4)
 test_dataloader = val_dataloader
 
-runner = dict(type='IterBasedRunner', max_iters=320000)
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=320000, val_interval=32000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
