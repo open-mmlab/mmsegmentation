43d8752f8d55b830a899b75b8de8e725141c3bad
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-05-20 18:08:20 +0800

    Update mobilenet-v3-d8_lraspp_4xb4-320k_cityscapes-512x1024.py

--- configs/mobilenet_v3/mobilenet-v3-d8_lraspp_4xb4-320k_cityscapes-512x1024.py
+++ configs/mobilenet_v3/mobilenet-v3-d8_lraspp_4xb4-320k_cityscapes-512x1024.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/lraspp_m-v3-d8.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_320k.py'
 ]
 crop_size = (512, 1024)
 data_preprocessor = dict(size=crop_size)
@@ -8,16 +8,7 @@ model = dict(
     data_preprocessor=data_preprocessor,
     pretrained='open-mmlab://contrib/mobilenet_v3_large')
 
-# optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
-
 # Re-config the data sampler.
 train_dataloader = dict(batch_size=4, num_workers=4)
 val_dataloader = dict(batch_size=1, num_workers=4)
 test_dataloader = val_dataloader
-
-train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=320000, val_interval=32000)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
