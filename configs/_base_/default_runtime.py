39f019f08b2f50279a32e3b1adbe0e8e9fe4abca
A01781042 <90709790+A01781042@users.noreply.github.com>
2024-06-04 19:02:07 -0600

    tensorboard

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,7 +4,9 @@ env_cfg = dict(
     mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
     dist_cfg=dict(backend='nccl'),
 )
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type='LocalVisBackend'),
+                dict(type='TensorboardVisBackend')]
+
 visualizer = dict(
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 log_processor = dict(by_epoch=False)
